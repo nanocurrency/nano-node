7be725a8b1183372e176356e6f77c97175161b74
clemahieu <clemahieu@gmail.com>
2014-05-11 11:10:50 -0500

    Adding some transaction processing capability.

--- mu_coin/ledger.cpp
+++ mu_coin/ledger.cpp
@@ -13,6 +13,76 @@ bool mu_coin::ledger::has_balance (mu_coin::address const & address_a)
     return latest.find (address_a) != latest.end ();
 }
 
-void mu_coin::ledger::replace (mu_coin::address const & address_a, mu_coin::transaction_block * block_a)
+bool mu_coin::ledger::process (mu_coin::transaction_block * block_a)
 {
+    auto result (false);
+    boost::multiprecision::uint256_t inputs;
+    for (auto i (block_a->inputs.begin ()), j (block_a->inputs.end ()); !result && i != j; ++i)
+    {
+        auto existing (latest.find (i->first));
+        if (existing != latest.end ())
+        {
+            if (existing->second->hash () == i->second.previous)
+            {
+                auto last (existing->second->outputs.find (i->first));
+                if (last != existing->second->outputs.end ())
+                {
+                    if (last->second.coins > i->second.coins)
+                    {
+                        inputs += last->second.coins - i->second.coins;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+        }
+        else
+        {
+            result = true;
+        }
+    }
+    if (!result)
+    {
+        boost::multiprecision::uint256_t outputs;
+        for (auto i (block_a->outputs.begin ()), j (block_a->outputs.end ()); !result && i != j; ++i)
+        {
+            auto existing (latest.find (i->first));
+            if (existing != latest.end ())
+            {
+                if (existing->second->hash() == i->second.previous)
+                {
+                    auto last (existing->second->outputs.find (i->first));
+                    if (last != existing->second->outputs.end ())
+                    {
+                        if (i->second.coins > last->second.coins)
+                        {
+                            outputs += last->second.coins - i->second.coins;
+                        }
+                    }
+                    else
+                    {
+                        result = true;
+                    }
+                }
+                else
+                {
+                    result = true;
+                }
+            }
+            else
+            {
+                result = true;
+            }
+        }
+    }
 }
