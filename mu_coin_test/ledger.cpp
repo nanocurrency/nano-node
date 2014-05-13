696247740b0dddb451999618c73314cf40116ccb
clemahieu <clemahieu@gmail.com>
2014-05-12 21:07:46 -0500

    Adding more tests to make sure spend joins work.

--- mu_coin_test/ledger.cpp
+++ mu_coin_test/ledger.cpp
@@ -145,3 +145,41 @@ TEST (ledger, fail_bad_signature)
     auto error (ledger.process (&spend));
     ASSERT_TRUE (error);
 }
+
+TEST (ledger, join_spend)
+{
+    mu_coin::keypair key1;
+    mu_coin::keypair key2;
+    mu_coin::keypair key3;
+    mu_coin::transaction_block genesis;
+    boost::multiprecision::uint256_t max (std::numeric_limits <boost::multiprecision::uint256_t>::max ());
+    mu_coin::entry entry1 (key1.pub, max, 0);
+    genesis.entries.push_back (entry1);
+    genesis.entries [0].sign (key1.prv, genesis.hash ());
+    mu_coin::ledger ledger;
+    ledger.latest [entry1.address] = &genesis;
+    mu_coin::transaction_block spend1;
+    mu_coin::entry entry2 (key1.pub, 0, 1);
+    spend1.entries.push_back (entry2);
+    mu_coin::entry entry3 (key2.pub, max - 11, 0);
+    spend1.entries.push_back (entry3);
+    mu_coin::entry entry4 (key3.pub, 10, 0);
+    spend1.entries.push_back (entry4);
+    spend1.entries [0].sign (key1.prv, spend1.hash ());
+    spend1.entries [1].sign (key2.prv, spend1.hash ());
+    spend1.entries [2].sign (key3.prv, spend1.hash ());
+    auto error1 (ledger.process (&spend1));
+    ASSERT_FALSE (error1);
+    mu_coin::transaction_block spend2;
+    mu_coin::entry entry5 (key1.pub, max - 2, 2);
+    spend2.entries.push_back (entry5);
+    mu_coin::entry entry6 (key2.pub, 0, 1);
+    spend2.entries.push_back (entry6);
+    mu_coin::entry entry7 (key3.pub, 0, 1);
+    spend2.entries.push_back (entry7);
+    spend2.entries [0].sign (key1.prv, spend2.hash ());
+    spend2.entries [1].sign (key2.prv, spend2.hash ());
+    spend2.entries [2].sign (key3.prv, spend2.hash ());
+    auto error2 (ledger.process (&spend2));
+    ASSERT_FALSE (error2);
+}
