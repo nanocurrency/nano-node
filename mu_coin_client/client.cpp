326c538f963941adbbfc8265441754f26537e0eb
clemahieu <clemahieu@gmail.com>
2014-05-18 20:29:48 -0500

    Adding a lot more functionality to client.

--- mu_coin_client/client.cpp
+++ mu_coin_client/client.cpp
@@ -5,10 +5,48 @@ mu_coin_client::client::client (int argc, char ** argv) :
 store (mu_coin_store::block_store_db_temp),
 wallet (mu_coin_wallet::wallet_temp),
 application (argc, argv),
-wallet_view (&main_window),
-add_key ("Add Key", &main_window)
+wallet_add_key ("Add Key"),
+new_key_password_label ("Password:"),
+new_key_add_key ("Add Key"),
+new_key_cancel ("Cancel")
 {
-    connect (&add_key, &QPushButton::released, [this] () {handle_add_key ();});
+    wallet_view.setModel (&wallet_model);
+    wallet_layout.addWidget (&wallet_add_key);
+    wallet_layout.addWidget (&wallet_view);
+    wallet_window.setLayout (&wallet_layout);
+    
+    new_key_layout.addWidget (&new_key_password_label);
+    new_key_password.setEchoMode (QLineEdit::EchoMode::Password);
+    new_key_layout.addWidget (&new_key_password);
+    new_key_layout.addWidget (&new_key_add_key);
+    new_key_layout.addWidget (&new_key_cancel);
+    new_key_window.setLayout (&new_key_layout);
+    
+    main_stack.addWidget (&wallet_window);
+    main_window.setCentralWidget (&main_stack);
+    connect (&wallet_add_key, &QPushButton::released, [this] ()
+    {
+        main_stack.addWidget (&new_key_window);
+        main_stack.setCurrentIndex (main_stack.count () - 1);
+    });
+    connect (&new_key_add_key, &QPushButton::released, [this] ()
+    {
+        mu_coin::keypair key;
+        CryptoPP::SHA256 hash;
+        QString text (new_key_password.text ());
+        new_key_password.clear ();
+        hash.Update (reinterpret_cast <uint8_t *> (text.data ()), text.size ());
+        text.fill (0);
+        mu_coin::uint256_union secret;
+        hash.Final (secret.bytes.data ());
+        wallet.insert (key.pub, key.prv, secret);
+        refresh_wallet ();
+        main_stack.removeWidget (main_stack.currentWidget ());
+    });
+    connect (&new_key_cancel, &QPushButton::released, [this] ()
+    {
+        main_stack.removeWidget (main_stack.currentWidget ());
+    });
 }
 
 void mu_coin_client::client::refresh_wallet ()
@@ -29,15 +67,6 @@ void mu_coin_client::client::refresh_wallet ()
         keys << string;
     }
     wallet_model.setStringList (keys);
-}
-
-void mu_coin_client::client::handle_add_key ()
-{
-    mu_coin::keypair key;
-    mu_coin::uint256_union secret;
-    secret.bytes.fill (0);
-    wallet.insert (key.pub, key.prv, secret);
-    refresh_wallet ();
 }
 
 mu_coin_client::client::~client ()
--- mu_coin_client/client.hpp
+++ mu_coin_client/client.hpp
@@ -15,13 +15,24 @@ namespace mu_coin_client {
         mu_coin_wallet::wallet wallet;
         QApplication application;
         QMainWindow main_window;
+        QStackedWidget main_stack;
+        
+        QWidget wallet_window;
+        QVBoxLayout wallet_layout;
         QStringListModel wallet_model;
+        QLabel wallet_balance_label;
         QListView wallet_view;
-        QPushButton add_key;
+        QPushButton wallet_add_key;
+        
+        QWidget new_key_window;
+        QVBoxLayout new_key_layout;
+        QLabel new_key_password_label;
+        QLineEdit new_key_password;
+        QPushButton new_key_add_key;
+        QPushButton new_key_cancel;
     private:
         QStringList keys;
     private:
         void refresh_wallet ();
-        void handle_add_key ();
     };
 }
