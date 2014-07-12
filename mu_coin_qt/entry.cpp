b06da64a892367bc17ec2f51d9a4c16e90fb09f9
clemahieu <clemahieu@gmail.com>
2014-07-11 19:40:21 -0500

    Updating qt entrypoint.

--- mu_coin_qt/entry.cpp
+++ mu_coin_qt/entry.cpp
@@ -6,10 +6,9 @@ int main (int argc, char ** argv)
 {
     QApplication application (argc, argv);
     static int count (2);
-    mu_coin::system system (1, 24000, 25000, count);
     mu_coin::keypair genesis_key;
+    mu_coin::system system (1, 24000, 25000, count, genesis_key.pub, std::numeric_limits <mu_coin::uint256_t>::max ());
     system.clients [0]->wallet.insert (genesis_key.pub, genesis_key.prv, system.clients [0]->wallet.password);
-    system.genesis (genesis_key.pub, std::numeric_limits <mu_coin::uint256_t>::max ());
     std::vector <std::unique_ptr <mu_coin_qt::gui>> guis;
     guis.reserve (count);
     for (auto i (0); i < count; ++i)
