5a863584f909ed3f1bd1b9c98c4aa1796dae4020
clemahieu <clemahieu@gmail.com>
2014-09-21 15:17:23 -0500

    Fixing cli.

--- mu_coin_cli/entry.cpp
+++ mu_coin_cli/entry.cpp
@@ -2,16 +2,8 @@
 
 int main ()
 {
-    /*
     mu_coin::system system (1, 24000, 25000, 1);
     system.clients [0]->wallet.insert (mu_coin::test_genesis_key.prv);
     size_t count (10000);
-    system.generate_mass_activity (count, *system.clients [0]);*/
-    
-    mu_coin::work work (32768);
-    auto begin (std::chrono::high_resolution_clock::now ());
-    work.perform (0, 32);
-    auto end (std::chrono::high_resolution_clock::now ());
-    auto us (std::chrono::duration_cast <std::chrono::microseconds> (end - begin));
-    std::cout << boost::str (boost::format ("Microseconds: %1%\n") % us.count ());
+    system.generate_mass_activity (count, *system.clients [0]);
 }
