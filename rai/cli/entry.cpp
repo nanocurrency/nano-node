638e8e00e597c70847e0b419bb65650cd41e4884
clemahieu <clemahieu@gmail.com>
2014-10-16 22:01:50 -0500

    Adding profiling for memory fill via xorshift.

--- rai/cli/entry.cpp
+++ rai/cli/entry.cpp
@@ -3,6 +3,102 @@
 
 #include <boost/program_options.hpp>
 
+#include <emmintrin.h>
+
+class xorshift128
+{
+public:
+    uint64_t s[ 2 ];
+    
+    uint64_t next(void) {
+        uint64_t s1 = s[ 0 ];
+        const uint64_t s0 = s[ 1 ];
+        s[ 0 ] = s0;
+        s1 ^= s1 << 23; // a
+        return ( s[ 1 ] = ( s1 ^ s0 ^ ( s1 >> 17 ) ^ ( s0 >> 26 ) ) ) + s0; // b, c
+    }
+};
+
+class xorshift1024
+{
+public:
+    uint64_t s[ 16 ];
+    int p;
+    
+    uint64_t next(void) {
+        uint64_t s0 = s[ p ];
+        uint64_t s1 = s[ p = ( p + 1 ) & 15 ];
+        s1 ^= s1 << 31; // a
+        s1 ^= s1 >> 11; // b
+        s0 ^= s0 >> 30; // c
+        return ( s[ p ] = s0 ^ s1 ) * 1181783497276652981LL;
+    }
+};
+
+void fill_128_reference (void * data)
+{
+    xorshift128 rng;
+    rng.s [0] = 1;
+    rng.s [1] = 0;
+    for (auto i (reinterpret_cast <uint64_t *> (data)), n (reinterpret_cast <uint64_t *> (data) + 1024 * 1024); i != n; ++i)
+    {
+        *i = rng.next ();
+    }
+}
+
+void fill_128_sse (void * data)
+{
+    xorshift128 rng;
+    rng.s [0] = 1;
+    rng.s [1] = 0;
+    for (auto i (reinterpret_cast <__m128i *> (data)), n (reinterpret_cast <__m128i *> (data) + 512 * 1024); i != n; ++i)
+    {
+        auto v0 (rng.next ());
+        auto v1 (rng.next ());
+        _mm_store_si128 (i, _mm_set_epi64x (v1, v0));
+    }
+}
+
+void fill_1024_reference (void * data)
+{
+    xorshift1024 rng;
+    rng.p = 0;
+    rng.s [0] = 1;
+    for (auto i (0u); i < 16; ++i)
+    {
+        rng.s [i] = 0;
+    }
+    for (auto i (reinterpret_cast <uint64_t *> (data)), n (reinterpret_cast <uint64_t *> (data) + 1024 * 1024); i != n; ++i)
+    {
+        *i = rng.next ();
+    }
+}
+
+void fill_1024_sse (void * data)
+{
+    xorshift1024 rng;
+    rng.p = 0;
+    rng.s [0] = 1;
+    for (auto i (0u); i < 16; ++i)
+    {
+        rng.s [i] = 0;
+    }
+    for (auto i (reinterpret_cast <__m128i *> (data)), n (reinterpret_cast <__m128i *> (data) + 512 * 1024); i != n; ++i)
+    {
+        auto v0 (rng.next ());
+        auto v1 (rng.next ());
+        _mm_store_si128 (i, _mm_set_epi64x (v1, v0));
+    }
+}
+
+void fill_zero (void * data)
+{
+    for (auto i (reinterpret_cast <__m128i *> (data)), n (reinterpret_cast <__m128i *> (data) + 512 * 1024); i != n; ++i)
+    {
+        _mm_store_si128 (i, _mm_setzero_pd ());
+    }
+}
+
 int main (int argc, char * const * argv)
 {
     boost::program_options::options_description description ("Command line options");
@@ -10,7 +106,9 @@ int main (int argc, char * const * argv)
         ("help", "Print out options")
         ("debug_activity", "Generates fake debug activity")
         ("profile_work", "Profile the work function")
-        ("generate_key", "Generates a random keypair");
+        ("generate_key", "Generates a random keypair")
+        ("xorshift_profile", "Profile xorshift algorithms")
+        ("verify_profile", "Profile signature verification");
     boost::program_options::variables_map vm;
     boost::program_options::store (boost::program_options::parse_command_line(argc, argv, description), vm);
     boost::program_options::notify (vm);
@@ -46,6 +144,75 @@ int main (int argc, char * const * argv)
         (void)work.validate (source, value);
         auto end2 (std::chrono::high_resolution_clock::now ());
         std::cerr << boost::str (boost::format ("Generation time: %1%us validation time: %2%us\n") % std::chrono::duration_cast <std::chrono::microseconds> (end1 - begin1).count () % std::chrono::duration_cast <std::chrono::microseconds> (end2 - end1).count ());
+    }
+    else if (vm.count ("xorshift_profile"))
+    {
+        auto unaligned (new uint8_t [64 * 1024 * 1024 + 16]);
+        auto aligned (reinterpret_cast <void *> (reinterpret_cast <uintptr_t> (unaligned) & ~uintptr_t (0xfu)));
+        {
+            memset (aligned, 0x0, 64 * 1024 * 1024);
+            auto begin (std::chrono::high_resolution_clock::now ());
+            for (auto i (0u); i < 1000; ++i)
+            {
+                fill_zero (aligned);
+            }
+            auto end (std::chrono::high_resolution_clock::now ());
+            std::cerr << "Memset " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
+        }
+        {
+            memset (aligned, 0x0, 64 * 1024 * 1024);
+            auto begin (std::chrono::high_resolution_clock::now ());
+            for (auto i (0u); i < 1000; ++i)
+            {
+                fill_128_reference (aligned);
+            }
+            auto end (std::chrono::high_resolution_clock::now ());
+            std::cerr << "Ref fill 128 " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
+        }
+        {
+            memset (aligned, 0x0, 64 * 1024 * 1024);
+            auto begin (std::chrono::high_resolution_clock::now ());
+            for (auto i (0u); i < 1000; ++i)
+            {
+                fill_1024_reference (aligned);
+            }
+            auto end (std::chrono::high_resolution_clock::now ());
+            std::cerr << "Ref fill 1024 " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
+        }
+        {
+            memset (aligned, 0x0, 64 * 1024 * 1024);
+            auto begin (std::chrono::high_resolution_clock::now ());
+            for (auto i (0u); i < 1000; ++i)
+            {
+                fill_128_sse (aligned);
+            }
+            auto end (std::chrono::high_resolution_clock::now ());
+            std::cerr << "SSE fill 128 " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
+        }
+        {
+            memset (aligned, 0x0, 64 * 1024 * 1024);
+            auto begin (std::chrono::high_resolution_clock::now ());
+            for (auto i (0u); i < 1000; ++i)
+            {
+                fill_1024_sse (aligned);
+            }
+            auto end (std::chrono::high_resolution_clock::now ());
+            std::cerr << "SSE fill 1024 " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
+        }
+    }
+    else if (vm.count ("verify_profile"))
+    {
+        rai::keypair key;
+        rai::uint256_union message;
+        rai::uint512_union signature;
+        rai::sign_message (key.prv, key.pub, message, signature);
+        auto begin (std::chrono::high_resolution_clock::now ());
+        for (auto i (0u); i < 1000; ++i)
+        {
+            rai::validate_message (key.pub, key.prv, signature);
+        }
+        auto end (std::chrono::high_resolution_clock::now ());
+        std::cerr << "Signature verifications " << std::chrono::duration_cast <std::chrono::microseconds> (end - begin).count () << std::endl;
     }
     else
     {
