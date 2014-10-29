49b3a1b77074af50d5cdacbae51f2e4706585f18
clemahieu <clemahieu@gmail.com>
2014-10-28 22:01:04 -0500

    Fields not yet implemented.

--- rai/core/core.hpp
+++ rai/core/core.hpp
@@ -150,9 +150,6 @@ namespace rai {
         bool deserialize (rai::stream &);
         void serialize (rai::stream &) override;
 		bool operator == (rai::keepalive const &) const;
-        uint8_t version_running;
-        uint8_t version_supported;
-        std::bitset <16> features;
 		std::array <rai::endpoint, 8> peers;
 		rai::uint256_union checksum;
     };
