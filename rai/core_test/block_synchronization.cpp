9f9ddd3289662b539ea8eb65221e72b4d06ad03c
clemahieu <clemahieu@gmail.com>
2015-01-03 16:11:14 -0600

    Added ladder dependency test.

--- rai/core_test/block_synchronization.cpp
+++ rai/core_test/block_synchronization.cpp
@@ -141,3 +141,46 @@ TEST (pull_synchronization, receive_dependencies)
     ASSERT_EQ (block4, *blocks [2]);
     ASSERT_EQ (block5, *blocks [3]);
 }
+
+TEST (pull_synchronization, ladder_dependencies)
+{
+    leveldb::Status init;
+    rai::block_store store (init, rai::block_store_temp);
+    rai::send_block block1;
+    block1.hashables.previous = 1;
+    rai::send_block block2;
+    block2.hashables.previous = block1.hash ();
+    rai::open_block block3;
+    block3.hashables.source = block2.hash ();
+    rai::send_block block4;
+    block4.hashables.previous = block3.hash ();
+    rai::receive_block block5;
+    block5.hashables.previous = block2.hash ();
+    block5.hashables.source = block4.hash ();
+    rai::send_block block6;
+    block6.hashables.previous = block5.hash ();
+    rai::receive_block block7;
+    block7.hashables.previous = block4.hash ();
+    block7.hashables.source = block6.hash ();
+    std::vector <std::unique_ptr <rai::block>> blocks;
+    store.block_put (block1.hash (), block1);
+    store.bootstrap_put (block2.hash (), block2);
+    store.bootstrap_put (block3.hash (), block3);
+    store.bootstrap_put (block4.hash (), block4);
+    store.bootstrap_put (block5.hash (), block5);
+    store.bootstrap_put (block6.hash (), block6);
+    store.bootstrap_put (block7.hash (), block7);
+    rai::pull_synchronization sync ([&blocks, &store] (rai::block const & block_a)
+    {
+        store.block_put (block_a.hash (), block_a);
+        blocks.push_back (block_a.clone ());
+    }, store);
+    ASSERT_FALSE (sync.synchronize (block7.hash ()));
+    ASSERT_EQ (6, blocks.size ());
+    ASSERT_EQ (block2, *blocks [0]);
+    ASSERT_EQ (block3, *blocks [1]);
+    ASSERT_EQ (block4, *blocks [2]);
+    ASSERT_EQ (block5, *blocks [3]);
+    ASSERT_EQ (block6, *blocks [4]);
+    ASSERT_EQ (block7, *blocks [5]);
+}
