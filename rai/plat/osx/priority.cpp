cf5cb5e059e43d4e76e0c2adb1c416be71a27dea
clemahieu <clemahieu@gmail.com>
2016-01-04 09:47:43 -0600

    Correcting implementation.

--- rai/plat/osx/priority.cpp
+++ rai/plat/osx/priority.cpp
@@ -1,16 +1,5 @@
 #include <rai/utility.hpp>
 
-#include <pthread.h>
-
 void rai::lower_priority ()
 {
-	auto handle (pthread_self ());
-	int policy;
-	struct sched_param sched;
-	if (pthread_getschedparam (handle, &policy, &sched) == 0)
-	{
-		policy = SCHED_BATCH;
-		auto result (pthread_setschedparam (handle, policy, &sched));
-		(void) result;
-	}
 }
--- rai/plat/posix/priority.cpp
+++ rai/plat/posix/priority.cpp
@@ -1,5 +1,16 @@
 #include <rai/utility.cpp>
 
+#include <pthread.h>
+
 void rai::lower_priority ()
 {
+	auto handle (pthread_self ());
+	int policy;
+	struct sched_param sched;
+	if (pthread_getschedparam (handle, &policy, &sched) == 0)
+	{
+		policy = SCHED_BATCH;
+		auto result (pthread_setschedparam (handle, policy, &sched));
+		(void) result;
+	}
 }
