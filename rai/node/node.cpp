86303de8f547df8d05217ef533e970f97d6b95be
clemahieu <clemahieu@gmail.com>
2017-08-19 13:05:51 -0500

    Adjusting block_processor.

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -1556,11 +1556,11 @@ void rai::network::confirm_send (rai::confirm_ack const & confirm_a, std::shared
 	});
 }
 
-void rai::block_processor::process_receive_republish (std::shared_ptr <rai::block> incoming)
+void rai::node::process_receive_republish (std::shared_ptr <rai::block> incoming)
 {
     assert (incoming != nullptr);
     auto node_l (shared_from_this ());
-    process_receive_many (incoming, [node_l] (MDB_txn * transaction_a, rai::process_return result_a, std::shared_ptr <rai::block> block_a)
+    block_processor.process_receive_many (incoming, [node_l] (MDB_txn * transaction_a, rai::process_return result_a, std::shared_ptr <rai::block> block_a)
     {
         switch (result_a.code)
         {
