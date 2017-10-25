82e066b0a2079ed2606a57b2b90b3feda98a56bc
SergiySW <sergiysw@gmail.com>
2017-10-25 17:01:34 +0300

    Upgrading serialize_json config to version 8

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -771,7 +771,7 @@ unchecked_cache_multiplier (32) // 32 * 256 = 8,192
 
 void rai::node_config::serialize_json (boost::property_tree::ptree & tree_a) const
 {
-	tree_a.put ("version", "7");
+	tree_a.put ("version", "8");
 	tree_a.put ("peering_port", std::to_string (peering_port));
 	tree_a.put ("bootstrap_fraction_numerator", std::to_string (bootstrap_fraction_numerator));
 	tree_a.put ("receive_minimum", receive_minimum.to_string_dec ());
