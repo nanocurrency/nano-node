04377bdfa97e13b5007f6a4a2803529bef462e40
SergiySW <sergiysw@gmail.com>
2017-12-07 22:35:50 +0300

    Processing vector of block-action pairs

--- rai/node/node.hpp
+++ rai/node/node.hpp
@@ -441,7 +441,7 @@ public:
     void flush ();
 	void add (std::shared_ptr <rai::block>, std::function <void (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>)> = [] (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>) {});
 	void process_receive_many (std::shared_ptr <rai::block>, std::function <void (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>)> = [] (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>) {});
-	void process_receive_many (std::vector <std::shared_ptr <rai::block>>, std::function <void (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>)> = [] (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>) {});
+	void process_receive_many (std::vector <std::pair <std::shared_ptr <rai::block>, std::function <void (MDB_txn *, rai::process_return, std::shared_ptr <rai::block>)>>>);
     rai::process_return process_receive_one (MDB_txn *, std::shared_ptr <rai::block>);
 	void process_blocks ();
 private:
