1165a1d6f7476ddfb55557f3d13ea18e3d7a67d3
Ryan Gardner <rygard@hotmail.com>
2017-12-26 08:46:35 +0000

    Improved code speed and efficiency
    Should still work if using Python 2.7
    
    Double check it still works though please

--- bootstrap.py
+++ bootstrap.py
@@ -4,21 +4,19 @@ import os
 import subprocess
 
 if not os.path.exists("bootstrap"):
-	os.mkdir("bootstrap")
+        os.mkdir("bootstrap")
 contents = sys.stdin.read()
 subs = re.findall("\{.*?\}", contents, re.DOTALL)
-block_number = 0
-html_file_name = "./bootstrap/index.html"
-html_file = open(html_file_name, "w")
+html_file = open("./bootstrap/index.html", "w")
 html_file.write("<body>")
-for i in subs:
-	block_file_name = "./bootstrap/block" + str(block_number) + ".json"
-	block_file = open(block_file_name, "w")
-	block_file.write(i)
-	block_file.close()
-	block_file = open(block_file_name, "r")
-	qr_file_name = "./bootstrap/block" + str(block_number) + ".png"
-	html_file.write("<img src=\"block" + str(block_number) + ".png\"/>")
-	subprocess.call(["qrencode", "-o", qr_file_name], stdin=block_file)
-	block_number = block_number + 1
+for block_number,sub in enumerate(subs):
+        block_number = str(block_number)
+        block_file_name = "".join(("./bootstrap/block", block_number, ".json"))
+        block_file = open(block_file_name, "w")
+        block_file.write(sub)
+        block_file.close()
+        block_file = open(block_file_name, "r")
+        qr_file_name = "".join(("./bootstrap/block", block_number, ".png"))
+        html_file.write("".join(("<img src=\"block", block_number, ".png\"/>")))
+        subprocess.call(["qrencode", "-o", qr_file_name], stdin=block_file)
 html_file.write("</body>")
