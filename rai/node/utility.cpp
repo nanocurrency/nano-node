27090ca9551515db0f1ebc0124179228a2dbef63
clemahieu <clemahieu@gmail.com>
2018-01-03 01:04:15 -0600

    Using safer MDB_NOTLS in case io_threads > mdb_env_set_maxreaders

--- rai/node/utility.cpp
+++ rai/node/utility.cpp
@@ -26,7 +26,9 @@ rai::mdb_env::mdb_env (bool & error_a, boost::filesystem::path const & path_a)
 			assert (status2 == 0);
 			auto status3 (mdb_env_set_mapsize (environment, 1ULL * 1024 * 1024 * 1024 * 1024)); // 1 Terabyte
 			assert (status3 == 0);
-			auto status4 (mdb_env_open (environment, path_a.string ().c_str (), MDB_NOSUBDIR, 00600));
+			// It seems if there's ever more threads than mdb_env_set_maxreaders has read slots available, we get failures on transaction creation unless MDB_NOTLS is specified
+			// This can happen if something like 256 io_threads are specified in the node config
+			auto status4 (mdb_env_open (environment, path_a.string ().c_str (), MDB_NOSUBDIR | MDB_NOTLS, 00600));
 			error_a = status4 != 0;
 		}
 		else
