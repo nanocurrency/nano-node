02cae5c28174c52adcc9723ef27e34cb76d2c382
androm3da <brian.cain@gmail.com>
2018-02-14 20:54:25 -0600

    [CI] Fix bootstrap boost
    Update src tarball URL
    Make wget less chatty

--- ci/bootstrap_boost.sh
+++ ci/bootstrap_boost.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+set -o unset
+set -o xtrace
+
 BOOST_BASENAME=boost_1_66_0
 BOOST_ROOT=${BOOST_ROOT-/usr/local/boost}
-BOOST_URL=http://sourceforge.net/projects/boost/files/boost/1.66.0/${BOOST_BASENAME}.tar.gz/download
+BOOST_URL=https://downloads.sourceforge.net/project/boost/boost/1.66.0/${BOOST_BASENAME}.tar.bz2
 
-wget -O ${BOOST_BASENAME}.tar.gz ${BOOST_URL}
+wget --quiet -O ${BOOST_BASENAME}.tar.gz "${BOOST_URL}"
 tar xf ${BOOST_BASENAME}.tar.gz
 cd ${BOOST_BASENAME}
 ./bootstrap.sh
-./b2 --prefix=${BOOST_ROOT} link=static install
+./b2 -d0 --prefix=${BOOST_ROOT} link=static install
 cd ..
 rm -rf ${BOOST_BASENAME}
 rm -f ${BOOST_BASENAME}.tar.gz
