a431f923e9895a4eec2a89ac111de92e83007138
Paul Melis <paul@floorball-flamingos.nl>
2018-02-17 16:28:24 +0100

    Fix formatting

--- rai/node/rpc.cpp
+++ rai/node/rpc.cpp
@@ -3136,7 +3136,7 @@ void rai::rpc_handler::version ()
 	boost::property_tree::ptree response_l;
 	response_l.put ("rpc_version", "1");
 	response_l.put ("store_version", std::to_string (node.store_version ()));
-	if (patch) 
+	if (patch)
 	{
 		response_l.put ("node_vendor", boost::str (boost::format ("RaiBlocks %1%.%2%.%3%") % RAIBLOCKS_VERSION_MAJOR % RAIBLOCKS_VERSION_MINOR % RAIBLOCKS_VERSION_PATCH));
 	}
