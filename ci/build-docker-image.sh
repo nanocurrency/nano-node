34b07f4ea2db9d5df8b4f2d43bdbc34664a324f1
Roy Keene <rkeene@users.noreply.github.com>
2018-03-08 12:03:26 -0600

    Docker altnets (#708)
    * Updated to check the boost SHA256 when downloading
    
    * Fixed invalid shell option
    
    * Allow the user to request a minimal build of Boost
    
    * Ensure boost build script exits on error
    
    * Removed apparently unused "mkdir"
    
    * Updated Docker build for node to support altnets
    
    * Allow user-supplied arguments to "docker build"
    
    * Updated Docker auto deploy to build a live and beta image for each tag
    
    * Print the correct image name when building the docker image
    
    * Corrected typo for the test network

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -eu
 
+if [ "$#" -lt 2 ]; then
+	echo 'Usage: build-docker-image.sh <dockerFile> <dockerImageTag> [<dockerBuildArgs>...]' >&2
+	exit 1
+fi
+
+dockerFile="$1"
+dockerTag="$2"
+shift; shift
+
 scripts="$(dirname "$0")"
 
-"$scripts"/custom-timeout.sh 20 docker pull "$2" || true
-echo "Building $2"
-"$scripts"/custom-timeout.sh 30 docker build -f "$1" -t "$2" --cache-from "$2" .
+"$scripts"/custom-timeout.sh 20 docker pull "${dockerTag}" || true
+echo "Building $dockerTag"
+"$scripts"/custom-timeout.sh 30 docker build "$@" -f "${dockerFile}" -t "${dockerTag}" --cache-from "${dockerTag}" .
