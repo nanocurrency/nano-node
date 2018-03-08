12dd3474a367d1ef6225490a410e55c44b6782e6
Roy Keene <github@rkeene.org>
2018-03-08 00:48:15 -0600

    Updated Docker auto deploy to build a live and beta image for each tag

--- ci/deploy-docker.sh
+++ ci/deploy-docker.sh
@@ -15,13 +15,23 @@ elif [ -n "$TRAVIS_TAG" ]; then
     tags+=("$TRAVIS_TAG" latest)
 fi
 
-ci/build-docker-image.sh docker/node/Dockerfile nanocurrency/nano
-for tag in "${tags[@]}"; do
-    # Sanitize docker tag
-    # https://docs.docker.com/engine/reference/commandline/tag/
-    tag="$(printf '%s' "$tag" | tr -c '[a-z][A-Z][0-9]_.-' -)"
-    if [ "$tag" != "latest" ]; then
-        docker tag nanocurrency/nano nanocurrency/nano:"$tag"
+for network in live beta; do
+    if [ "${network}" = 'live' ]; then
+        network_tag_suffix=''
+    else
+        network_tag_suffix="-${network}"
     fi
-    "$scripts"/custom-timeout.sh 30 docker push nanocurrency/nano:"$tag"
+
+    docker_image_name="nanocurrency/nano${network_tag_suffix}"
+
+    ci/build-docker-image.sh docker/node/Dockerfile "$docker_image_name" --build-arg NETWORK="${network}"
+    for tag in "${tags[@]}"; do
+        # Sanitize docker tag
+        # https://docs.docker.com/engine/reference/commandline/tag/
+        tag="$(printf '%s' "$tag" | tr -c '[a-z][A-Z][0-9]_.-' -)"
+        if [ "$tag" != "latest" ]; then
+            docker tag "$docker_image_name" "${docker_image_name}:$tag"
+        fi
+        "$scripts"/custom-timeout.sh 30 docker push "${docker_image_name}:$tag"
+    done
 done
