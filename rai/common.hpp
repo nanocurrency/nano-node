a6d60d2f16014e53f513bb566e5e0db3da5d9166
Russel Waters <vaelstrom@gmail.com>
2018-06-11 01:40:13 -0400

    Version bump
    Bumping protocol version on master since we have a V14 stabilization branch

--- rai/common.hpp
+++ rai/common.hpp
@@ -23,7 +23,7 @@ struct hash<rai::uint256_union>
 }
 namespace rai
 {
-const uint8_t protocol_version = 0x0a;
+const uint8_t protocol_version = 0x0b;
 const uint8_t protocol_version_min = 0x07;
 
 class block_store;
