a63503d6a4774025f1c9c90db3067b4acc1e723d
SergiySW <sergiysw@gmail.com>
2018-06-29 16:26:38 +0300

    Clang formatting

--- rai/qt/qt.cpp
+++ rai/qt/qt.cpp
@@ -2176,7 +2176,7 @@ void rai_qt::block_creation::create_receive ()
 						{
 							auto rep_block (wallet.node.store.block_get (transaction, info.rep_block));
 							assert (rep_block != nullptr);
-							rai::state_block receive (pending_key.account, info.head, rep_block->representative(), info.balance.number() + pending.amount.number(), source_l, key, pending_key.account, 0);
+							rai::state_block receive (pending_key.account, info.head, rep_block->representative (), info.balance.number () + pending.amount.number (), source_l, key, pending_key.account, 0);
 							wallet.node.work_generate_blocking (receive);
 							std::string block_l;
 							receive.serialize_json (block_l);
