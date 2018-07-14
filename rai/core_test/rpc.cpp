01edf2cd897c792dbdd0a861ae5dede52982bda1
Lee Bousfield <ljbousfield@gmail.com>
2018-07-13 18:30:07 -0600

    Fix rpc.version test

--- rai/core_test/rpc.cpp
+++ rai/core_test/rpc.cpp
@@ -1528,7 +1528,10 @@ TEST (rpc, version)
 	ASSERT_EQ (200, response1.status);
 	ASSERT_EQ ("1", response1.json.get<std::string> ("rpc_version"));
 	ASSERT_EQ (200, response1.status);
-	ASSERT_EQ ("11", response1.json.get<std::string> ("store_version"));
+	{
+		rai::transaction transaction (system.nodes[0]->store.environment, nullptr, false);
+		ASSERT_EQ (std::to_string (node1->store.version_get (transaction)), response1.json.get<std::string> ("store_version"));
+	}
 	ASSERT_EQ (boost::str (boost::format ("RaiBlocks %1%.%2%") % RAIBLOCKS_VERSION_MAJOR % RAIBLOCKS_VERSION_MINOR), response1.json.get<std::string> ("node_vendor"));
 	auto headers (response1.resp.base ());
 	auto allowed_origin (headers.at ("Access-Control-Allow-Origin"));
