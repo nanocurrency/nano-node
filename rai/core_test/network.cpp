8c3192f2767cc8fe234869e3b8cc5963118cdbbf
Roy Keene <github@rkeene.org>
2018-07-24 12:07:51 -0500

    Fixed a test that was relying on incorrect semantics for "bulk_pull"

--- rai/core_test/network.cpp
+++ rai/core_test/network.cpp
@@ -473,7 +473,7 @@ TEST (bulk_pull, none)
 	auto connection (std::make_shared<rai::bootstrap_server> (nullptr, system.nodes[0]));
 	rai::genesis genesis;
 	std::unique_ptr<rai::bulk_pull> req (new rai::bulk_pull{});
-	req->start = genesis.hash ();
+	req->start = rai::test_genesis_key.pub;
 	req->end = genesis.hash ();
 	connection->requests.push (std::unique_ptr<rai::message>{});
 	auto request (std::make_shared<rai::bulk_pull_server> (connection, std::move (req)));
