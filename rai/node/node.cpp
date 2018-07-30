d1699c8f4ab3c0afaa5f21c834b9ef1676837d82
SergiySW <sergiysw@gmail.com>
2018-07-30 19:46:15 +0300

    Disable double vote request for forks
    https://github.com/nanocurrency/raiblocks/pull/803
    cover this case

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -3652,10 +3652,6 @@ void rai::active_transactions::announce_votes ()
 				{
 					// broadcast_confirm_req_base modifies reps, so we clone it once to avoid aliasing
 					node.network.broadcast_confirm_req_base (i->confirm_req_options.first, std::make_shared<std::vector<rai::peer_information>> (*reps), 0);
-					if (i->confirm_req_options.second)
-					{
-						node.network.broadcast_confirm_req_base (i->confirm_req_options.second, reps, 0);
-					}
 				}
 				else
 				{
