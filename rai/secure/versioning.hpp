pnede3/deur

Removing unused function and fix formatting.

rai/versioning.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <rai/lib/blocks.hpp>
-#include <rai/node/utility.hpp>
 #include <rai/node/lmdb.hpp>
+#include <rai/node/utility.hpp>
 
 namespace rai
 {

nanocurrency/nano-node

rai/secure/versioning.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <rai/lib/blocks.hpp>
-#include <rai/secure/utility.hpp>
 #include <rai/node/lmdb.hpp>
+#include <rai/secure/utility.hpp>
 
 namespace rai
 {