pnede3/deur

Using an explicit operator for conversions from mdb_val instead of a function so names won't drift from typenames.

rai/core_test/block_store.cpp
@@ -289,7 +289,7 @@ TEST (block_store, one_bootstrap)
 	auto begin (store.unchecked_begin (transaction));
 	auto end (store.unchecked_end ());
 	ASSERT_NE (end, begin);
-	auto hash1 (begin->first.uint256 ());
+	rai::uint256_union hash1 (begin->first);
 	ASSERT_EQ (block1->hash (), hash1);
 	auto block2 (rai::deserialize_block (begin->second));
 	ASSERT_EQ (*block1, *block2);
@@ -333,7 +333,7 @@ TEST (block_store, one_account)
 	auto begin (store.latest_begin (transaction));
 	auto end (store.latest_end ());
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account, begin->first.uint256 ());
+	ASSERT_EQ (account, rai::uint256_union (begin->first));
 	rai::account_info info (begin->second);
 	ASSERT_EQ (hash, info.head);
 	ASSERT_EQ (42, info.balance.number ());
@@ -379,7 +379,7 @@ TEST (block_store, two_account)
 	auto begin (store.latest_begin (transaction));
 	auto end (store.latest_end ());
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account1, begin->first.uint256 ());
+	ASSERT_EQ (account1, rai::uint256_union (begin->first));
 	rai::account_info info1 (begin->second);
 	ASSERT_EQ (hash1, info1.head);
 	ASSERT_EQ (42, info1.balance.number ());
@@ -387,7 +387,7 @@ TEST (block_store, two_account)
 	ASSERT_EQ (300, info1.block_count);
 	++begin;
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account2, begin->first.uint256 ());
+	ASSERT_EQ (account2, rai::uint256_union (begin->first));
 	rai::account_info info2 (begin->second);
 	ASSERT_EQ (hash2, info2.head);
 	ASSERT_EQ (84, info2.balance.number ());
@@ -492,7 +492,7 @@ TEST (block_store, unsynced)
 	store.unsynced_put (transaction, hash1);
 	ASSERT_TRUE (store.unsynced_exists (transaction, hash1));
 	ASSERT_NE (store.unsynced_end (), store.unsynced_begin (transaction));
-	ASSERT_EQ (hash1, rai::uint256_union (store.unsynced_begin (transaction)->first.uint256 ()));
+	ASSERT_EQ (hash1, rai::uint256_union (store.unsynced_begin (transaction)->first));
 	store.unsynced_del (transaction, hash1);
 	ASSERT_FALSE (store.unsynced_exists (transaction, hash1));
 	ASSERT_EQ (store.unsynced_end (), store.unsynced_begin (transaction));
@@ -512,7 +512,7 @@ TEST (block_store, unsynced_iteration)
 	std::unordered_set<rai::block_hash> hashes;
 	for (auto i (store.unsynced_begin (transaction)), n (store.unsynced_end ()); i != n; ++i)
 	{
-		hashes.insert (rai::uint256_union (i->first.uint256 ()));
+		hashes.insert (rai::uint256_union (i->first));
 	}
 	ASSERT_EQ (2, hashes.size ());
 	ASSERT_TRUE (hashes.find (hash1) != hashes.end ());
@@ -538,7 +538,7 @@ TEST (block_store, large_iteration)
 	rai::transaction transaction (store.environment, nullptr, false);
 	for (auto i (store.latest_begin (transaction, 0)), n (store.latest_end ()); i != n; ++i)
 	{
-		rai::account current (i->first.uint256 ());
+		rai::account current (i->first);
 		assert (current.number () > previous.number ());
 		accounts2.insert (current);
 		previous = current;

rai/node/rpc.cpp
@@ -413,7 +413,7 @@ void rai::rpc_handler::account_list ()
 			for (auto i (existing->second->store.begin (transaction)), j (existing->second->store.end ()); i != j; ++i)
 			{
 				boost::property_tree::ptree entry;
-				entry.put ("", rai::uint256_union (i->first.uint256 ()).to_account ());
+				entry.put ("", rai::uint256_union (i->first).to_account ());
 				accounts.push_back (std::make_pair ("", entry));
 			}
 			response_l.add_child ("accounts", accounts);
@@ -1463,7 +1463,7 @@ void rai::rpc_handler::delegators ()
 			{
 				std::string balance;
 				rai::uint128_union (info.balance).encode_dec (balance);
-				delegators.put (rai::account (i->first.uint256 ()).to_account (), balance);
+				delegators.put (rai::account (i->first).to_account (), balance);
 			}
 		}
 		response_l.add_child ("delegators", delegators);
@@ -1556,7 +1556,7 @@ void rai::rpc_handler::frontiers ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (node.store.latest_begin (transaction, start)), n (node.store.latest_end ()); i != n && frontiers.size () < count; ++i)
 			{
-				frontiers.put (rai::account (i->first.uint256 ()).to_account (), rai::account_info (i->second).head.to_string ());
+				frontiers.put (rai::account (i->first).to_account (), rai::account_info (i->second).head.to_string ());
 			}
 			response_l.add_child ("frontiers", frontiers);
 			response (response_l);
@@ -1897,7 +1897,7 @@ void rai::rpc_handler::ledger ()
 				rai::account_info info (i->second);
 				if (info.modified >= modified_since)
 				{
-					rai::account account (i->first.uint256 ());
+					rai::account account (i->first);
 					boost::property_tree::ptree response_l;
 					response_l.put ("frontier", info.head.to_string ());
 					response_l.put ("open_block", info.open_block.to_string ());
@@ -1936,7 +1936,7 @@ void rai::rpc_handler::ledger ()
 				rai::uint128_union balance (info.balance);
 				if (info.modified >= modified_since)
 				{
-					ledger_l.push_back (std::make_pair (balance, rai::account (i->first.uint256 ())));
+					ledger_l.push_back (std::make_pair (balance, rai::account (i->first)));
 				}
 			}
 			std::sort (ledger_l.begin (), ledger_l.end ());
@@ -2786,7 +2786,7 @@ void rai::rpc_handler::representatives ()
 	{
 		for (auto i (node.store.representation_begin (transaction)), n (node.store.representation_end ()); i != n && representatives.size () < count; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			auto amount (node.store.representation_get (transaction, account));
 			representatives.put (account.to_account (), amount.convert_to<std::string> ());
 		}
@@ -2796,7 +2796,7 @@ void rai::rpc_handler::representatives ()
 		std::vector<std::pair<rai::uint128_union, std::string>> representation;
 		for (auto i (node.store.representation_begin (transaction)), n (node.store.representation_end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			auto amount (node.store.representation_get (transaction, account));
 			representation.push_back (std::make_pair (amount, account.to_account ()));
 		}
@@ -3223,7 +3223,7 @@ void rai::rpc_handler::unchecked_keys ()
 		auto block (rai::deserialize_block (stream));
 		std::string contents;
 		block->serialize_json (contents);
-		entry.put ("key", rai::block_hash (i->first.uint256 ()).to_string ());
+		entry.put ("key", rai::block_hash (i->first).to_string ());
 		entry.put ("hash", block->hash ().to_string ());
 		entry.put ("contents", contents);
 		unchecked.push_back (std::make_pair ("", entry));
@@ -3371,7 +3371,7 @@ void rai::rpc_handler::wallet_balance_total ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				balance = balance + node.ledger.account_balance (transaction, account);
 				pending = pending + node.ledger.account_pending (transaction, account);
 			}
@@ -3416,7 +3416,7 @@ void rai::rpc_handler::wallet_balances ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				rai::uint128_t balance = node.ledger.account_balance (transaction, account);
 				if (threshold.is_zero ())
 				{
@@ -3640,7 +3640,7 @@ void rai::rpc_handler::wallet_frontiers ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				auto latest (node.ledger.latest (transaction, account));
 				if (!latest.is_zero ())
 				{
@@ -3712,7 +3712,7 @@ void rai::rpc_handler::wallet_ledger ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				rai::account_info info;
 				if (!node.store.account_get (transaction, account, info))
 				{
@@ -3832,7 +3832,7 @@ void rai::rpc_handler::wallet_pending ()
 			rai::transaction transaction (node.store.environment, nullptr, false);
 			for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				boost::property_tree::ptree peers_l;
 				rai::account end (account.number () + 1);
 				for (auto ii (node.store.pending_begin (transaction, rai::pending_key (account, 0))), nn (node.store.pending_begin (transaction, rai::pending_key (end, 0))); ii != nn && peers_l.size () < count; ++ii)
@@ -3974,7 +3974,7 @@ void rai::rpc_handler::wallet_republish ()
 					rai::transaction transaction (node.store.environment, nullptr, false);
 					for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 					{
-						rai::account account (i->first.uint256 ());
+						rai::account account (i->first);
 						auto latest (node.ledger.latest (transaction, account));
 						std::unique_ptr<rai::block> block;
 						std::vector<rai::block_hash> hashes;
@@ -4036,7 +4036,7 @@ void rai::rpc_handler::wallet_work_get ()
 				rai::transaction transaction (node.store.environment, nullptr, false);
 				for (auto i (existing->second->store.begin (transaction)), n (existing->second->store.end ()); i != n; ++i)
 				{
-					rai::account account (i->first.uint256 ());
+					rai::account account (i->first);
 					uint64_t work (0);
 					auto error_work (existing->second->store.work_get (transaction, account, work));
 					works.put (account.to_account (), rai::to_string_hex (work));

nanocurrency/nano-node

rai/core_test/block_store.cpp
@@ -290,7 +290,7 @@ TEST (block_store, one_bootstrap)
 	auto begin (store.unchecked_begin (transaction));
 	auto end (store.unchecked_end ());
 	ASSERT_NE (end, begin);
-	auto hash1 (begin->first.uint256 ());
+	rai::uint256_union hash1 (begin->first);
 	ASSERT_EQ (block1->hash (), hash1);
 	auto block2 (rai::deserialize_block (begin->second));
 	ASSERT_EQ (*block1, *block2);
@@ -334,7 +334,7 @@ TEST (block_store, one_account)
 	auto begin (store.latest_begin (transaction));
 	auto end (store.latest_end ());
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account, begin->first.uint256 ());
+	ASSERT_EQ (account, rai::account(begin->first));
 	rai::account_info info (begin->second);
 	ASSERT_EQ (hash, info.head);
 	ASSERT_EQ (42, info.balance.number ());
@@ -380,7 +380,7 @@ TEST (block_store, two_account)
 	auto begin (store.latest_begin (transaction));
 	auto end (store.latest_end ());
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account1, begin->first.uint256 ());
+	ASSERT_EQ (account1, rai::account (begin->first));
 	rai::account_info info1 (begin->second);
 	ASSERT_EQ (hash1, info1.head);
 	ASSERT_EQ (42, info1.balance.number ());
@@ -388,7 +388,7 @@ TEST (block_store, two_account)
 	ASSERT_EQ (300, info1.block_count);
 	++begin;
 	ASSERT_NE (end, begin);
-	ASSERT_EQ (account2, begin->first.uint256 ());
+	ASSERT_EQ (account2, rai::account(begin->first));
 	rai::account_info info2 (begin->second);
 	ASSERT_EQ (hash2, info2.head);
 	ASSERT_EQ (84, info2.balance.number ());
@@ -500,7 +500,7 @@ TEST (block_store, large_iteration)
 	rai::transaction transaction (store.environment, nullptr, false);
 	for (auto i (store.latest_begin (transaction, 0)), n (store.latest_end ()); i != n; ++i)
 	{
-		rai::account current (i->first.uint256 ());
+		rai::account current (i->first);
 		assert (current.number () > previous.number ());
 		accounts2.insert (current);
 		previous = current;

rai/node/rpc.cpp
@@ -515,7 +515,7 @@ void rai::rpc_handler::account_list ()
 		for (auto i (wallet->store.begin (transaction)), j (wallet->store.end ()); i != j; ++i)
 		{
 			boost::property_tree::ptree entry;
-			entry.put ("", rai::uint256_union (i->first.uint256 ()).to_account ());
+			entry.put ("", rai::account (i->first).to_account ());
 			accounts.push_back (std::make_pair ("", entry));
 		}
 		response_l.add_child ("accounts", accounts);
@@ -1399,7 +1399,7 @@ void rai::rpc_handler::delegators ()
 			{
 				std::string balance;
 				rai::uint128_union (info.balance).encode_dec (balance);
-				delegators.put (rai::account (i->first.uint256 ()).to_account (), balance);
+				delegators.put (rai::account (i->first).to_account (), balance);
 			}
 		}
 		response_l.add_child ("delegators", delegators);
@@ -1468,7 +1468,7 @@ void rai::rpc_handler::frontiers ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (node.store.latest_begin (transaction, start)), n (node.store.latest_end ()); i != n && frontiers.size () < count; ++i)
 		{
-			frontiers.put (rai::account (i->first.uint256 ()).to_account (), rai::account_info (i->second).head.to_string ());
+			frontiers.put (rai::account (i->first).to_account (), rai::account_info (i->second).head.to_string ());
 		}
 		response_l.add_child ("frontiers", frontiers);
 	}
@@ -1781,7 +1781,7 @@ void rai::rpc_handler::ledger ()
 				rai::account_info info (i->second);
 				if (info.modified >= modified_since)
 				{
-					rai::account account (i->first.uint256 ());
+					rai::account account (i->first);
 					boost::property_tree::ptree response_a;
 					response_a.put ("frontier", info.head.to_string ());
 					response_a.put ("open_block", info.open_block.to_string ());
@@ -1820,7 +1820,7 @@ void rai::rpc_handler::ledger ()
 				rai::uint128_union balance (info.balance);
 				if (info.modified >= modified_since)
 				{
-					ledger_l.push_back (std::make_pair (balance, rai::account (i->first.uint256 ())));
+					ledger_l.push_back (std::make_pair (balance, rai::account (i->first)));
 				}
 			}
 			std::sort (ledger_l.begin (), ledger_l.end ());
@@ -2390,7 +2390,7 @@ void rai::rpc_handler::representatives ()
 		{
 			for (auto i (node.store.representation_begin (transaction)), n (node.store.representation_end ()); i != n && representatives.size () < count; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				auto amount (node.store.representation_get (transaction, account));
 				representatives.put (account.to_account (), amount.convert_to<std::string> ());
 			}
@@ -2400,7 +2400,7 @@ void rai::rpc_handler::representatives ()
 			std::vector<std::pair<rai::uint128_union, std::string>> representation;
 			for (auto i (node.store.representation_begin (transaction)), n (node.store.representation_end ()); i != n; ++i)
 			{
-				rai::account account (i->first.uint256 ());
+				rai::account account (i->first);
 				auto amount (node.store.representation_get (transaction, account));
 				representation.push_back (std::make_pair (amount, account.to_account ()));
 			}
@@ -2771,7 +2771,7 @@ void rai::rpc_handler::unchecked_keys ()
 			auto block (rai::deserialize_block (stream));
 			std::string contents;
 			block->serialize_json (contents);
-			entry.put ("key", rai::block_hash (i->first.uint256 ()).to_string ());
+			entry.put ("key", rai::block_hash (i->first).to_string ());
 			entry.put ("hash", block->hash ().to_string ());
 			entry.put ("contents", contents);
 			unchecked.push_back (std::make_pair ("", entry));
@@ -2867,7 +2867,7 @@ void rai::rpc_handler::wallet_info ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			balance = balance + node.ledger.account_balance (transaction, account);
 			pending = pending + node.ledger.account_pending (transaction, account);
 			rai::key_type key_type (wallet->store.key_type (i->second));
@@ -2902,7 +2902,7 @@ void rai::rpc_handler::wallet_balances ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			rai::uint128_t balance = node.ledger.account_balance (transaction, account);
 			if (balance >= threshold.number ())
 			{
@@ -3032,7 +3032,7 @@ void rai::rpc_handler::wallet_frontiers ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			auto latest (node.ledger.latest (transaction, account));
 			if (!latest.is_zero ())
 			{
@@ -3074,7 +3074,7 @@ void rai::rpc_handler::wallet_ledger ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			rai::account_info info;
 			if (!node.store.account_get (transaction, account, info))
 			{
@@ -3142,7 +3142,7 @@ void rai::rpc_handler::wallet_pending ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			boost::property_tree::ptree peers_l;
 			rai::account end (account.number () + 1);
 			for (auto ii (node.store.pending_begin (transaction, rai::pending_key (account, 0))), nn (node.store.pending_begin (transaction, rai::pending_key (end, 0))); ii != nn && peers_l.size () < count; ++ii)
@@ -3239,7 +3239,7 @@ void rai::rpc_handler::wallet_republish ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			auto latest (node.ledger.latest (transaction, account));
 			std::unique_ptr<rai::block> block;
 			std::vector<rai::block_hash> hashes;
@@ -3274,7 +3274,7 @@ void rai::rpc_handler::wallet_work_get ()
 		rai::transaction transaction (node.store.environment, nullptr, false);
 		for (auto i (wallet->store.begin (transaction)), n (wallet->store.end ()); i != n; ++i)
 		{
-			rai::account account (i->first.uint256 ());
+			rai::account account (i->first);
 			uint64_t work (0);
 			auto error_work (wallet->store.work_get (transaction, account, work));
 			works.put (account.to_account (), rai::to_string_hex (work));