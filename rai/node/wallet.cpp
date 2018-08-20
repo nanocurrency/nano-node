a87eb91ab6060804f77c159366f8452d2901bdd2
SergiySW <sergiysw@gmail.com>
2018-08-20 18:44:30 +0300

    Fix possible write transaction lock (#1079)

--- rai/node/wallet.cpp
+++ rai/node/wallet.cpp
@@ -762,7 +762,6 @@ node (node_a)
 
 void rai::wallet::enter_initial_password ()
 {
-	rai::transaction transaction (store.environment, nullptr, true);
 	std::lock_guard<std::recursive_mutex> lock (store.mutex);
 	rai::raw_key password_l;
 	store.password.value (password_l);
@@ -771,6 +770,7 @@ void rai::wallet::enter_initial_password ()
 		if (valid_password ())
 		{
 			// Newly created wallets have a zero key
+			rai::transaction transaction (store.environment, nullptr, true);
 			store.rekey (transaction, "");
 		}
 		enter_password ("");
