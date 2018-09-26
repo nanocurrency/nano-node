a9e53e14302d93e71c65ffdd8eab8bce4a80ea4f
Roy Keene <rkeene@users.noreply.github.com>
2018-09-26 15:30:52 -0500

    Add a 0ms to 50ms random delay
    Add a 0ms to 50ms random delay when scheduling more nodes to broadcast to.
    
    This should help stochastically balance this traffic.

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -5,6 +5,7 @@
 #include <rai/node/common.hpp>
 #include <rai/node/rpc.hpp>
 
+#include <cstdlib>
 #include <algorithm>
 #include <future>
 #include <sstream>
@@ -363,6 +364,7 @@ void rai::network::broadcast_confirm_req_base (std::shared_ptr<rai::block> block
 	if (!endpoints_a->empty ())
 	{
 		delay_a += 50;
+		delay_a += std::rand() % 30;
 
 		if (node.config.logging.network_logging ())
 		{
