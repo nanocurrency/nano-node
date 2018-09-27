nanocurrency/nano-node

commit 1915a7b210dd88a0cf5534c91813966b8d7b46f9

Fix max vote sequence comparison (#964)

rai/node/node.cpp
@@ -1341,7 +1341,7 @@ rai::vote_code rai::vote_processor::vote_blocking (rai::transaction const & tran
 	{
 		result = rai::vote_code::replay;
 		auto max_vote (node.store.vote_max (transaction_a, vote_a));
-		if (!node.active.vote (vote_a) || max_vote->sequence > vote_a->sequence)
+		if (!node.active.vote (vote_a) || max_vote->sequence < vote_a->sequence)
 		{
 			result = rai::vote_code::vote;
 		}

commit cb7f3f0cbd5e1ba32fc2343465d5c76742a0a371

Fix vote check (#1233)
This was broken in #964

rai/node/node.cpp
@@ -1404,9 +1404,9 @@ rai::vote_code rai::vote_processor::vote_blocking (rai::transaction const & tran
 	auto result (rai::vote_code::invalid);
 	if (!vote_a->validate ())
 	{
-		result = rai::vote_code::replay;
 		auto max_vote (node.store.vote_max (transaction_a, vote_a));
-		if (!node.active.vote (vote_a) || max_vote->sequence < vote_a->sequence)
+		result = rai::vote_code::replay;
+		if (!node.active.vote (vote_a))
 		{
 			result = rai::vote_code::vote;
 		}
@@ -1428,7 +1428,9 @@ rai::vote_code rai::vote_processor::vote_blocking (rai::transaction const & tran
 					}
 					node.network.confirm_send (confirm, bytes, endpoint_a);
 				}
+				break;
 			case rai::vote_code::invalid:
+				assert (false);
 				break;
 		}
 	}