57925aa243c7881c1c2a5ba588949365f3440951
Roy Keene <github@rkeene.org>
2018-10-01 09:20:54 -0500

    Update build_prep scripts

--- util/build_prep/ubuntu/prep.sh
+++ util/build_prep/ubuntu/prep.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
 
 set -euo pipefail
 
-apt-get update
-apt-get --yes --force-yes install git cmake ninja-build autotools-dev \
+apt-get update --yes
+apt-get --yes install git cmake ninja-build autotools-dev \
             build-essential g++ clang python-dev \
-            libicu-dev libbz2-dev libboost-all-dev \
+            libicu-dev libbz2-dev \
             locales wget curl apt-utils \
             lsb-release
-apt-get --yes --force-yes install xorg xvfb xauth xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
-apt-get --yes --force-yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+apt-get --yes install xorg xvfb xauth xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
+apt-get --yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+apt remove --yes libboost-all-dev
+apt autoremove --yes
+
+# XXX:TODO: Use common.sh
+boost_dir=/usr/local/boost
+
+echo "All verified."
+echo ""
+echo "Next steps:"
+echo "    cmake -DBOOST_ROOT=${boost_dir} -DRAIBLOCKS_GUI=ON <dir>"
+echo "    cpack -G \"TBZ2\""
+
+exit 0
