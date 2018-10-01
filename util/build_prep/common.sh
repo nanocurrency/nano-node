aeb2ca1c0659e86f937b314a60cf29e62a52019f
Roy Keene <github@rkeene.org>
2018-10-01 09:41:18 -0500

    Added script to separate out the common parts

--- util/build_prep/common.sh
+++ util/build_prep/common.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/env bash
-
-# -----BEGIN COMMON.SH-----
 function boost_version () {
 	local boost_version
 	boost_version="$(
@@ -76,30 +73,3 @@ function version_min () {
 
 	return 0
 }
-# -----END COMMON.SH-----
-
-export DEBIAN_FRONTEND=noninteractive
-
-set -euo pipefail
-
-apt-get update --yes
-apt-get --yes install git cmake ninja-build autotools-dev \
-            build-essential g++ clang python-dev \
-            libicu-dev libbz2-dev \
-            locales wget curl apt-utils \
-            lsb-release
-apt-get --yes install xorg xvfb xauth xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
-apt-get --yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-apt remove --yes libboost-all-dev
-apt autoremove --yes
-
-# XXX:TODO: Use common.sh
-boost_dir=/usr/local/boost
-
-echo "All verified."
-echo ""
-echo "Next steps:"
-echo "    cmake -DBOOST_ROOT=${boost_dir} -DRAIBLOCKS_GUI=ON <dir>"
-echo "    cpack -G \"TBZ2\""
-
-exit 0
