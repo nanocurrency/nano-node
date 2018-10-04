66495375f2783464f5c70b91bb6cb969d28b0a5d
Roy Keene <github@rkeene.org>
2018-10-04 11:32:12 -0500

    Moved things around to account for more implementations

--- rai/lib/plat/freebsd/thread_role.cpp
+++ rai/lib/plat/freebsd/thread_role.cpp
@@ -1,8 +1,9 @@
 #include <pthread.h>
+#include <pthread_np.h>
 #include <rai/lib/utility.hpp>
 
 void rai::thread_role::set_name (std::string thread_name)
 {
-	pthread_setname_np (pthread_self (), thread_name.c_str ());
+	pthread_set_name_np (pthread_self (), thread_name.c_str ());
 	return;
 }
--- rai/lib/plat/windows/thread_role.cpp
+++ rai/lib/plat/windows/thread_role.cpp
@@ -1,8 +1,6 @@
-#include <pthread.h>
 #include <rai/lib/utility.hpp>
 
 void rai::thread_role::set_name (std::string thread_name)
 {
-	pthread_setname_np (pthread_self (), thread_name.c_str ());
 	return;
 }
