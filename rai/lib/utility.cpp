f634604c01aa121568f3dbdd186054f40d0cc46d
Roy Keene <github@rkeene.org>
2018-10-04 10:53:15 -0500

    More formatting

--- rai/lib/utility.cpp
+++ rai/lib/utility.cpp
@@ -3,21 +3,31 @@
 
 namespace rai
 {
-	namespace thread_role
-	{
-		static thread_local rai::thread_role::name current_thread_role = rai::thread_role::name::unknown;
-		rai::thread_role::name get (void)
-		{
-			return current_thread_role;
-		}
+namespace thread_role
+{
 
-		void set (rai::thread_role::name role)
-		{
-			rai::thread_role::current_thread_role = role;
-		}
-	}
+/*
+ * rai::thread_role namespace
+ *
+ * Manage thread role
+ */
+static thread_local rai::thread_role::name current_thread_role = rai::thread_role::name::unknown;
+rai::thread_role::name get (void)
+{
+	return current_thread_role;
 }
 
+void set (rai::thread_role::name role)
+{
+	rai::thread_role::current_thread_role = role;
+}
+
+}
+}
+
+/*
+ * Backing code for "release_assert", which is itself a macro
+ */
 void release_assert_internal (bool check, const char * check_expr, const char * file, unsigned int line)
 {
 	if (check)
--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -49,7 +49,7 @@ on (true)
 	for (size_t i = 0; i < node.config.io_threads; ++i)
 	{
 		packet_processing_threads.push_back (std::thread ([this]() {
-			rai::thread_role::set(rai::thread_role::name::io);
+			rai::thread_role::set (rai::thread_role::name::io);
 			try
 			{
 				process_packets ();
