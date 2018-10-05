adbb5a24401884ed7b709371a801fdb251764a98
SergiySW <sergiysw@gmail.com>
2018-10-05 17:56:33 +0300

    Remove processing blocks from confirm_req (#1246)

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -490,12 +490,15 @@ public:
 		node.stats.inc (rai::stat::type::message, rai::stat::detail::confirm_req, rai::stat::dir::in);
 		node.peers.contacted (sender, message_a.header.version_using);
 		node.process_active (message_a.block);
-		node.active.publish (message_a.block);
-		auto transaction_a (node.store.tx_begin_read ());
-		auto successor (node.ledger.successor (transaction_a, message_a.block->root ()));
-		if (successor != nullptr)
+		// Don't load nodes with disabled voting
+		if (node.config.enable_voting)
 		{
-			confirm_block (transaction_a, node, sender, std::move (successor));
+			auto transaction (node.store.tx_begin_read ());
+			auto successor (node.ledger.successor (transaction, message_a.block->root ()));
+			if (successor != nullptr)
+			{
+				confirm_block (transaction, node, sender, std::move (successor));
+			}
 		}
 	}
 	void confirm_ack (rai::confirm_ack const & message_a) override
