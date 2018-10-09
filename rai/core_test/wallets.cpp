ffa895c02214261d5a70dfba6aa38d9c734a0bd8
SergiySW <sergiysw@gmail.com>
2018-10-10 02:25:03 +0300

    Test for vote_minimum

--- rai/core_test/wallets.cpp
+++ rai/core_test/wallets.cpp
@@ -95,3 +95,31 @@ TEST (wallets, DISABLED_wallet_create_max)
 	auto existing = wallets.items.find (key.pub);
 	ASSERT_TRUE (existing == wallets.items.end ());
 }
+
+TEST (wallets, vote_minimum)
+{
+	rai::system system (24000, 1);
+	auto & node1 (*system.nodes[0]);
+	bool error (false);
+	rai::wallets wallets (error, node1);
+	ASSERT_FALSE (error);
+	rai::keypair key1;
+	rai::keypair key2;
+	rai::genesis genesis;
+	rai::state_block send1 (rai::test_genesis_key.pub, genesis.hash (), rai::test_genesis_key.pub, std::numeric_limits<rai::uint128_t>::max () - node1.config.vote_minimum.number (), key1.pub, rai::test_genesis_key.prv, rai::test_genesis_key.pub, system.work.generate (genesis.hash ()));
+	ASSERT_EQ (rai::process_result::progress, node1.process (send1).code);
+	rai::state_block open1 (key1.pub, 0, key1.pub, node1.config.vote_minimum.number (), send1.hash (), key1.prv, key1.pub, system.work.generate (key1.pub));
+	ASSERT_EQ (rai::process_result::progress, node1.process (open1).code);
+	// send2 with amount vote_minimum - 1 (not voting representative)
+	rai::state_block send2 (rai::test_genesis_key.pub, send1.hash (), rai::test_genesis_key.pub, std::numeric_limits<rai::uint128_t>::max () - 2 * node1.config.vote_minimum.number () + 1, key2.pub, rai::test_genesis_key.prv, rai::test_genesis_key.pub, system.work.generate (send1.hash ()));
+	ASSERT_EQ (rai::process_result::progress, node1.process (send2).code);
+	rai::state_block open2 (key2.pub, 0, key2.pub, node1.config.vote_minimum.number () - 1, send2.hash (), key2.prv, key2.pub, system.work.generate (key2.pub));
+	ASSERT_EQ (rai::process_result::progress, node1.process (open2).code);
+	auto wallet (wallets.items.begin ()->second);
+	ASSERT_EQ (0, wallet->representatives.size ());
+	wallet->insert_adhoc (rai::test_genesis_key.prv);
+	wallet->insert_adhoc (key1.prv);
+	wallet->insert_adhoc (key2.prv);
+	wallets.compute_reps ();
+	ASSERT_EQ (2, wallet->representatives.size ());
+}
