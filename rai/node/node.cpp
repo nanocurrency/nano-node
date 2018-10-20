234d275961feb01a98c8558f1fe309a88a722169
Cathal Tummon <cstummon@hotmail.com>
2018-10-20 17:41:57 +0100

    Revert cast change.

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -1480,7 +1480,7 @@ stats (config.stat_config)
 	peers.online_weight_minimum = config.online_weight_minimum.number ();
 	if (rai::rai_network == rai::rai_networks::rai_live_network || rai::rai_network == rai::rai_networks::rai_beta_network)
 	{
-		rai::bufferstream weight_stream (static_cast<uint8_t *> (rai_bootstrap_weights), rai_bootstrap_weights_size);
+		rai::bufferstream weight_stream ((const uint8_t *)rai_bootstrap_weights, rai_bootstrap_weights_size);
 		rai::uint128_union block_height;
 		if (!rai::read (weight_stream, block_height))
 		{
