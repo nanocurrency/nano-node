8b1bb98d71d70fc0ae3c4f95915d4eacd82f4dc6
cryptocode <cryptocode@zolo.io>
2018-10-27 12:27:02 +0200

    Log upnp devices only if configured to do so

--- rai/node/logging.hpp
+++ rai/node/logging.hpp
@@ -28,6 +28,7 @@ public:
 	bool network_node_id_handshake_logging () const;
 	bool node_lifetime_tracing () const;
 	bool insufficient_work_logging () const;
+	bool upnp_details_logging () const;
 	bool log_rpc () const;
 	bool bulk_pull_logging () const;
 	bool callback_logging () const;
@@ -49,6 +50,7 @@ public:
 	bool log_rpc_value;
 	bool bulk_pull_logging_value;
 	bool work_generation_time_value;
+	bool upnp_details_logging_value;
 	bool log_to_cerr_value;
 	bool flush;
 	uintmax_t max_size;
