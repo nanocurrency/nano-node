62e896f085c4249783c50147e884425d38f1e0a9
Roy Keene <rkeene@users.noreply.github.com>
2018-11-09 15:18:57 -0600

    Updated wording

--- ed25519-donna/ed25519.c
+++ ed25519-donna/ed25519.c
@@ -66,13 +66,13 @@ ED25519_FN(ed25519_sign) (const unsigned char *m, size_t mlen, const ed25519_sec
 
 	ed25519_extsk(extsk, sk);
 
-	/* r = H(aExt[32..64], randr[0..31], zero[0..31], m) */
+	/* r = H(aExt[32..64], randr[0..31], zero[0..63], m) */
 	ed25519_hash_init(&ctx);
 	ed25519_hash_update(&ctx, extsk + 32, 32);
 	ed25519_randombytes_unsafe(randr, 32);
 	ed25519_hash_update(&ctx, randr, 32);
 	/*
-	 * Fill up the rest of the hash block.
+	 * Pad with zeros the rest of the hash block.
 	 * This puts the message (possibly known to a side
 	 * channel attacker) in a separate block.
 	 */
