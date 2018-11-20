6e8cb66e9d68ecbdbdad9d97fb69d7d24a12a748
Roy Keene <rkeene@users.noreply.github.com>
2018-11-20 15:39:03 -0600

    Update the version

--- rai/node/nodeconfig.hpp
+++ rai/node/nodeconfig.hpp
@@ -49,6 +49,6 @@ public:
 	static std::chrono::seconds constexpr keepalive_period = std::chrono::seconds (60);
 	static std::chrono::seconds constexpr keepalive_cutoff = keepalive_period * 5;
 	static std::chrono::minutes constexpr wallet_backup_interval = std::chrono::minutes (5);
-	static constexpr int json_version = 15;
+	static constexpr int json_version = 16;
 };
 }
