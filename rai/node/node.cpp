12b397eb028c8fadb9a4e0d82f8c84dc0d49baf1
SergiySW <sergiysw@gmail.com>
2018-12-07 02:37:38 +0300

    Lazy & legacy bootstrap can be disabled at once

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -1857,14 +1857,14 @@ void rai::gap_cache::vote (std::shared_ptr<rai::vote> vote_a)
 					tally += node.ledger.weight (transaction, voter);
 				}
 				bool start_bootstrap (false);
-				if (!node.flags.disable_lazy_bootstrap || node.flags.disable_legacy_bootstrap)
+				if (!node.flags.disable_lazy_bootstrap)
 				{
 					if (tally >= node.config.online_weight_minimum.number ())
 					{
 						start_bootstrap = true;
 					}
 				}
-				else if (tally > bootstrap_threshold (transaction))
+				else if (!node.flags.disable_legacy_bootstrap && tally > bootstrap_threshold (transaction))
 				{
 					start_bootstrap = true;
 				}
@@ -1880,11 +1880,11 @@ void rai::gap_cache::vote (std::shared_ptr<rai::vote> vote_a)
 							{
 								BOOST_LOG (node_l->log) << boost::str (boost::format ("Missing block %1% which has enough votes to warrant lazy bootstrapping it") % hash.to_string ());
 							}
-							if (!node_l->flags.disable_lazy_bootstrap || node_l->flags.disable_legacy_bootstrap)
+							if (!node_l->flags.disable_lazy_bootstrap)
 							{
 								node_l->bootstrap_initiator.bootstrap_lazy (hash);
 							}
-							else
+							else if (!node_l->flags.disable_legacy_bootstrap)
 							{
 								node_l->bootstrap_initiator.bootstrap ();
 							}
