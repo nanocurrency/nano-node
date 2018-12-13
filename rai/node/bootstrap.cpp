80fea28f730065e2b8ca696bb5f9279f9161966e
cryptocode <cryptocode@zolo.io>
2018-12-13 01:04:17 +0100

    Close sockets gracefully

--- rai/node/bootstrap.cpp
+++ rai/node/bootstrap.cpp
@@ -83,6 +83,14 @@ void rai::socket::close ()
 {
 	if (socket_m.is_open ())
 	{
+		try
+		{
+			socket_m.shutdown (boost::asio::ip::tcp::socket::shutdown_both);
+		}
+		catch (...)
+		{
+			/* Ignore spurious exceptions; shutdown is best effort. */
+		}
 		socket_m.close ();
 	}
 }
