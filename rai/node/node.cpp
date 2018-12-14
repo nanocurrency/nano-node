653638f1685a773dc7bb807b3ecf25a05cebbf30
Sergey Kroshnin <sergiysw@gmail.com>
2018-12-14 22:13:28 +0300

    Add max_broadcast_queue for rai::active_transactions (#1440)

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -23,6 +23,7 @@ std::chrono::seconds constexpr rai::node::search_pending_interval;
 int constexpr rai::port_mapping::mapping_timeout;
 int constexpr rai::port_mapping::check_timeout;
 unsigned constexpr rai::active_transactions::announce_interval_ms;
+size_t constexpr rai::active_transactions::max_broadcast_queue;
 size_t constexpr rai::block_arrival::arrival_size_min;
 std::chrono::seconds constexpr rai::block_arrival::arrival_time_min;
 
@@ -376,8 +377,7 @@ void rai::network::broadcast_confirm_req_base (std::shared_ptr<rai::block> block
 	}
 	if (!endpoints_a->empty ())
 	{
-		delay_a += 50;
-		delay_a += std::rand () % 50;
+		delay_a += std::rand () % broadcast_interval_ms;
 
 		std::weak_ptr<rai::node> node_w (node.shared ());
 		node.alarm.add (std::chrono::steady_clock::now () + std::chrono::milliseconds (delay_a), [node_w, block_a, endpoints_a, delay_a]() {
@@ -3141,7 +3141,10 @@ void rai::active_transactions::announce_votes (std::unique_lock<std::mutex> & lo
 				if (node.ledger.could_fit (transaction, *election_l->status.winner))
 				{
 					// Broadcast winner
-					rebroadcast_bundle.push_back (election_l->status.winner);
+					if (rebroadcast_bundle.size () < max_broadcast_queue)
+					{
+						rebroadcast_bundle.push_back (election_l->status.winner);
+					}
 				}
 				else
 				{
@@ -3181,9 +3184,12 @@ void rai::active_transactions::announce_votes (std::unique_lock<std::mutex> & lo
 						}
 					}
 				}
-				if ((!reps->empty () && total_weight > node.config.online_weight_minimum.number ()) || roots.size () > 5)
+				if ((!reps->empty () && total_weight > node.config.online_weight_minimum.number ()) || roots_size > 5)
 				{
-					confirm_req_bundle.push_back (std::make_pair (i->election->status.winner, reps));
+					if (confirm_req_bundle.size () < max_broadcast_queue)
+					{
+						confirm_req_bundle.push_back (std::make_pair (i->election->status.winner, reps));
+					}
 				}
 				else
 				{
@@ -3238,7 +3244,7 @@ void rai::active_transactions::announce_loop ()
 	while (!stopped)
 	{
 		announce_votes (lock);
-		condition.wait_for (lock, std::chrono::milliseconds (announce_interval_ms + roots.size () * node.network.broadcast_interval_ms * 3 / 2));
+		condition.wait_for (lock, std::chrono::milliseconds (announce_interval_ms + (std::min (roots.size (), max_broadcast_queue) * node.network.broadcast_interval_ms * 2)));
 	}
 }
 
