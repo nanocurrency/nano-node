f861ff4c0a67f02ef58660d602e7510be78099df
clemahieu <clemahieu@gmail.com>
2018-12-28 18:21:29 +0100

    Fix a possible thread stall resulting in processing delay or OS udp buffer overflow. (#1494)

--- rai/node/node.cpp
+++ rai/node/node.cpp
@@ -3542,7 +3542,7 @@ void rai::udp_buffer::enqueue (rai::udp_data * data_a)
 	assert (data_a != nullptr);
 	std::lock_guard<std::mutex> lock (mutex);
 	full.push_back (data_a);
-	condition.notify_one ();
+	condition.notify_all ();
 }
 rai::udp_data * rai::udp_buffer::dequeue ()
 {
@@ -3564,7 +3564,7 @@ void rai::udp_buffer::release (rai::udp_data * data_a)
 	assert (data_a != nullptr);
 	std::lock_guard<std::mutex> lock (mutex);
 	free.push_back (data_a);
-	condition.notify_one ();
+	condition.notify_all ();
 }
 void rai::udp_buffer::stop ()
 {
