2eda5179fe20c2b02dd65b22b2daa48fa49d6c47
Roy Keene <rkeene@users.noreply.github.com>
2018-12-28 14:58:50 -0600

    Rename internal project bits from RaiBlocks to Nano (#1504)

---
+++ nano/node/xorshift.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <array>
+
+namespace nano
+{
+class xorshift1024star
+{
+public:
+	xorshift1024star () :
+	p (0)
+	{
+	}
+	std::array<uint64_t, 16> s;
+	unsigned p;
+	uint64_t next ()
+	{
+		auto p_l (p);
+		auto pn ((p_l + 1) & 15);
+		p = pn;
+		uint64_t s0 = s[p_l];
+		uint64_t s1 = s[pn];
+		s1 ^= s1 << 31; // a
+		s1 ^= s1 >> 11; // b
+		s0 ^= s0 >> 30; // c
+		return (s[pn] = s0 ^ s1) * 1181783497276652981LL;
+	}
+};
+}
