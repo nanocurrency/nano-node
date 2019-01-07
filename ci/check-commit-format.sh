2148b74b344d83442d5653beaabc38f83939dd5e
SergiySW <sergiysw@gmail.com>
2019-01-08 01:10:04 +0300

    Temporary excluding external C libraries check

--- ci/check-commit-format.sh
+++ ci/check-commit-format.sh
@@ -6,7 +6,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 RESULT=`python $REPO_ROOT/ci/git-clang-format.py --diff -f --commit HEAD~1 --extensions "h,hpp,cpp"`
 if [ "$RESULT" != "no modified files to format" ] && [ "$RESULT" != "clang-format did not modify any files" ]; then
-    python $REPO_ROOT/ci/git-clang-format.py --diff -f --commit HEAD~1
+    python $REPO_ROOT/ci/git-clang-format.py --diff -f --commit HEAD~1 --extensions "hpp,cpp"
     echo
     echo "Code formatting differs from expected - please run ci/clang-format-all.sh"
     exit 1
