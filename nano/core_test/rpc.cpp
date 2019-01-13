3cca1736a7e9e1ef922481cb66576e989ed7ef68
SergiySW <sergiysw@gmail.com>
2019-01-14 01:32:53 +0300

    Remove commented out line

--- nano/core_test/rpc.cpp
+++ nano/core_test/rpc.cpp
@@ -3805,7 +3805,6 @@ TEST (rpc, online_reps)
 	nano::system system (24000, 2);
 	system.wallet (0)->insert_adhoc (nano::test_genesis_key.prv);
 	ASSERT_TRUE (system.nodes[1]->online_reps.online_stake () == system.nodes[1]->config.online_weight_minimum.number ());
-	//system.wallet (0)->send_action (nano::test_genesis_key.pub, nano::test_genesis_key.pub, nano::Gxrb_ratio);
 	auto send_block (system.wallet (0)->send_action (nano::test_genesis_key.pub, nano::test_genesis_key.pub, nano::Gxrb_ratio));
 	ASSERT_NE (nullptr, send_block);
 	system.deadline_set (10s);
