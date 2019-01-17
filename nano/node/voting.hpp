38e40bc92ea3a30f05302dd7b46848bb77dfbf70
SergiySW <sergiysw@gmail.com>
2019-01-18 01:54:49 +0300

    Formatting

--- nano/node/voting.hpp
+++ nano/node/voting.hpp
@@ -3,7 +3,6 @@
 #include <nano/lib/numbers.hpp>
 #include <nano/secure/common.hpp>
 
-
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
