f00288a6c96b764dd9a1f6bebbef26fa4d284726
Russel Waters <vaelstrom@gmail.com>
2019-02-01 08:14:23 -0500

    Revert "Initial Travis Windows support (#1644)"
    This reverts commit 00dd12a9f9c7e6346ecc3ed69026aaa6848ff268.

--- util/build_prep/windows/prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/platform/boost/lib
-nuget install ./util/build_prep/windows/packages.config -ExcludeVersion -OutputDirectory ~/tmp
-cp ~/tmp/boost_*/lib/native/libboost*.lib ~/platform/boost/lib
-mv ~/tmp/boost/lib/native/include/* ~/platform/boost/
