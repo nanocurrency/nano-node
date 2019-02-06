48ee9426448ebc67900d4880fa2aae9bf5ac31e4
SergiySW <sergiysw@gmail.com>
2019-02-06 16:51:51 +0300

    Revert "Make network endpoint static"
    This reverts commit daf523bd589840773633940513428dd6bf36426b.

--- nano/node/node.cpp
+++ nano/node/node.cpp
@@ -43,12 +43,11 @@ extern size_t nano_bootstrap_weights_size;
 nano::network::network (nano::node & node_a, uint16_t port) :
 buffer_container (node_a.stats, nano::network::buffer_size, 4096), // 2Mb receive buffer
 socket (node_a.io_ctx, nano::endpoint (boost::asio::ip::address_v6::any (), port)),
-endpoint (nano::endpoint (boost::asio::ip::address_v6::loopback (), port)),
+endpoint (local_endpoint ()),
 resolver (node_a.io_ctx),
 node (node_a),
 on (true)
 {
-	assert (local_endpoint () == endpoint);
 	boost::thread::attributes attrs;
 	nano::thread_attributes::set (attrs);
 	for (size_t i = 0; i < node.config.network_threads; ++i)
@@ -2703,7 +2702,6 @@ void nano::node::process_message (nano::message & message_a, nano::endpoint cons
 
 nano::endpoint nano::network::local_endpoint ()
 {
-	std::unique_lock<std::mutex> lock (socket_mutex);
 	boost::system::error_code ec;
 	auto port (socket.local_endpoint (ec).port ());
 	if (ec)
