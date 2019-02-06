044ccb5a2de7f317074f2cdb31ec2b21e67d9f14
Cathal Tummon <cstummon@hotmail.com>
2019-02-06 20:24:22 +0000

    Add default param for history visitor

--- nano/node/rpc.cpp
+++ nano/node/rpc.cpp
@@ -1739,7 +1739,7 @@ namespace
 class history_visitor : public nano::block_visitor
 {
 public:
-	history_visitor (nano::rpc_handler & handler_a, bool raw_a, nano::transaction & transaction_a, boost::property_tree::ptree & tree_a, nano::block_hash const & hash_a, std::vector<nano::public_key> & accounts_filter_a) :
+    history_visitor(nano::rpc_handler & handler_a, bool raw_a, nano::transaction & transaction_a, boost::property_tree::ptree & tree_a, nano::block_hash const & hash_a, std::vector<nano::public_key> & accounts_filter_a = {}) :
 	handler (handler_a),
 	raw (raw_a),
 	transaction (transaction_a),
