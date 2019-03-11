9c185c82c26bc407afcccd51f5a0365296bba79b
Russel Waters <russel@nano.org>
2019-03-11 13:28:34 -0400

    missed instances of nano::work_pool::publish_threshold

--- nano/node/rpc.cpp
+++ nano/node/rpc.cpp
@@ -4248,7 +4248,7 @@ void nano::rpc_handler::work_validate ()
 {
 	auto hash (hash_impl ());
 	auto work (work_optional_impl ());
-	uint64_t difficulty (nano::work_pool::publish_threshold);
+	uint64_t difficulty (node.network_params.publish_threshold);
 	boost::optional<std::string> difficulty_text (request.get_optional<std::string> ("difficulty"));
 	if (!ec && difficulty_text.is_initialized ())
 	{
