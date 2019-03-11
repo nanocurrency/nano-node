588a2d2166ce1d5253032a43468a32e5b5255610
SergiySW <sergiysw@gmail.com>
2019-03-12 00:27:03 +0300

    Clang formatting

--- nano/lib/debug.cpp
+++ nano/lib/debug.cpp
@@ -7,16 +7,17 @@ std::ostream & nano::debug::internal (nano::debug::subsystem subsystem, nano::de
 {
 	static thread_local uint64_t debug_thread_id = -1;
 	static std::atomic<uint64_t> debug_thread_id_counter{ 0 };
-	std::ostream *out_stream;
+	std::ostream * out_stream;
 
 	out_stream = &std::cerr;
 
-	if (debug_thread_id == -1) {
-		debug_thread_id = debug_thread_id_counter.fetch_add(1, std::memory_order_relaxed);
+	if (debug_thread_id == -1)
+	{
+		debug_thread_id = debug_thread_id_counter.fetch_add (1, std::memory_order_relaxed);
 	}
 
 	/* XXX:TODO: Move this boost log */
-	(*out_stream) << "[DEBUG] " << time(NULL) << " [Thread#" << debug_thread_id << "] " << function << ":" << line << "/" << ((int) subsystem) << "." << ((int) level) << ": ";
+	(*out_stream) << "[DEBUG] " << time (NULL) << " [Thread#" << debug_thread_id << "] " << function << ":" << line << "/" << ((int)subsystem) << "." << ((int)level) << ": ";
 
 	return (*out_stream);
 }
--- nano/lib/debug.hpp
+++ nano/lib/debug.hpp
@@ -5,30 +5,34 @@
 #endif
 
 namespace nano {
-	namespace debug {
-		enum class level {
-			trace   = 0, /* Trace message, entered/exited a function or branch */
-			comment = 1, /* Comment about section of code */
-			debug   = 2, /* Helpful messages for debuggers */
-			note    = 3  /* Helpful notes for high-level status */
-		};
+namespace debug {
+	enum class level {
+		trace   = 0, /* Trace message, entered/exited a function or branch */
+		comment = 1, /* Comment about section of code */
+		debug   = 2, /* Helpful messages for debuggers */
+		note    = 3  /* Helpful notes for high-level status */
+	};
 
-		enum class subsystem {
-			unknown,
-			ledger,
-			vote,
-			network,
-			bootstrap
-		};
+	enum class subsystem {
+		unknown,
+		ledger,
+		vote,
+		network,
+		bootstrap
+	};
 
-		std::ostream & internal (nano::debug::subsystem subsystem, nano::debug::level level, const char * function, int line);
-	}
+	std::ostream & internal (nano::debug::subsystem subsystem, nano::debug::level level, const char * function, int line);
+}
 }
 
 #ifndef NDEBUG
-#define nano_debug (levelName, ostreamInput) { nano::debug::internal (nano::debug::subsystem::NANO_DEBUG_SUBSYSTEM, nano::debug::level::levelName, __func__, __LINE__) << ostreamInput << std::endl; }
+#define nano_debug                                                                                                                                            \
+	(levelName, ostreamInput)                                                                                                                                 \
+	{                                                                                                                                                         \
+		nano::debug::internal (nano::debug::subsystem::NANO_DEBUG_SUBSYSTEM, nano::debug::level::levelName, __func__, __LINE__) << ostreamInput << std::endl; \
+	}
 #else
 #define nano_debug (levelName, ostreamInput) /**/
 #endif
-#define nano_debug_trace_enter nano_debug(trace, "Entered")
-#define nano_debug_trace_exit  nano_debug(trace, "Exit")
+#define nano_debug_trace_enter nano_debug (trace, "Entered")
+#define nano_debug_trace_exit  nano_debug (trace, "Exit")
