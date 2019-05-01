229d5c5f4342250c6c989e6aa743d55c27c71d42
wezrule <wezrule@hotmail.com>
2019-05-01 14:53:59 +0100

    Modify the minimum time for a lock held before outputting in the log file to 5 seconds (from 10)

--- nano/node/lmdb_txn_tracker.hpp
+++ nano/node/lmdb_txn_tracker.hpp
@@ -37,6 +37,6 @@ private:
 	nano::logger_mt & logger;
 	bool is_logging_database_locking;
 	void output_finished (nano::mdb_txn_stats & mdb_txn_stats);
-	static std::chrono::seconds constexpr min_time_locked_ouput{ 10 };
+	static std::chrono::seconds constexpr min_time_locked_ouput{ 5 };
 };
 }
