6563591314b22a0cbaf64c1b24f66f41902fd823
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-05-09 10:54:34 +0100

    Use a low value work for test_genesis_data

--- nano/secure/common.cpp
+++ nano/secure/common.cpp
@@ -38,7 +38,7 @@ char const * test_genesis_data = R"%%%({
 	"source": "B0311EA55708D6A53C75CDBF88300259C6D018522FE3D4D0A242E431F9E8B6D0",
 	"representative": "xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpiij4txtdo",
 	"account": "xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpiij4txtdo",
-	"work": "9680625b39d3363d",
+	"work": "7b42a00ee91d5810",
 	"signature": "ECDA914373A2F0CA1296475BAEE40500A7F0A7AD72A5A80C81D7FAB7F6C802B2CC7DB50F5DD0FB25B2EF11761FA7344A158DD5A700B21BD47DE5BD0F63153A02"
 	})%%%";
 
