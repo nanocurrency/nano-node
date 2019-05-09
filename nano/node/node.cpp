6ca069976668cdc3a4809f4b9595f59e5be57d96
Wesley Shillingford <wezrule@hotmail.com>
2019-05-09 15:20:35 +0100

    Makes sure a write lock can be avoided when launching an inactive_node (#1967)
    * Makes sure a write lock can be avoided when launching an inactive_node
    
    * Update test so it doesn't make other tests fail
    
    * Forgot to wait on write lock promise

--- nano/node/node.cpp
+++ nano/node/node.cpp
@@ -1237,13 +1237,23 @@ startup_time (std::chrono::steady_clock::now ())
 		{
 			logger.always_log ("Constructing node");
 		}
-		nano::genesis genesis;
-		auto transaction (store.tx_begin_write ());
-		if (store.latest_begin (transaction) == store.latest_end ())
+
+		// First do a pass with a read to see if any writing needs doing, this saves needing to open a write lock (and potentially blocking)
+		auto is_initialized (false);
 		{
+			auto transaction (store.tx_begin_read ());
+			is_initialized = (store.latest_begin (transaction) != store.latest_end ());
+		}
+
+		nano::genesis genesis;
+		if (!is_initialized)
+		{
+			auto transaction (store.tx_begin_write ());
 			// Store was empty meaning we just created it, add the genesis block
 			store.initialize (transaction, genesis);
 		}
+
+		auto transaction (store.tx_begin_read ());
 		if (!store.block_exists (transaction, genesis.hash ()))
 		{
 			logger.always_log ("Genesis block not found. Make sure the node network ID is correct.");
