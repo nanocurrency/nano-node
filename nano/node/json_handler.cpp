b0e7b902bcc9b4d9c40a2f036b8f5e0970fd87c3
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-05-13 21:07:34 +0100

    Use double for multiplier everywhere, no reason to lose precision

--- nano/node/json_handler.cpp
+++ nano/node/json_handler.cpp
@@ -838,7 +838,7 @@ void nano::json_handler::active_difficulty ()
 	response_l.put ("difficulty_threshold", nano::to_string_hex (node.network_params.network.publish_threshold));
 	auto difficulty_active = node.active.active_difficulty ();
 	response_l.put ("difficulty_active", nano::to_string_hex (difficulty_active));
-	float multiplier = nano::difficulty::to_multiplier (difficulty_active, node.network_params.network.publish_threshold);
+	auto multiplier = nano::difficulty::to_multiplier (difficulty_active, node.network_params.network.publish_threshold);
 	response_l.put ("multiplier", std::to_string (multiplier));
 	response_errors ();
 }
@@ -4453,7 +4453,7 @@ void nano::json_handler::work_validate ()
 		bool valid (!invalid && result_difficulty >= difficulty);
 		response_l.put ("valid", valid ? "1" : "0");
 		response_l.put ("value", nano::to_string_hex (result_difficulty));
-		float multiplier = nano::difficulty::to_multiplier (result_difficulty, node.network_params.network.publish_threshold);
+		auto multiplier = nano::difficulty::to_multiplier (result_difficulty, node.network_params.network.publish_threshold);
 		response_l.put ("multiplier", std::to_string (multiplier));
 	}
 	response_errors ();
