56caa6611830959074835cdb478acfcad96d62ad
Russel Waters <russel@nano.org>
2019-05-15 20:05:12 -0400

    Validate adjusted_difficulty ordering of roots container

--- nano/core_test/active_transactions.cpp
+++ nano/core_test/active_transactions.cpp
@@ -60,3 +60,80 @@ TEST (active_transactions, long_unconfirmed_size)
 	//only 2 should appear unconfirmed now
 	ASSERT_EQ (node1.active.long_unconfirmed_size (), 2);
 }
+
+TEST (active_transactions, adjusted_difficulty_priority)
+{
+	nano::system system (24000, 1);
+	auto & node1 (*system.nodes[0]);
+	auto & wallet (*system.wallet (0));
+	node1.config.enable_voting = false;
+	nano::genesis genesis;
+	nano::keypair key1, key2, key3;
+	auto transaction (node1.store.tx_begin_read());
+	
+	auto send1 (std::make_shared<nano::state_block> (nano::test_genesis_key.pub, genesis.hash(), nano::test_genesis_key.pub, nano::genesis_amount-10*nano::xrb_ratio, key1.pub, nano::test_genesis_key.prv, nano::test_genesis_key.pub, system.work.generate (genesis.hash())));
+	auto send2 (std::make_shared<nano::state_block> (nano::test_genesis_key.pub, send1->hash(), nano::test_genesis_key.pub, nano::genesis_amount-20*nano::xrb_ratio, key2.pub, nano::test_genesis_key.prv, nano::test_genesis_key.pub, system.work.generate (send1->hash())));
+	auto open1 (std::make_shared<nano::state_block> (key1.pub, 0, key1.pub, 10*nano::xrb_ratio, send1->hash (), key1.prv, key1.pub, system.work.generate (key1.pub)));
+	auto open2 (std::make_shared<nano::state_block> (key2.pub, 0, key2.pub, 10*nano::xrb_ratio, send2->hash (), key2.prv, key2.pub, system.work.generate (key2.pub)));
+	node1.process_active (send1);
+	node1.process_active (send2);
+	node1.process_active (open1);
+	node1.process_active (open2);
+	system.deadline_set (10s);
+	while (node1.active.size () != 4)
+	{
+		ASSERT_NO_ERROR (system.poll ());
+	}
+
+	while (node1.active.size () != 0)
+	{
+		std::lock_guard<std::mutex> active_guard (node1.active.mutex);
+		auto it(node1.active.roots.begin());
+		while (!node1.active.roots.empty() && it != node1.active.roots.end ())
+		{
+			auto election (it->election);
+			election->confirm_once();
+			it++;;
+		}
+	}
+
+	while (node1.active.confirmed.size() !=4)
+	{
+		ASSERT_NO_ERROR(system.poll());
+	}
+
+	//genesis and key1,key2 are opened
+	//start chain of 2 on each
+	auto send3 (std::make_shared<nano::state_block> (nano::test_genesis_key.pub, send2->hash(), nano::test_genesis_key.pub, 9*nano::xrb_ratio, key3.pub, nano::test_genesis_key.prv, nano::test_genesis_key.pub, system.work.generate (send2->hash(), nano::difficulty::from_multiplier(1500,node1.network_params.network.publish_threshold))));
+	auto send4 (std::make_shared<nano::state_block> (nano::test_genesis_key.pub, send3->hash(), nano::test_genesis_key.pub, 8*nano::xrb_ratio, key3.pub, nano::test_genesis_key.prv, nano::test_genesis_key.pub, system.work.generate (send3->hash(), nano::difficulty::from_multiplier(1500,node1.network_params.network.publish_threshold))));
+	auto send5 (std::make_shared<nano::state_block> (key1.pub, open1->hash(), key1.pub, 9*nano::xrb_ratio, key3.pub, key1.prv, key1.pub, system.work.generate (open1->hash(), nano::difficulty::from_multiplier(100,node1.network_params.network.publish_threshold))));
+	auto send6 (std::make_shared<nano::state_block> (key1.pub, send5->hash(), key1.pub, 8*nano::xrb_ratio, key3.pub, key1.prv, key1.pub, system.work.generate (send5->hash(), nano::difficulty::from_multiplier(100,node1.network_params.network.publish_threshold))));
+	auto send7 (std::make_shared<nano::state_block> (key2.pub, open2->hash(), key2.pub, 9*nano::xrb_ratio, key3.pub, key2.prv, key2.pub, system.work.generate (open2->hash(), nano::difficulty::from_multiplier(500,node1.network_params.network.publish_threshold))));
+	auto send8 (std::make_shared<nano::state_block> (key2.pub, send7->hash(), key2.pub, 8*nano::xrb_ratio, key3.pub, key2.prv, key2.pub, system.work.generate (send7->hash(), nano::difficulty::from_multiplier(500,node1.network_params.network.publish_threshold))));
+	
+	node1.process_active (send3); //genesis
+	node1.process_active (send5); //key1
+	node1.process_active (send7); //key2
+	node1.process_active (send4); //genesis
+	node1.process_active (send6); //key1
+	node1.process_active (send8); //key2
+
+	while (node1.active.size () != 6)
+	{
+		ASSERT_NO_ERROR (system.poll ());
+	}
+
+	{
+		std::lock_guard<std::mutex> lock (node1.active.mutex);
+		uint64_t last_adjusted(0);
+		for (auto i (node1.active.roots.get<1> ().begin ()), n (node1.active.roots.get<1> ().end ()); i != n; ++i)
+		{
+			//first root has nothing to compare
+			if(last_adjusted != 0)
+			{
+				ASSERT_LT (i->adjusted_difficulty, last_adjusted);
+			}
+			last_adjusted = i->adjusted_difficulty;
+		}
+	}
+}
