3d3f17ecaee5fa2a85d6a4136db0ee32af6ffd0f
wezrule <wezrule@hotmail.com>
2019-05-24 18:14:49 +0100

    Forgot to add it to add purging in rpc_test

--- nano/rpc_test/entry.cpp
+++ nano/rpc_test/entry.cpp
@@ -9,6 +9,7 @@ void force_nano_test_network ();
 int main (int argc, char ** argv)
 {
 	nano::force_nano_test_network ();
+	nano::block_memory_pool_cleanup_guard block_memory_pool_cleanup_guard;
 	testing::InitGoogleTest (&argc, argv);
 	auto res = RUN_ALL_TESTS ();
 	nano::cleanup_test_directories_on_exit ();
