594724be267b0709ce298e8a62db329bbf37a8f3
SergiySW <sergiysw@gmail.com>
2019-05-27 21:19:27 +0300

    Typo

--- nano/node/network.cpp
+++ nano/node/network.cpp
@@ -280,7 +280,7 @@ void nano::network::broadcast_confirm_req_base (std::shared_ptr<nano::block> blo
 	{
 		auto channel (endpoints_a->back ());
 		// Confirmation request with full block
-		if (node.network_params.network.is_live_network ()
+		if (node.network_params.network.is_live_network ())
 		{
 			nano::confirm_req req (block_a);
 			channel->send (req);
