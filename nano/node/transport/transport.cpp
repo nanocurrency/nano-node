416e2a7ce23dab804e69356cd63f0d24264c9c06
Russel Waters <russel@nano.org>
2019-05-28 13:01:26 -0400

    stuff under logging;packet logging

--- nano/node/transport/transport.cpp
+++ nano/node/transport/transport.cpp
@@ -87,7 +87,10 @@ void nano::transport::channel::send (nano::message const & message_a, std::funct
 	else
 	{
 		node.stats.inc (nano::stat::type::drop, detail, nano::stat::dir::out);
-		node.logger.always_log (boost::str (boost::format ("%1% of size %2% dropped") % node.stats.detail_raw_to_string (detail) % buffer->size ()));
+		if (node.config.logging.network_packet_logging == true)
+		{
+			node.logger.always_log (boost::str (boost::format ("%1% of size %2% dropped") % node.stats.detail_raw_to_string (detail) % buffer->size ()));
+		}
 	}
 }
 
