947dda26d3f853706ead9ca06adbb81edf11d917
cryptocode <stein@nano.org>
2019-06-11 19:38:55 +0200

    Fix ASAN issue in ws unit tests (#2074)
    * Fix ASAN issue in ws unit tests
    
    * Capture io context as well
    
    * Revert to using a local for ioctx

--- nano/core_test/websocket.cpp
+++ nano/core_test/websocket.cpp
@@ -39,19 +39,19 @@ boost::optional<std::string> websocket_test_call (std::string host, std::string 
 	boost::optional<std::string> ret;
 	boost::asio::io_context ioc;
 	boost::asio::ip::tcp::resolver resolver{ ioc };
-	boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ ioc };
+	auto ws (std::make_shared<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>> (ioc));
 
 	auto const results = resolver.resolve (host, port);
-	boost::asio::connect (ws.next_layer (), results.begin (), results.end ());
+	boost::asio::connect (ws->next_layer (), results.begin (), results.end ());
 
-	ws.handshake (host, "/");
-	ws.text (true);
-	ws.write (boost::asio::buffer (message_a));
+	ws->handshake (host, "/");
+	ws->text (true);
+	ws->write (boost::asio::buffer (message_a));
 
 	if (await_ack)
 	{
 		boost::beast::flat_buffer buffer;
-		ws.read (buffer);
+		ws->read (buffer);
 		ack_ready = true;
 	}
 
@@ -59,7 +59,7 @@ boost::optional<std::string> websocket_test_call (std::string host, std::string 
 	{
 		assert (response_deadline > 0s);
 		auto buffer (std::make_shared<boost::beast::flat_buffer> ());
-		ws.async_read (*buffer, [&ret, buffer](boost::beast::error_code const & ec, std::size_t const n) {
+		ws->async_read (*buffer, [&ret, ws, buffer](boost::beast::error_code const & ec, std::size_t const n) {
 			if (!ec)
 			{
 				std::ostringstream res;
@@ -70,9 +70,9 @@ boost::optional<std::string> websocket_test_call (std::string host, std::string 
 		ioc.run_one_for (response_deadline);
 	}
 
-	if (ws.is_open ())
+	if (ws->is_open ())
 	{
-		ws.async_close (boost::beast::websocket::close_code::normal, [](boost::beast::error_code const & ec) {
+		ws->async_close (boost::beast::websocket::close_code::normal, [ws](boost::beast::error_code const & ec) {
 			// A synchronous close usually hangs in tests when the server's io_context stops looping
 			// An async_close solves this problem
 		});
