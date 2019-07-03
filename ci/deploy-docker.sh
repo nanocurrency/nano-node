4599b43a5d7364fe9019594a0339ad5a035f6cd7
Russel Waters <russel@nano.org>
2019-07-03 12:17:56 -0400

    Docker cache (#2121)
    * Build new unified docker base
    build clang and gcc docker test env's off base image
    Build releases based off gcc base
    deploy across multiple jobs
    
    * use correct tag for base image
    
    * add 2 more stages, one to build base if necessary, split environment building before testing, then split deploy live/beta releases
    
    * move clang deps back to clang specific dockerfile
    
    * re factor secure variable

--- ci/deploy-docker.sh
+++ ci/deploy-docker.sh
@@ -7,33 +7,30 @@ if [ -n "$DOCKER_PASSWORD" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u nanoreleaseteam --password-stdin
 
     # We push this just so it can be a cache next time
-    if [ "$TRAVIS_BRANCH" = "master" ]; then
-        for compiler in gcc clang; do
-            ci_image_name="nanocurrency/nano-ci-${compiler}"
-            ci/build-docker-image.sh docker/ci/Dockerfile-${compiler} nanocurrency/nano-ci-${compiler};
-            "$scripts"/custom-timeout.sh 30 docker push "$ci_image_name"
-        done
+    if [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "docker_cache" ]] && [[ "${TRAVIS_BUILD_STAGE_NAME}" =~ 'Build' ]]; then
+        ci_image_name="nanocurrency/nano-env:$TRAVIS_JOB_NAME"
+        ci/build-docker-image.sh docker/ci/Dockerfile-$TRAVIS_JOB_NAME "$ci_image_name";
+        "$scripts"/custom-timeout.sh 30 docker push "$ci_image_name"
     fi
 
-    tags=()
-    if [[ "${TRAVIS_TAG}" =~ 'RC' ]]; then
-        tags+=("$TRAVIS_TAG" latest-including-rc)
-    elif [ -n "$TRAVIS_TAG" ]; then
-        tags+=("$TRAVIS_TAG" latest latest-including-rc)
-    elif [ -n "$TRAVIS_BRANCH" ]; then
-        tags+=("$TRAVIS_BRANCH")
-    fi
+    if [[ "$TRAVIS_BUILD_STAGE_NAME" == "Deploy" ]]; then
+        tags=()
+        if [[ "${TRAVIS_TAG}" =~ 'RC' ]]; then
+            tags+=("$TRAVIS_TAG" latest-including-rc)
+        elif [ -n "$TRAVIS_TAG" ]; then
+            tags+=("$TRAVIS_TAG" latest latest-including-rc)
+        elif [ -n "$TRAVIS_BRANCH" ]; then
+            tags+=("$TRAVIS_BRANCH")
+        fi
 
-    for network in live beta; do
-        if [ "${network}" = 'live' ]; then
+        if [[ "$TRAVIS_JOB_NAME" == "live" ]]; then
             network_tag_suffix=''
         else
-            network_tag_suffix="-${network}"
+            network_tag_suffix="-$TRAVIS_JOB_NAME"
         fi
 
         docker_image_name="nanocurrency/nano${network_tag_suffix}"
-
-        "$scripts"/custom-timeout.sh 30 docker build --build-arg NETWORK="$network" -f docker/node/Dockerfile -t "$docker_image_name" .
+        "$scripts"/custom-timeout.sh 30 docker build --build-arg NETWORK="$TRAVIS_JOB_NAME" -f docker/node/Dockerfile -t "$docker_image_name" .
         for tag in "${tags[@]}"; do
             # Sanitize docker tag
             # https://docs.docker.com/engine/reference/commandline/tag/
@@ -43,6 +40,5 @@ if [ -n "$DOCKER_PASSWORD" ]; then
             fi
             "$scripts"/custom-timeout.sh 30 docker push "${docker_image_name}:$tag"
         done
-    done
-
+    fi
 fi
