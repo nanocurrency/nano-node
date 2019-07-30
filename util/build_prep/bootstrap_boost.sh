1ab3bdb62cab1e8bf7db1a6c16839cef0c49f50d
Russel Waters <russel@nano.org>
2019-07-30 15:59:50 -0400

    boost 1.70 requires log_setup component specifically updated CMAKE 1.67 and 1.69 dont require explicit but will use (#2185)
    update boostrap_boost.sh to mirror https://docs.nano.org/integration-guides/build-options/

--- util/build_prep/bootstrap_boost.sh
+++ util/build_prep/bootstrap_boost.sh
@@ -26,7 +26,7 @@ while getopts 'hmcCkpvB:' OPT; do
 			exit 0
 			;;
 		m)
-			bootstrapArgs+=('--with-libraries=thread,log,filesystem,program_options')
+			bootstrapArgs+=('--with-libraries=system,thread,log,filesystem,program_options')
 			;;
 		c)
 			useClang='true'
