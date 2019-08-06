13db5cbae7f47652f8aad3a32265658a0e4df4e4
Russel Waters <russel@nano.org>
2019-08-06 15:08:56 -0400

    split artifacts between beta and live build mac live always to catch new errors deploy live on tags not RC or DB build and deploy beta on tags RC or DB bundle QtDBus framework as required for plugin libqcocoa.dylib hardcode default DragNDrop and TBZ2 update deploy-docker script to hand travis tags and new stage name

---
+++ ci/build-deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+qt_dir=${1}
+src_dir=${2}
+
+set -o errexit
+set -o nounset
+set -o xtrace
+OS=`uname`
+
+mkdir build
+pushd build
+
+if [[ "${BETA:-0}" -eq 1 ]]; then
+    NETWORK_CFG="beta"
+    CONFIGURATION="RelWithDebInfo"
+else
+    NETWORK_CFG="live"
+    CONFIGURATION="Release"
+fi
+
+cmake \
+    -G'Unix Makefiles' \
+    -DACTIVE_NETWORK=nano_${NETWORK_CFG}_network \
+    -DNANO_GUI=ON \
+    -DCMAKE_BUILD_TYPE=${CONFIGURATION} \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DBOOST_ROOT=/usr/local \
+    -DQt5_DIR=${qt_dir} \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    ..
+
+if [[ "$OS" == 'Linux' ]]; then
+    cmake --build ${PWD} --target package --config ${CONFIGURATION} -- -j$(nproc)
+else
+    sudo cmake --build ${PWD} --target package --config ${CONFIGURATION} -- -j2
+fi
+
+popd
