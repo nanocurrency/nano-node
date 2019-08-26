94e021c0bc17c3c4d379a8dba855c56b04872198
Guilherme Lawless <guilherme@nano.org>
2019-08-26 15:34:53 +0100

    Enhancements to OpenCL work handling (#2247)
    * Enhance OpenCL work handling
    
    OpenCL work is now properly queued along with CPU work.
    Any work can now be cancelled properly and the node won't wait for completion when shutting down.
    Work threads now help in work generation; one extra thread is added on startup to handle OpenCL (this thread is usually at 100% usage too). If using OpenCL the best is to have at most hardware_concurrency - 1 threads.
    
    * (unrelated) clarify bandwidth limit is in bytes
    
    * Was missing blake2b_init, use work_value

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -87,7 +87,7 @@ public:
 	static std::chrono::seconds constexpr keepalive_period = std::chrono::seconds (60);
 	static std::chrono::seconds constexpr keepalive_cutoff = keepalive_period * 5;
 	static std::chrono::minutes constexpr wallet_backup_interval = std::chrono::minutes (5);
-	size_t bandwidth_limit{ 5 * 1024 * 1024 }; // 5Mb/s
+	size_t bandwidth_limit{ 5 * 1024 * 1024 }; // 5MB/s
 	std::chrono::milliseconds conf_height_processor_batch_min_time{ 50 };
 	bool backup_before_upgrade{ false };
 	std::chrono::seconds work_watcher_period{ std::chrono::seconds (5) };
