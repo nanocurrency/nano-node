ed32a4171eb0559f675c53bb1aaa1f4d08c95fb6
Guilherme Lawless <guilherme@nano.org>
2019-08-28 11:40:38 +0100

    Test for multiple requests

--- nano/core_test/distributed_work.cpp
+++ nano/core_test/distributed_work.cpp
@@ -72,3 +72,49 @@ TEST (distributed_work, no_peers_cancel)
 	}
 	ASSERT_TRUE (node->distributed_work.work.empty ());
 }
+
+TEST (distributed_work, no_peers_multi)
+{
+	nano::system system (24000, 1);
+	auto node (system.nodes[0]);
+	nano::block_hash hash;
+	unsigned total{ 10 };
+	std::atomic<unsigned> count{ 0 };
+	auto callback = [&count](boost::optional<uint64_t> work_a) {
+		ASSERT_TRUE (work_a.is_initialized ());
+		++count;
+	};
+	// Test many works for the same root
+	for (unsigned i{ 0 }; i < total; ++i)
+	{
+		node->distributed_work.make (hash, callback, node->network_params.network.publish_threshold);
+	}
+	system.deadline_set (5s);
+	while (count < total)
+	{
+		ASSERT_NO_ERROR (system.poll ());
+	}
+	// 1 root, and _total_ requests for that root are expected
+	ASSERT_EQ (1, node->distributed_work.work.size ());
+	{
+		auto requests (node->distributed_work.work.begin ());
+		ASSERT_EQ (hash, requests->first);
+		ASSERT_EQ (total, requests->second.size ());
+	}
+
+	node->distributed_work.cleanup ();
+	ASSERT_EQ (0, node->distributed_work.work.size ());
+	count = 0;
+	// Test many works for different roots
+	for (unsigned i{ 0 }; i < total; ++i)
+	{
+		nano::block_hash hash_i (i + 1);
+		node->distributed_work.make (hash_i, callback, node->network_params.network.publish_threshold);
+	}
+	// 10 roots expected with 1 work each
+	ASSERT_EQ (10, node->distributed_work.work.size ());
+	for (auto & requests : node->distributed_work.work)
+	{
+		ASSERT_EQ (1, requests.second.size ());
+	}
+}
