730f47ad991a5dd5ea304c12704d6941007b5eb8
Sergey Kroshnin <sergiysw@gmail.com>
2019-08-31 22:47:05 +0300

    Prevent possible from_multiplier () function overflow

--- nano/core_test/difficulty.cpp
+++ nano/core_test/difficulty.cpp
@@ -27,6 +27,24 @@ TEST (difficulty, multipliers)
 	}
 
 	{
+		uint64_t base = std::numeric_limits<std::uint64_t>::max ();
+		uint64_t difficulty = 0xffffffffffffff00;
+		double expected_multiplier = 0.00390625;
+
+		ASSERT_NEAR (expected_multiplier, nano::difficulty::to_multiplier (difficulty, base), 1e-10);
+		ASSERT_EQ (difficulty, nano::difficulty::from_multiplier (expected_multiplier, base));
+	}
+
+	{
+		uint64_t base = 0x8000000000000000;
+		uint64_t difficulty = 0xf000000000000000;
+		double expected_multiplier = 8.0;
+
+		ASSERT_NEAR (expected_multiplier, nano::difficulty::to_multiplier (difficulty, base), 1e-10);
+		ASSERT_EQ (difficulty, nano::difficulty::from_multiplier (expected_multiplier, base));
+	}
+
+	{
 #ifndef NDEBUG
 		// Causes valgrind to be noisy
 		if (!nano::running_within_valgrind ())
@@ -45,4 +63,25 @@ TEST (difficulty, multipliers)
 TEST (difficulty, network_constants)
 {
 	ASSERT_NEAR (16., nano::difficulty::to_multiplier (nano::network_constants::publish_full_threshold, nano::network_constants::publish_beta_threshold), 1e-10);
+}
+
+TEST (difficulty, overflow)
+{
+	// Overflow max
+	{
+		uint64_t base = std::numeric_limits<std::uint64_t>::max ();
+		uint64_t difficulty = std::numeric_limits<std::uint64_t>::max ();
+		double expected_multiplier = 1.001;
+
+		ASSERT_EQ (difficulty, nano::difficulty::from_multiplier (expected_multiplier, base));
+	}
+
+	// Overflow min
+	{
+		uint64_t base = 1;
+		uint64_t difficulty = 0;
+		double expected_multiplier = 0.999;
+
+		ASSERT_EQ (difficulty, nano::difficulty::from_multiplier (expected_multiplier, base));
+	}
 }
