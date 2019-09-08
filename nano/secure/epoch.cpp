eebce500238105c24583ac64cb316705c3ba06db
Wesley Shillingford <wezrule@hotmail.com>
2019-09-08 11:34:40 +0100

    Some minor cleanup

--- nano/secure/epoch.cpp
+++ nano/secure/epoch.cpp
@@ -1,23 +1,23 @@
 #include <nano/secure/epoch.hpp>
 
-nano::uint256_union nano::epochs::link (nano::epoch epoch_a) const
+nano::uint256_union const & nano::epochs::link (nano::epoch epoch_a) const
 {
 	return epochs_m.at (epoch_a).link;
 }
 
 bool nano::epochs::is_epoch_link (nano::uint256_union const & link_a) const
 {
-	return std::any_of (epochs_m.begin (), epochs_m.end (), [link_a](decltype (epochs_m)::value_type const & item_a) { return item_a.second.link == link_a; });
+	return std::any_of (epochs_m.begin (), epochs_m.end (), [&link_a](auto const & item_a) { return item_a.second.link == link_a; });
 }
 
-nano::public_key nano::epochs::signer (nano::epoch epoch_a) const
+nano::public_key const & nano::epochs::signer (nano::epoch epoch_a) const
 {
 	return epochs_m.at (epoch_a).signer;
 }
 
 nano::epoch nano::epochs::epoch (nano::uint256_union const & link_a) const
 {
-	auto existing (std::find_if (epochs_m.begin (), epochs_m.end (), [link_a](decltype (epochs_m)::value_type const & item_a) { return item_a.second.link == link_a; }));
+	auto existing (std::find_if (epochs_m.begin (), epochs_m.end (), [&link_a](auto const & item_a) { return item_a.second.link == link_a; }));
 	assert (existing != epochs_m.end ());
 	return existing->first;
 }
