0c59f0c11e9216ca1c3264f587267122617a6656
Guilherme Lawless <guilherme@nano.org>
2019-10-16 11:33:57 +0100

    Non-blocking UPnP device discovery

--- nano/node/portmapping.cpp
+++ nano/node/portmapping.cpp
@@ -6,46 +6,46 @@
 
 nano::port_mapping::port_mapping (nano::node & node_a) :
 node (node_a),
-devices (nullptr),
-protocols ({ { { "TCP", 0, boost::asio::ip::address_v4::any (), 0 }, { "UDP", 0, boost::asio::ip::address_v4::any (), 0 } } }),
-check_count (0),
-on (false)
+protocols ({ { { "TCP", 0, boost::asio::ip::address_v4::any (), 0 }, { "UDP", 0, boost::asio::ip::address_v4::any (), 0 } } })
 {
-	urls = { 0 };
-	data = { { 0 } };
 }
 
 void nano::port_mapping::start ()
 {
-	check_mapping_loop ();
+	on = true;
+	node.background ([this] {
+		this->check_mapping_loop ();
+	});
 }
 
 void nano::port_mapping::refresh_devices ()
 {
 	if (!network_params.network.is_test_network ())
 	{
-		nano::lock_guard<std::mutex> lock (mutex);
-		int discover_error = 0;
-		freeUPNPDevlist (devices);
-		devices = upnpDiscover (2000, nullptr, nullptr, UPNP_LOCAL_PORT_ANY, false, 2, &discover_error);
+		upnp_state upnp_l;
+		int discover_error_l = 0;
+		upnp_l.devices = upnpDiscover (2000, nullptr, nullptr, UPNP_LOCAL_PORT_ANY, false, 2, &discover_error_l);
 		std::array<char, 64> local_address;
 		local_address.fill (0);
-		auto igd_error (UPNP_GetValidIGD (devices, &urls, &data, local_address.data (), sizeof (local_address)));
-		if (igd_error == 1 || igd_error == 2)
-		{
-			boost::system::error_code ec;
-			address = boost::asio::ip::address_v4::from_string (local_address.data (), ec);
-		}
+		auto igd_error_l (UPNP_GetValidIGD (upnp_l.devices, &upnp_l.urls, &upnp_l.data, local_address.data (), sizeof (local_address)));
 		if (check_count % 15 == 0)
 		{
-			node.logger.always_log (boost::str (boost::format ("UPnP local address: %1%, discovery: %2%, IGD search: %3%") % local_address.data () % discover_error % igd_error));
+			node.logger.always_log (boost::str (boost::format ("UPnP local address: %1%, discovery: %2%, IGD search: %3%") % local_address.data () % discover_error_l % igd_error_l));
 			if (node.config.logging.upnp_details_logging ())
 			{
-				for (auto i (devices); i != nullptr; i = i->pNext)
+				for (auto i (upnp_l.devices); i != nullptr; i = i->pNext)
 				{
 					node.logger.always_log (boost::str (boost::format ("UPnP device url: %1% st: %2% usn: %3%") % i->descURL % i->st % i->usn));
 				}
 			}
+		}
+		// Update port mapping
+		nano::lock_guard<std::mutex> lock (mutex);
+		upnp = std::move (upnp_l);
+		if (igd_error_l == 1 || igd_error_l == 2)
+		{
+			boost::system::error_code ec;
+			address = boost::asio::ip::address_v4::from_string (local_address.data (), ec);
 		}
 	}
 }
@@ -75,15 +75,18 @@ void nano::port_mapping::refresh_mapping ()
 		// We don't map the RPC port because, unless RPC authentication was added, this would almost always be a security risk
 		for (auto & protocol : protocols)
 		{
-			auto add_port_mapping_error (UPNP_AddPortMapping (urls.controlURL, data.first.servicetype, config_port.c_str (), node_port.c_str (), address.to_string ().c_str (), nullptr, protocol.name, nullptr, nullptr));
-			if (check_count % 15 == 0)
+			auto add_port_mapping_error (UPNP_AddPortMapping (upnp.urls.controlURL, upnp.data.first.servicetype, config_port.c_str (), node_port.c_str (), address.to_string ().c_str (), nullptr, protocol.name, nullptr, nullptr));
+			if (node.config.logging.upnp_details_logging ())
 			{
 				node.logger.always_log (boost::str (boost::format ("UPnP %1% port mapping response: %2%") % protocol.name % add_port_mapping_error));
 			}
 			if (add_port_mapping_error == UPNPCOMMAND_SUCCESS)
 			{
-				node.logger.always_log (boost::str (boost::format ("%1% mapped to %2%") % config_port % node_port));
 				protocol.external_port = static_cast<uint16_t> (std::atoi (config_port.data ()));
+				if (node.config.logging.upnp_details_logging ())
+				{
+					node.logger.always_log (boost::str (boost::format ("%1% mapped to %2%") % config_port % node_port));
+				}
 			}
 			else
 			{
@@ -109,7 +112,7 @@ int nano::port_mapping::check_mapping ()
 			std::array<char, 6> int_port;
 			std::array<char, 16> remaining_mapping_duration;
 			remaining_mapping_duration.fill (0);
-			auto verify_port_mapping_error (UPNP_GetSpecificPortMappingEntry (urls.controlURL, data.first.servicetype, config_port.c_str (), protocol.name, nullptr, int_client.data (), int_port.data (), nullptr, nullptr, remaining_mapping_duration.data ()));
+			auto verify_port_mapping_error (UPNP_GetSpecificPortMappingEntry (upnp.urls.controlURL, upnp.data.first.servicetype, config_port.c_str (), protocol.name, nullptr, int_client.data (), int_port.data (), nullptr, nullptr, remaining_mapping_duration.data ()));
 			if (verify_port_mapping_error == UPNPCOMMAND_SUCCESS)
 			{
 				protocol.remaining = std::atoi (remaining_mapping_duration.data ());
@@ -122,7 +125,7 @@ int nano::port_mapping::check_mapping ()
 			result = std::min (result, protocol.remaining);
 			std::array<char, 64> external_address;
 			external_address.fill (0);
-			auto external_ip_error (UPNP_GetExternalIPAddress (urls.controlURL, data.first.servicetype, external_address.data ()));
+			auto external_ip_error (UPNP_GetExternalIPAddress (upnp.urls.controlURL, upnp.data.first.servicetype, external_address.data ()));
 			if (external_ip_error == UPNPCOMMAND_SUCCESS)
 			{
 				boost::system::error_code ec;
@@ -133,7 +136,7 @@ int nano::port_mapping::check_mapping ()
 				protocol.external_address = boost::asio::ip::address_v4::any ();
 				node.logger.always_log (boost::str (boost::format ("UPNP_GetExternalIPAddress failed %1%: %2%") % verify_port_mapping_error % strupnperror (verify_port_mapping_error)));
 			}
-			if (check_count % 15 == 0)
+			if (node.config.logging.upnp_details_logging ())
 			{
 				node.logger.always_log (boost::str (boost::format ("UPnP %1% mapping verification response: %2%, external ip response: %3%, external ip: %4%, internal ip: %5%, remaining lease: %6%") % protocol.name % verify_port_mapping_error % external_ip_error % external_address.data () % address.to_string () % remaining_mapping_duration.data ()));
 			}
@@ -146,7 +149,7 @@ void nano::port_mapping::check_mapping_loop ()
 {
 	int wait_duration = network_params.portmapping.check_timeout;
 	refresh_devices ();
-	if (devices != nullptr)
+	if (upnp.devices != nullptr)
 	{
 		auto remaining (check_mapping ());
 		// If the mapping is lost, refresh it
@@ -182,10 +185,39 @@ void nano::port_mapping::stop ()
 		if (protocol.external_port != 0)
 		{
 			// Be a good citizen for the router and shut down our mapping
-			auto delete_error (UPNP_DeletePortMapping (urls.controlURL, data.first.servicetype, std::to_string (protocol.external_port).c_str (), protocol.name, address.to_string ().c_str ()));
-			node.logger.always_log (boost::str (boost::format ("Shutdown port mapping response: %1%") % delete_error));
+			auto delete_error (UPNP_DeletePortMapping (upnp.urls.controlURL, upnp.data.first.servicetype, std::to_string (protocol.external_port).c_str (), protocol.name, address.to_string ().c_str ()));
+			if (delete_error)
+			{
+				node.logger.always_log (boost::str (boost::format ("Shutdown port mapping response: %1%") % delete_error));
+			}
 		}
 	}
-	freeUPNPDevlist (devices);
-	devices = nullptr;
+}
+
+nano::upnp_state::~upnp_state ()
+{
+	if (devices)
+	{
+		freeUPNPDevlist (devices);
+		devices = nullptr;
+	}
+}
+
+nano::upnp_state & nano::upnp_state::operator= (nano::upnp_state && other_a)
+{
+	if (this == &other_a)
+	{
+		return *this;
+	}
+	if (devices)
+	{
+		freeUPNPDevlist (devices);
+	}
+	devices = other_a.devices;
+	other_a.devices = nullptr;
+	urls = other_a.urls;
+	other_a.urls = { 0 };
+	data = other_a.data;
+	other_a.data = { { 0 } };
+	return *this;
 }
