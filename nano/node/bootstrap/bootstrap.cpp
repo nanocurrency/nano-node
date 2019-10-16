ca7b02933f2bb63439345930c043da4bbf6b4c26
Guilherme Lawless <guilherme@nano.org>
2019-10-16 18:17:03 +0100

    Fix bootstrap attempt not stopping (#2347)

--- nano/node/bootstrap/bootstrap.cpp
+++ nano/node/bootstrap/bootstrap.cpp
@@ -1091,7 +1091,7 @@ void nano::bootstrap_initiator::bootstrap (nano::endpoint const & endpoint_a, bo
 		{
 			attempt->stop ();
 			// clang-format off
-			condition.wait (lock, [attempt = attempt, &stopped = stopped] { return stopped || attempt == nullptr; });
+			condition.wait (lock, [&attempt = attempt, &stopped = stopped] { return stopped || attempt == nullptr; });
 			// clang-format on
 		}
 		node.stats.inc (nano::stat::type::bootstrap, nano::stat::detail::initiate, nano::stat::dir::out);
@@ -1111,7 +1111,7 @@ void nano::bootstrap_initiator::bootstrap_lazy (nano::hash_or_account const & ha
 			{
 				attempt->stop ();
 				// clang-format off
-				condition.wait (lock, [attempt = attempt, &stopped = stopped] { return stopped || attempt == nullptr; });
+				condition.wait (lock, [&attempt = attempt, &stopped = stopped] { return stopped || attempt == nullptr; });
 				// clang-format on
 			}
 		}
