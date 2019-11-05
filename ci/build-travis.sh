30e4a65eaaa10f154f804ac04009cb5daabf262c
Russel Waters <russel@nano.org>
2019-11-05 10:03:05 -0500

    fetch cached boost rocksdb qt update dockerfile to use cache dont use rocksdb cache for mac testing update build-travis.sh to handle the difference between cached and brew installs of rocksdb

--- ci/build-travis.sh
+++ ci/build-travis.sh
@@ -41,16 +41,25 @@ fi
 
 ulimit -S -n 8192
 
+if [[ "$OS" == 'Linux' ]]; then
+    ROCKSDB="-DROCKSDB_LIBRARIES=/tmp/rocksdb/lib/librocksdb.a \
+    -DROCKSDB_INCLUDE_DIRS=/tmp/rocksdb/include"
+else
+    ROCKSDB=""
+fi
+
+
 cmake \
     -G'Unix Makefiles' \
     -DACTIVE_NETWORK=nano_test_network \
     -DNANO_TEST=ON \
     -DNANO_GUI=ON \
     -DNANO_ROCKSDB=ON \
+    ${ROCKSDB} \
     -DNANO_WARN_TO_ERR=ON \
     -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DBOOST_ROOT=/usr/local \
+    -DBOOST_ROOT=/tmp/boost/ \
     -DQt5_DIR=${qt_dir} \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     ${SANITIZERS} \
