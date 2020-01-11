b70b41be87a6ad06cd8eab2c2cd2801b568af0ca
Guilherme Lawless <guilherme@nano.org>
2020-01-11 13:39:36 +0000

    Use a strand in distributed_work
    Wrap all distributed_work socket handling in a strand. Could use one strand for each peer [I think], but with the very low amount of messages (2 sent and 1 received at max) this seems unnecessary
    
    Other fixes and enhancements:
    
    - Fix a rare issue where generation would finish before `start_work()` is done (by moving the local generation code to the end of the function)
    - TSAN race fix in rpc.work_peer_many
    - Wrap request parameters in a work_request class
    - No logging when ongoing operations are canceled
    - Make `backoff` a chrono object
    - Move the work_peer_request class into distributed_work and rename it

--- nano/rpc_test/rpc.cpp
+++ nano/rpc_test/rpc.cpp
@@ -3038,7 +3038,7 @@ TEST (rpc, work_peer_many)
 	for (auto i (0); i < 10; ++i)
 	{
 		nano::keypair key1;
-		uint64_t work (0);
+		std::atomic<uint64_t> work (0);
 		node1.work_generate (key1.pub, [&work](boost::optional<uint64_t> work_a) {
 			ASSERT_TRUE (work_a.is_initialized ());
 			work = *work_a;
@@ -3051,6 +3051,7 @@ TEST (rpc, work_peer_many)
 			system4.poll ();
 		}
 	}
+	node1.stop ();
 }
 
 TEST (rpc, block_count)
