196f59ec5e2b2a1918105b900401262731a58503
Guilherme Lawless <guilherme@nano.org>
2020-03-06 16:22:18 +0000

    Batch erase in unchecked_cleanup due to a potentially large list

--- nano/secure/network_filter.cpp
+++ nano/secure/network_filter.cpp
@@ -99,5 +99,6 @@ nano::uint128_t nano::network_filter::hash (uint8_t const * bytes_a, size_t coun
 
 // Explicitly instantiate
 template nano::uint128_t nano::network_filter::hash (std::shared_ptr<nano::vote> const &) const;
+template nano::uint128_t nano::network_filter::hash (std::shared_ptr<nano::block> const &) const;
 template void nano::network_filter::clear (std::shared_ptr<nano::vote> const &);
 template void nano::network_filter::clear (std::shared_ptr<nano::block> const &);
