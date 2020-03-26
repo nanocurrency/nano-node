b276aff08652c669482bd0c96e35b058502de85c
Guilherme Lawless <guilherme@nano.org>
2020-03-26 17:57:20 +0000

    Consistently add conflicting block to election (#2652)
    * Add conflicting block after creating the election
    
    * Add a test ensuring correct behavior

--- nano/node/node.cpp
+++ nano/node/node.cpp
@@ -532,7 +532,6 @@ void nano::node::process_fork (nano::transaction const & transaction_a, std::sha
 	auto root (block_a->root ());
 	if (!store.block_exists (transaction_a, block_a->type (), block_a->hash ()) && store.root_exists (transaction_a, block_a->root ()))
 	{
-		active.publish (block_a);
 		std::shared_ptr<nano::block> ledger_block (ledger.forked_block (transaction_a, *block_a));
 		if (ledger_block && !block_confirmed_or_being_confirmed (transaction_a, ledger_block->hash ()))
 		{
@@ -562,6 +561,7 @@ void nano::node::process_fork (nano::transaction const & transaction_a, std::sha
 				election.election->transition_active ();
 			}
 		}
+		active.publish (block_a);
 	}
 }
 
