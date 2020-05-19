152f2cda1fe56c631279620b51d105b5b678caea
Russel Waters <russel@nano.org>
2020-05-19 14:53:44 -0400

    update clang/clang-6 docker images to use cached boost artifacts macOS updated to used cached boost artifacts Rename variable

--- util/build_prep/fetch_boost.sh
+++ util/build_prep/fetch_boost.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 OS=`uname`
 
-TRAVIS_COMPILER="${TRAVIS_COMPILER:-clang}"
+COMPILER="${COMPILER:-clang}"
 
 pushd /tmp
-wget -O boost-$OS-$TRAVIS_COMPILER-1.70.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$TRAVIS_COMPILER-1.70.tgz
-tar -zxf boost-$OS-$TRAVIS_COMPILER-1.70.tgz
+wget -O boost-$OS-$COMPILER-1.70.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$COMPILER-1.70.tgz
+tar -zxf boost-$OS-$COMPILER-1.70.tgz
 mv tmp/* .
 rm -fr tmp
 popd
