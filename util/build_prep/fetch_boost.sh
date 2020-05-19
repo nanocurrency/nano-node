8a3d9209ab3e9b814ae624303a9990bd124fcf3c
Russel Waters <russel@nano.org>
2020-05-19 12:47:52 -0400

    update fetch_boost to use 1.70 url

--- util/build_prep/fetch_boost.sh
+++ util/build_prep/fetch_boost.sh
@@ -4,8 +4,8 @@ OS=`uname`
 TRAVIS_COMPILER="${TRAVIS_COMPILER:-clang}"
 
 pushd /tmp
-wget -O boost-$OS-$TRAVIS_COMPILER-latest.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$TRAVIS_COMPILER-latest.tgz
-tar -zxf boost-$OS-$TRAVIS_COMPILER-latest.tgz
+wget -O boost-$OS-$TRAVIS_COMPILER-1.70.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$TRAVIS_COMPILER-1.70.tgz
+tar -zxf boost-$OS-$TRAVIS_COMPILER-1.70.tgz
 mv tmp/* .
 rm -fr tmp
 popd
