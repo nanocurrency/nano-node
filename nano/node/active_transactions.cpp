nanocurrency/nano-node

commit 22794c2c1e4de1d9030e4eff61d6828535bc2826

Bisected election backtracking (#2778)
* Add ledger::backtrack limited to 128 jumps

* Bisect election dependencies when activating

* Addressing special case where the winner may not have a loaded sideband

* Simplify ledger::backtrack by specifying desired number of jumps instead. Limit is placed on the caller

* Perform DB transactions without holding the active mutex by batch processing

* Add a debug_assert on getting the previous block since it happens in the same tx

* Add a pessimistic fallback by also starting an election for the first unconfirmed block

* Set empty sideband, required by recent change (thanks Serg!)

* Check if first unconfirmed block is not being processed by conf height

* Already in active_transactions

* Use std::min<uint64_t> to avoid overflow (Wes review)

* Use owns_lock from mutex (Wes)

nano/node/active_transactions.cpp
@@ -256,6 +256,7 @@ void nano::active_transactions::request_confirm (nano::unique_lock<std::mutex> &
 	solicitor.flush ();
 	generator_session.flush ();
 	lock_a.lock ();
+	activate_dependencies (lock_a);
 
 	// This is updated after the loop to ensure slow machines don't do the full check often
 	if (check_all_elections_l)
@@ -291,6 +292,81 @@ void nano::active_transactions::frontiers_confirmation (nano::unique_lock<std::m
 		prioritize_frontiers_for_confirmation (transaction, node.network_params.network.is_test_network () ? std::chrono::milliseconds (50) : time_spent_prioritizing_ledger_accounts, time_spent_prioritizing_wallet_accounts);
 		confirm_prioritized_frontiers (transaction);
 		lock_a.lock ();
+	}
+}
+
+void nano::active_transactions::activate_dependencies (nano::unique_lock<std::mutex> & lock_a)
+{
+	debug_assert (lock_a.owns_lock ());
+	decltype (pending_dependencies) pending_l;
+	pending_l.swap (pending_dependencies);
+	lock_a.unlock ();
+	// Store blocks to activate when the lock is re-acquired, adding the hash of the original election as a dependency
+	std::vector<std::pair<std::shared_ptr<nano::block>, nano::block_hash>> activate_l;
+	{
+		auto transaction = node.store.tx_begin_read ();
+		for (auto const & entry_l : pending_l)
+		{
+			auto const & block_l (entry_l.first);
+			auto const height_l (entry_l.second);
+			bool escalated_l (false);
+			auto previous_hash_l (block_l->previous ());
+			if (!previous_hash_l.is_zero ())
+			{
+				/* Insert first unconfirmed block (pessimistic) and bisect the chain (likelihood) */
+				auto account (node.ledger.account (transaction, block_l->hash ()));
+				nano::confirmation_height_info conf_info_l;
+				if (!node.store.confirmation_height_get (transaction, account, conf_info_l))
+				{
+					if (height_l > conf_info_l.height + 1 && !conf_info_l.frontier.is_zero ())
+					{
+						auto successor_hash_l = node.store.block_successor (transaction, conf_info_l.frontier);
+						if (!confirmation_height_processor.is_processing_block (successor_hash_l))
+						{
+							auto successor_l = node.store.block_get (transaction, successor_hash_l);
+							debug_assert (successor_l != nullptr);
+							if (successor_l != nullptr)
+							{
+								activate_l.emplace_back (successor_l, block_l->hash ());
+							}
+						}
+					}
+					if (height_l > conf_info_l.height + 2)
+					{
+						auto const jumps_l = std::min<uint64_t> (128, (height_l - conf_info_l.height) / 2);
+						auto backtracked_l (node.ledger.backtrack (transaction, block_l, jumps_l));
+						if (backtracked_l != nullptr)
+						{
+							activate_l.emplace_back (backtracked_l, block_l->hash ());
+						}
+					}
+				}
+			}
+			/* If previous block not existing/not commited yet, block_source can cause segfault for state blocks
+				So source check can be done only if previous != nullptr or previous is 0 (open account) */
+			if (previous_hash_l.is_zero () || node.ledger.block_exists (previous_hash_l))
+			{
+				auto source_hash_l (node.ledger.block_source (transaction, *block_l));
+				if (!source_hash_l.is_zero () && source_hash_l != previous_hash_l && blocks.find (source_hash_l) == blocks.end ())
+				{
+					auto source_l (node.store.block_get (transaction, source_hash_l));
+					if (source_l != nullptr && !node.block_confirmed_or_being_confirmed (transaction, source_hash_l))
+					{
+						activate_l.emplace_back (source_l, block_l->hash ());
+					}
+				}
+			}
+		}
+	}
+	lock_a.lock ();
+	for (auto const & entry_l : activate_l)
+	{
+		auto election = insert_impl (entry_l.first);
+		if (election.inserted)
+		{
+			election.election->transition_active ();
+			election.election->dependent_blocks.insert (entry_l.second);
+		}
 	}
 }
 

nano/node/active_transactions.hpp
@@ -207,6 +207,8 @@ private:
 	void frontiers_confirmation (nano::unique_lock<std::mutex> &);
 	nano::account next_frontier_account{ 0 };
 	std::chrono::steady_clock::time_point next_frontier_check{ std::chrono::steady_clock::now () };
+	void activate_dependencies (nano::unique_lock<std::mutex> &);
+	std::vector<std::pair<std::shared_ptr<nano::block>, uint64_t>> pending_dependencies;
 	nano::condition_variable condition;
 	bool started{ false };
 	std::atomic<bool> stopped{ false };
@@ -262,6 +264,9 @@ private:
 	bool inactive_votes_bootstrap_check (std::vector<nano::account> const &, nano::block_hash const &, bool &);
 	boost::thread thread;
 
+	friend class election;
+	friend std::unique_ptr<container_info_component> collect_container_info (active_transactions &, const std::string &);
+
 	friend class active_transactions_dropped_cleanup_Test;
 	friend class active_transactions_vote_replays_Test;
 	friend class confirmation_height_prioritize_frontiers_Test;
@@ -269,7 +274,7 @@ private:
 	friend class active_transactions_confirmation_consistency_Test;
 	friend class active_transactions_vote_generator_session_Test;
 	friend class node_vote_by_hash_bundle_Test;
-	friend std::unique_ptr<container_info_component> collect_container_info (active_transactions &, const std::string &);
+	friend class election_bisect_dependencies_Test;
 };
 
 std::unique_ptr<container_info_component> collect_container_info (active_transactions & active_transactions, const std::string & name);

nano/node/election.hpp
@@ -111,5 +111,8 @@ public:
 	std::unordered_map<nano::block_hash, nano::uint128_t> last_tally;
 	std::unordered_set<nano::block_hash> dependent_blocks;
 	std::chrono::seconds late_blocks_delay{ 5 };
+	uint64_t const height;
+
+	friend class election_bisect_dependencies_Test;
 };
 }

nano/core_test/election.cpp
@@ -17,3 +17,82 @@ TEST (election, construction)
 	election->transition_passive ();
 	ASSERT_FALSE (election->idle ());
 }
+
+namespace nano
+{
+TEST (election, bisect_dependencies)
+{
+	nano::system system;
+	nano::node_flags flags;
+	flags.disable_request_loop = true;
+	auto & node = *system.add_node (flags);
+	nano::genesis genesis;
+	nano::confirmation_height_info conf_info;
+	ASSERT_FALSE (node.store.confirmation_height_get (node.store.tx_begin_read (), nano::test_genesis_key.pub, conf_info));
+	ASSERT_EQ (1, conf_info.height);
+	std::vector<std::shared_ptr<nano::block>> blocks;
+	blocks.push_back (nullptr); // idx == height
+	blocks.push_back (genesis.open);
+	nano::block_builder builder;
+	auto amount = nano::genesis_amount;
+	for (int i = 0; i < 299; ++i)
+	{
+		auto latest = blocks.back ();
+		blocks.push_back (builder.state ()
+		                  .previous (latest->hash ())
+		                  .account (nano::test_genesis_key.pub)
+		                  .representative (nano::test_genesis_key.pub)
+		                  .balance (--amount)
+		                  .link (nano::test_genesis_key.pub)
+		                  .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+		                  .work (*system.work.generate (latest->hash ()))
+		                  .build ());
+		ASSERT_EQ (nano::process_result::progress, node.process (*blocks.back ()).code);
+	}
+	ASSERT_EQ (301, blocks.size ());
+	ASSERT_TRUE (node.active.empty ());
+	{
+		auto election = node.active.insert (blocks.back ()).election;
+		ASSERT_NE (nullptr, election);
+		ASSERT_EQ (300, election->blocks.begin ()->second->sideband ().height);
+		nano::unique_lock<std::mutex> lock (node.active.mutex);
+		election->activate_dependencies ();
+		node.active.activate_dependencies (lock);
+	}
+	// The first dependency activation also starts an election for the first unconfirmed block
+	ASSERT_EQ (3, node.active.size ());
+	{
+		auto election = node.active.election (blocks[2]->qualified_root ());
+		ASSERT_NE (nullptr, election);
+		ASSERT_EQ (2, election->blocks.begin ()->second->sideband ().height);
+	}
+
+	auto check_height_and_activate_next = [&node, &blocks](uint64_t height_a) {
+		auto election = node.active.election (blocks[height_a]->qualified_root ());
+		ASSERT_NE (nullptr, election);
+		ASSERT_EQ (height_a, election->blocks.begin ()->second->sideband ().height);
+		nano::unique_lock<std::mutex> lock (node.active.mutex);
+		election->activate_dependencies ();
+		node.active.activate_dependencies (lock);
+	};
+	check_height_and_activate_next (300 - 128); // ensure limited by 128 jumps
+	ASSERT_EQ (4, node.active.size ());
+	check_height_and_activate_next (87);
+	ASSERT_EQ (5, node.active.size ());
+	check_height_and_activate_next (44);
+	ASSERT_EQ (6, node.active.size ());
+	check_height_and_activate_next (23);
+	ASSERT_EQ (7, node.active.size ());
+	check_height_and_activate_next (12);
+	ASSERT_EQ (8, node.active.size ());
+	check_height_and_activate_next (7);
+	ASSERT_EQ (9, node.active.size ());
+	check_height_and_activate_next (4);
+	ASSERT_EQ (10, node.active.size ());
+	check_height_and_activate_next (3);
+	ASSERT_EQ (10, node.active.size ()); // height 2 already inserted initially, no more blocks to activate
+	check_height_and_activate_next (2);
+	ASSERT_EQ (10, node.active.size ()); // conf height is 1, no more blocks to activate
+	ASSERT_EQ (node.active.blocks.size (), node.active.roots.size ());
+}
+}

nano/node/election.cpp
@@ -27,7 +27,8 @@ nano::election::election (nano::node & node_a, std::shared_ptr<nano::block> bloc
 confirmation_action (confirmation_action_a),
 prioritized_m (prioritized_a),
 node (node_a),
-status ({ block_a, 0, std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now ().time_since_epoch ()), std::chrono::duration_values<std::chrono::milliseconds>::zero (), 0, 1, 0, nano::election_status_type::ongoing })
+status ({ block_a, 0, std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now ().time_since_epoch ()), std::chrono::duration_values<std::chrono::milliseconds>::zero (), 0, 1, 0, nano::election_status_type::ongoing }),
+height (block_a->sideband ().height)
 {
 	last_votes.emplace (node.network_params.random.not_an_account, nano::vote_info{ std::chrono::steady_clock::now (), 0, block_a->hash () });
 	blocks.emplace (block_a->hash (), block_a);
@@ -210,46 +211,8 @@ bool nano::election::confirmed () const
 
 void nano::election::activate_dependencies ()
 {
-	auto transaction = node.store.tx_begin_read ();
-	bool escalated_l (false);
-	std::shared_ptr<nano::block> previous_l;
-	auto previous_hash_l (status.winner->previous ());
-	if (!previous_hash_l.is_zero () && node.active.blocks.find (previous_hash_l) == node.active.blocks.end ())
-	{
-		previous_l = node.store.block_get (transaction, previous_hash_l);
-		if (previous_l != nullptr && !node.block_confirmed_or_being_confirmed (transaction, previous_hash_l))
-		{
-			auto election = node.active.insert_impl (previous_l);
-			if (election.inserted)
-			{
-				election.election->transition_active ();
-				escalated_l = true;
-			}
-		}
-	}
-	/* If previous block not existing/not commited yet, block_source can cause segfault for state blocks
-				So source check can be done only if previous != nullptr or previous is 0 (open account) */
-	if (previous_hash_l.is_zero () || previous_l != nullptr)
-	{
-		auto source_hash_l (node.ledger.block_source (transaction, *status.winner));
-		if (!source_hash_l.is_zero () && source_hash_l != previous_hash_l && node.active.blocks.find (source_hash_l) == node.active.blocks.end ())
-		{
-			auto source_l (node.store.block_get (transaction, source_hash_l));
-			if (source_l != nullptr && !node.block_confirmed_or_being_confirmed (transaction, source_hash_l))
-			{
-				auto election = node.active.insert_impl (source_l);
-				if (election.inserted)
-				{
-					election.election->transition_active ();
-					escalated_l = true;
-				}
-			}
-		}
-	}
-	if (escalated_l)
-	{
-		update_dependent ();
-	}
+	debug_assert (!node.active.mutex.try_lock ());
+	node.active.pending_dependencies.emplace_back (status.winner, height);
 }
 
 void nano::election::broadcast_block (nano::confirmation_solicitor & solicitor_a)
@@ -266,7 +229,7 @@ void nano::election::broadcast_block (nano::confirmation_solicitor & solicitor_a
 bool nano::election::transition_time (nano::confirmation_solicitor & solicitor_a)
 {
 	debug_assert (!node.active.mutex.try_lock ());
-	nano::unique_lock<std::mutex> lock (timepoints_mutex);
+	nano::lock_guard<std::mutex> guard (timepoints_mutex);
 	bool result = false;
 	switch (state_m)
 	{
@@ -293,9 +256,7 @@ bool nano::election::transition_time (nano::confirmation_solicitor & solicitor_a
 			if (base_latency () * active_broadcasting_duration_factor < std::chrono::steady_clock::now () - state_start)
 			{
 				state_change (nano::election::state_t::broadcasting, nano::election::state_t::backtracking);
-				lock.unlock ();
 				activate_dependencies ();
-				lock.lock ();
 			}
 			break;
 		case nano::election::state_t::backtracking:

commit fa526273ef21955e6b795f91a8c064ae65f9a25f

Fix open blocks not getting activated through dependencies
This was a regression introduced by https://github.com/nanocurrency/nano-node/pull/2778

Adds two tests to ensure proper behavior:
- `election.dependencies_open_link` which covers the fix by itself
- `dependency_graph_frontier`, a more complex test ensuring we can walk down a more complex graph before confirming it from the bottom up

nano/node/active_transactions.cpp
@@ -313,6 +313,22 @@ void nano::active_transactions::activate_dependencies (nano::unique_lock<std::mu
 	decltype (pending_dependencies) pending_l;
 	pending_l.swap (pending_dependencies);
 	lock_a.unlock ();
+
+	auto first_unconfirmed = [this](nano::read_transaction const & transaction_a, nano::account const & account_a, nano::block_hash const & confirmed_frontier_a) {
+		if (!confirmed_frontier_a.is_zero ())
+		{
+			return this->node.store.block_successor (transaction_a, confirmed_frontier_a);
+		}
+		else
+		{
+			nano::account_info account_info_l;
+			auto error = node.store.account_get (transaction_a, account_a, account_info_l);
+			(void)error;
+			debug_assert (!error);
+			return account_info_l.open_block;
+		}
+	};
+
 	// Store blocks to activate when the lock is re-acquired, adding the hash of the original election as a dependency
 	std::vector<std::pair<std::shared_ptr<nano::block>, nano::block_hash>> activate_l;
 	{
@@ -332,9 +348,9 @@ void nano::active_transactions::activate_dependencies (nano::unique_lock<std::mu
 					nano::confirmation_height_info conf_info_l;
 					if (!node.store.confirmation_height_get (transaction, account, conf_info_l))
 					{
-						if (height_l > conf_info_l.height + 1 && !conf_info_l.frontier.is_zero ())
+						if (height_l > conf_info_l.height + 1)
 						{
-							auto const successor_hash_l = node.store.block_successor (transaction, conf_info_l.frontier);
+							auto const successor_hash_l = first_unconfirmed (transaction, account, conf_info_l.frontier);
 							if (!confirmation_height_processor.is_processing_block (successor_hash_l))
 							{
 								auto const successor_l = node.store.block_get (transaction, successor_hash_l);
@@ -366,7 +382,7 @@ void nano::active_transactions::activate_dependencies (nano::unique_lock<std::mu
 						auto source_l (node.store.block_get (transaction, source_hash_l));
 						if (source_l != nullptr && !node.block_confirmed_or_being_confirmed (transaction, source_hash_l))
 						{
-							activate_l.emplace_back (source_l, block_l->hash ());
+							activate_l.emplace_back (source_l, hash_l);
 						}
 					}
 				}

nano/node/active_transactions.hpp
@@ -279,6 +279,7 @@ private:
 	friend class active_transactions_vote_generator_session_Test;
 	friend class node_vote_by_hash_bundle_Test;
 	friend class election_bisect_dependencies_Test;
+	friend class election_dependencies_open_link_Test;
 };
 
 std::unique_ptr<container_info_component> collect_container_info (active_transactions & active_transactions, const std::string & name);

nano/node/election.hpp
@@ -118,5 +118,6 @@ public:
 	friend class active_transactions;
 
 	friend class election_bisect_dependencies_Test;
+	friend class election_dependencies_open_link_Test;
 };
 }

nano/core_test/election.cpp
@@ -95,4 +95,83 @@ TEST (election, bisect_dependencies)
 	ASSERT_EQ (10, node.active.size ()); // conf height is 1, no more blocks to activate
 	ASSERT_EQ (node.active.blocks.size (), node.active.roots.size ());
 }
+
+// Tests successful dependency activation of the open block of an account, and its corresponding source
+TEST (election, dependencies_open_link)
+{
+	nano::system system;
+	nano::node_flags flags;
+	flags.disable_request_loop = true;
+	auto & node = *system.add_node (flags);
+
+	nano::state_block_builder builder;
+	nano::keypair key;
+
+	// Send to key
+	auto gen_send = builder.make_block ()
+	                .account (nano::test_genesis_key.pub)
+	                .previous (nano::genesis_hash)
+	                .representative (nano::test_genesis_key.pub)
+	                .link (key.pub)
+	                .balance (nano::genesis_amount - 1)
+	                .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+	                .work (*system.work.generate (nano::genesis_hash))
+	                .build ();
+	// Receive from genesis
+	auto key_open = builder.make_block ()
+	                .account (key.pub)
+	                .previous (0)
+	                .representative (key.pub)
+	                .link (gen_send->hash ())
+	                .balance (1)
+	                .sign (key.prv, key.pub)
+	                .work (*system.work.generate (key.pub))
+	                .build ();
+
+	// Send to self
+	std::shared_ptr<nano::block> key_send = builder.make_block ()
+	                                        .account (key.pub)
+	                                        .previous (key_open->hash ())
+	                                        .representative (key.pub)
+	                                        .link (key.pub)
+	                                        .balance (0)
+	                                        .sign (key.prv, key.pub)
+	                                        .work (*system.work.generate (key_open->hash ()))
+	                                        .build ();
+
+	node.process (*gen_send);
+	node.process (*key_open);
+	node.process (*key_send);
+
+	// Insert frontier
+	node.block_confirm (key_send);
+	ASSERT_EQ (1, node.active.size ());
+	{
+		auto election = node.active.election (key_send->qualified_root ());
+		ASSERT_NE (nullptr, election);
+		nano::unique_lock<std::mutex> lock (node.active.mutex);
+		election->activate_dependencies ();
+		node.active.activate_dependencies (lock);
+	}
+	// Must have activated the open block
+	ASSERT_EQ (2, node.active.size ());
+	{
+		auto election = node.active.election (key_open->qualified_root ());
+		ASSERT_NE (nullptr, election);
+		nano::unique_lock<std::mutex> lock (node.active.mutex);
+		election->activate_dependencies ();
+		node.active.activate_dependencies (lock);
+	}
+	// Must have activated the open's source block
+	ASSERT_EQ (3, node.active.size ());
+	{
+		auto election = node.active.election (gen_send->qualified_root ());
+		ASSERT_NE (nullptr, election);
+		nano::unique_lock<std::mutex> lock (node.active.mutex);
+		election->activate_dependencies ();
+		node.active.activate_dependencies (lock);
+	}
+	// Nothing else to activate
+	ASSERT_EQ (3, node.active.size ());
+}
 }

nano/core_test/node.cpp
@@ -3906,6 +3906,7 @@ TEST (node, rollback_vote_self)
 	ASSERT_EQ (fork->hash (), vote->second.hash);
 }
 
+// Confirm a complex dependency graph starting from the first block
 TEST (node, dependency_graph)
 {
 	nano::system system;
@@ -4099,8 +4100,182 @@ TEST (node, dependency_graph)
 		EXPECT_FALSE (error);
 		return error || node.ledger.cache.cemented_count == node.ledger.cache.block_count;
 	}));
-	ASSERT_TIMELY (10s, node.ledger.cache.cemented_count == node.ledger.cache.block_count);
+	ASSERT_EQ (node.ledger.cache.cemented_count, node.ledger.cache.block_count);
 	ASSERT_TIMELY (5s, node.active.empty ());
+}
+
+// Confirm a complex dependency graph starting from a frontier
+TEST (node, dependency_graph_frontier)
+{
+	nano::system system;
+	nano::node_config config (nano::get_available_port (), system.logging);
+	config.frontiers_confirmation = nano::frontiers_confirmation_mode::disabled;
+	auto & node1 = *system.add_node (config);
+	config.peering_port = nano::get_available_port ();
+	auto & node2 = *system.add_node (config);
+
+	nano::state_block_builder builder;
+	nano::keypair key1, key2, key3;
+
+	// Send to key1
+	std::shared_ptr<nano::state_block> gen_send1 = builder.make_block ()
+	                                               .account (nano::test_genesis_key.pub)
+	                                               .previous (nano::genesis_hash)
+	                                               .representative (nano::test_genesis_key.pub)
+	                                               .link (key1.pub)
+	                                               .balance (nano::genesis_amount - 1)
+	                                               .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+	                                               .work (*system.work.generate (nano::genesis_hash))
+	                                               .build ();
+	// Receive from genesis
+	auto key1_open = builder.make_block ()
+	                 .account (key1.pub)
+	                 .previous (0)
+	                 .representative (key1.pub)
+	                 .link (gen_send1->hash ())
+	                 .balance (1)
+	                 .sign (key1.prv, key1.pub)
+	                 .work (*system.work.generate (key1.pub))
+	                 .build ();
+	// Send to genesis
+	auto key1_send1 = builder.make_block ()
+	                  .account (key1.pub)
+	                  .previous (key1_open->hash ())
+	                  .representative (key1.pub)
+	                  .link (nano::test_genesis_key.pub)
+	                  .balance (0)
+	                  .sign (key1.prv, key1.pub)
+	                  .work (*system.work.generate (key1_open->hash ()))
+	                  .build ();
+	// Receive from key1
+	auto gen_receive = builder.make_block ()
+	                   .from (*gen_send1)
+	                   .previous (gen_send1->hash ())
+	                   .link (key1_send1->hash ())
+	                   .balance (nano::genesis_amount)
+	                   .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+	                   .work (*system.work.generate (gen_send1->hash ()))
+	                   .build ();
+	// Send to key2
+	auto gen_send2 = builder.make_block ()
+	                 .from (*gen_receive)
+	                 .previous (gen_receive->hash ())
+	                 .link (key2.pub)
+	                 .balance (gen_receive->balance ().number () - 2)
+	                 .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+	                 .work (*system.work.generate (gen_receive->hash ()))
+	                 .build ();
+	// Receive from genesis
+	auto key2_open = builder.make_block ()
+	                 .account (key2.pub)
+	                 .previous (0)
+	                 .representative (key2.pub)
+	                 .link (gen_send2->hash ())
+	                 .balance (2)
+	                 .sign (key2.prv, key2.pub)
+	                 .work (*system.work.generate (key2.pub))
+	                 .build ();
+	// Send to key3
+	auto key2_send1 = builder.make_block ()
+	                  .account (key2.pub)
+	                  .previous (key2_open->hash ())
+	                  .representative (key2.pub)
+	                  .link (key3.pub)
+	                  .balance (1)
+	                  .sign (key2.prv, key2.pub)
+	                  .work (*system.work.generate (key2_open->hash ()))
+	                  .build ();
+	// Receive from key2
+	auto key3_open = builder.make_block ()
+	                 .account (key3.pub)
+	                 .previous (0)
+	                 .representative (key3.pub)
+	                 .link (key2_send1->hash ())
+	                 .balance (1)
+	                 .sign (key3.prv, key3.pub)
+	                 .work (*system.work.generate (key3.pub))
+	                 .build ();
+	// Send to key1
+	auto key2_send2 = builder.make_block ()
+	                  .from (*key2_send1)
+	                  .previous (key2_send1->hash ())
+	                  .link (key1.pub)
+	                  .balance (key2_send1->balance ().number () - 1)
+	                  .sign (key2.prv, key2.pub)
+	                  .work (*system.work.generate (key2_send1->hash ()))
+	                  .build ();
+	// Receive from key2
+	auto key1_receive = builder.make_block ()
+	                    .from (*key1_send1)
+	                    .previous (key1_send1->hash ())
+	                    .link (key2_send2->hash ())
+	                    .balance (key1_send1->balance ().number () + 1)
+	                    .sign (key1.prv, key1.pub)
+	                    .work (*system.work.generate (key1_send1->hash ()))
+	                    .build ();
+	// Send to key3
+	auto key1_send2 = builder.make_block ()
+	                  .from (*key1_receive)
+	                  .previous (key1_receive->hash ())
+	                  .link (key3.pub)
+	                  .balance (key1_receive->balance ().number () - 1)
+	                  .sign (key1.prv, key1.pub)
+	                  .work (*system.work.generate (key1_receive->hash ()))
+	                  .build ();
+	// Receive from key1
+	auto key3_receive = builder.make_block ()
+	                    .from (*key3_open)
+	                    .previous (key3_open->hash ())
+	                    .link (key1_send2->hash ())
+	                    .balance (key3_open->balance ().number () + 1)
+	                    .sign (key3.prv, key3.pub)
+	                    .work (*system.work.generate (key3_open->hash ()))
+	                    .build ();
+	// Upgrade key3
+	auto key3_epoch = builder.make_block ()
+	                  .from (*key3_receive)
+	                  .previous (key3_receive->hash ())
+	                  .link (node1.ledger.epoch_link (nano::epoch::epoch_1))
+	                  .balance (key3_receive->balance ())
+	                  .sign (nano::test_genesis_key.prv, nano::test_genesis_key.pub)
+	                  .work (*system.work.generate (key3_receive->hash ()))
+	                  .build ();
+
+	for (auto const & node : system.nodes)
+	{
+		auto transaction (node->store.tx_begin_write ());
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *gen_send1).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key1_open).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key1_send1).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *gen_receive).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *gen_send2).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key2_open).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key2_send1).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key3_open).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key2_send2).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key1_receive).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key1_send2).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key3_receive).code);
+		ASSERT_EQ (nano::process_result::progress, node->ledger.process (transaction, *key3_epoch).code);
+	}
+
+	ASSERT_TRUE (node1.active.empty () && node2.active.empty ());
+
+	// node1 can vote, but only on the first block
+	system.wallet (0)->insert_adhoc (nano::test_genesis_key.prv);
+
+	// activate the graph frontier
+	// node2 activates dependencies in sequence until it reaches the first block
+	node2.block_confirm (node2.block (key3_epoch->hash ()));
+
+	// Eventually the first block in the graph gets activated and confirmed via node1
+	ASSERT_TIMELY (15s, node2.block_confirmed (gen_send1->hash ()));
+
+	// Activate the first block in node1, allowing it to confirm all blocks for both nodes
+	node1.block_confirm (gen_send1);
+	ASSERT_TIMELY (15s, node1.ledger.cache.cemented_count == node1.ledger.cache.block_count);
+	ASSERT_TIMELY (5s, node2.ledger.cache.cemented_count == node2.ledger.cache.block_count);
+	ASSERT_TIMELY (5s, node1.active.empty () && node2.active.empty ());
 }
 
 namespace