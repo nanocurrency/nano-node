c3782ce0aaea82c001a3a9205cc72f9c3df5d88a
Guilherme Lawless <guilherme@nano.org>
2020-08-03 18:42:07 +0100

    Beta reset for V22 development (#2830)
    * Change beta preconfigured_representatives
    
    One of them was not used. Account->key:
    
    - nano_1beta1ayfkpj1tfbhi3e9ihkocjkqi6ms5e4xrbmbybqnkza1e5jrake8wai ->
    259A4011E6CAD1069A97C02C3C1F2AAA32BC093C8D82EE1334F937A4BE803071
    - nano_1betazh7m3c9gwcsy7w3rzynbqr9gomjwn3cp59xqky48we46eaqptbdskh4 ->
    259A47DE59854777159F1781C7FD44DF0775671E502AB0CFDBCBC23718223117
    
    * Change online weight samples to span a 1-day period for beta
    
    * Beta network reset for V22 development
    
    Genesis
    nano_1betag41gqumw8fywwp1yay6k9jinswhqhbjtogmm1ibmnyfo1apej3medr3
    
    * Change epoch 2 send/change threshold to be the same as epoch 1
    
    * Fix difficulty tests
    
    * Change default beta network representative to a burn address
    
    * Fix cross initialization error

--- nano/lib/config.cpp
+++ nano/lib/config.cpp
@@ -15,7 +15,7 @@ work_thresholds const network_constants::publish_full (
 
 work_thresholds const network_constants::publish_beta (
 0xfffff00000000000, // 64x lower than publish_full.epoch_1
-0xfffff80000000000, // 2x higher than epoch_1
+0xfffff00000000000, // same as epoch_1
 0xffffe00000000000 // 2x lower than epoch_1
 );
 
