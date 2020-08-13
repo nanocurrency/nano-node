62f6a38407ca0a28bd8d0345d26ae2102d5ae24a
wezrule <wezrule@hotmail.com>
2020-08-13 17:47:10 +0100

    Use is_dev_network

--- nano/node/rocksdb/rocksdb.cpp
+++ nano/node/rocksdb/rocksdb.cpp
@@ -293,7 +293,7 @@ size_t nano::rocksdb_store::count (nano::transaction const & transaction_a, tabl
 	// These should only be used in tests to check database consistency
 	else if (table_a == tables::accounts)
 	{
-		debug_assert (network_constants ().is_test_network ());
+		debug_assert (network_constants ().is_dev_network ());
 		for (auto i (latest_begin (transaction_a)), n (latest_end ()); i != n; ++i)
 		{
 			++sum;
