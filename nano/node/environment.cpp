5559a03f239e5543dba2936e288353bae255fd62
clemahieu <clemahieu@gmail.com>
2020-08-23 13:48:35 +0200

    Moving directory initialization in to environment class.

--- nano/node/environment.cpp
+++ nano/node/environment.cpp
@@ -3,9 +3,16 @@
 #include <algorithm>
 #include <thread>
 
-nano::environment::environment () :
-alarm (ctx),
-work_impl (std::make_unique<nano::work_pool> (std::max (std::thread::hardware_concurrency (), 1u))),
-work (*work_impl)
+nano::environment::environment (boost::filesystem::path const & path_a) :
+path{ path_a },
+alarm{ ctx },
+work_impl{ std::make_unique<nano::work_pool> (std::max (std::thread::hardware_concurrency (), 1u)) },
+work{ *work_impl }
 {
+	/*
+	 * @warning May throw a filesystem exception
+	 */
+	boost::system::error_code error_chmod;
+	boost::filesystem::create_directories (path_a);
+	nano::set_secure_perm_directory (path_a, error_chmod);
 }
--- nano/node/environment.hpp
+++ nano/node/environment.hpp
@@ -3,6 +3,7 @@
 #include <nano/lib/alarm.hpp>
 #include <nano/lib/work.hpp>
 #include <nano/secure/common.hpp>
+#include <nano/secure/utility.hpp>
 
 #include <boost/asio/io_context.hpp>
 
@@ -16,7 +17,8 @@ namespace nano
 class environment
 {
 public:
-	environment ();
+	environment (boost::filesystem::path const & = nano::working_path ());
+	boost::filesystem::path path;
 	boost::asio::io_context ctx;
 	nano::alarm alarm;
 	std::unique_ptr<nano::work_pool> work_impl;
