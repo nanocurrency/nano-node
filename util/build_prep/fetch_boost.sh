a825b5fd2f8f62accc0292ddf79bfade10d08842
Russel <russel@nano.org>
2020-08-26 16:29:36 -0700

    update base docker images to ubuntu 18.04 reference updated artifacts

--- util/build_prep/fetch_boost.sh
+++ util/build_prep/fetch_boost.sh
@@ -4,7 +4,7 @@ OS=`uname`
 COMPILER="${COMPILER:-clang}"
 
 pushd /tmp
-wget -O boost-$OS-$COMPILER.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$COMPILER-1.70-full.tgz
+wget -O boost-$OS-$COMPILER.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/boost-$OS-$COMPILER-1.70-full-18.tgz
 tar -zxf boost-$OS-$COMPILER.tgz
 mv tmp/* .
 rm -fr tmp
