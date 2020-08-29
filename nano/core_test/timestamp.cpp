43c792e81b9bff527681c217ba176f0beb2685db
clemahieu <clemahieu@gmail.com>
2020-08-29 03:25:52 +0200

    Renaming timestamp_now to now.

--- nano/core_test/timestamp.cpp
+++ nano/core_test/timestamp.cpp
@@ -12,7 +12,7 @@ TEST (timestamp, now)
 {
 	nano::timestamp_generator generator;
 	auto before (generator.timestamp_from_ms (std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now ().time_since_epoch ()).count ()));
-	auto now (generator.now ());
+	auto now (generator.now_base ());
 	auto after (generator.timestamp_from_ms (std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now ().time_since_epoch ()).count ()));
 	ASSERT_LE (before, now);
 	ASSERT_LE (now, after);
@@ -21,22 +21,22 @@ TEST (timestamp, now)
 TEST (timestamp, basic)
 {
 	nano::timestamp_generator generator;
-	auto one (generator.timestamp_now ());
+	auto one (generator.now ());
 	ASSERT_EQ (0, generator.component_count (one));
 	ASSERT_NE (0, generator.component_time (one));
-	auto two (generator.timestamp_now ());
+	auto two (generator.now ());
 	ASSERT_NE (one, two);
 }
 
 TEST (timestamp, count)
 {
 	nano::timestamp_generator generator;
-	auto one (generator.timestamp_now ());
-	auto two (generator.timestamp_now ());
+	auto one (generator.now ());
+	auto two (generator.now ());
 	while (generator.component_time (one) != generator.component_time (two))
 	{
 		one = two;
-		two = generator.timestamp_now ();
+		two = generator.now ();
 	}
 	ASSERT_EQ (one + 1, two);
 }
@@ -53,7 +53,7 @@ TEST (timestamp, parallel)
 		threads.push_back (std::thread ([&timestamps, &generator, &mutex] () {
 			for (auto i (0); i < 1000; ++i)
 			{
-				auto stamp (generator.timestamp_now ());
+				auto stamp (generator.now ());
 				std::lock_guard<std::mutex> lock (mutex);
 				auto inserted (timestamps.insert (stamp));
 				assert (inserted.second);
--- nano/lib/timestamp.hpp
+++ nano/lib/timestamp.hpp
@@ -37,20 +37,20 @@ public:
 		auto result (value_a << count_bits);
 		return result;
 	}
-	static uint64_t now ()
+	static uint64_t now_base ()
 	{
 		uint64_t ms_since_epoch (std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now ().time_since_epoch ()).count ());
 		uint64_t result (timestamp_from_ms (ms_since_epoch));
 		return result;
 	}
-	uint64_t timestamp_now ()
+	uint64_t now ()
 	{
 		static_assert (time_bits + count_bits == 64);
 		uint64_t result (0);
 		while (result == 0)
 		{
 			result = next;
-			auto now_l (now ());
+			auto now_l (now_base ());
 			if (component_time (result) != now_l)
 			{
 				if (next.compare_exchange_weak (result, now_l + 1))
