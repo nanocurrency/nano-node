0434bc5b2da303773386a4e4efa6970e90fc5834
Sergey Kroshnin <sergiysw@gmail.com>
2020-09-11 01:06:23 +0300

    Wesley's fix for RocksDB pruned issue
    Co-Authored-By: Wesley Shillingford <650038+wezrule@users.noreply.github.com>

--- nano/node/rocksdb/rocksdb.cpp
+++ nano/node/rocksdb/rocksdb.cpp
@@ -238,6 +238,11 @@ rocksdb::ColumnFamilyOptions nano::rocksdb_store::get_cf_options (std::string co
 		std::shared_ptr<rocksdb::TableFactory> table_factory (rocksdb::NewBlockBasedTableFactory (get_active_table_options (block_cache_size_bytes * 2)));
 		cf_options = get_active_cf_options (table_factory, memtable_size_bytes);
 	}
+	else if (cf_name_a == "pruned")
+	{
+		std::shared_ptr<rocksdb::TableFactory> table_factory (rocksdb::NewBlockBasedTableFactory (get_active_table_options (block_cache_size_bytes * 2)));
+		cf_options = get_active_cf_options (table_factory, memtable_size_bytes);
+	}
 	else if (cf_name_a == rocksdb::kDefaultColumnFamilyName)
 	{
 		// Do nothing.
