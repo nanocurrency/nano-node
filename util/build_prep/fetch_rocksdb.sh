89281c821a9834d35e3a3427b1c2905ee7aae874
Russel <russel@nano.org>
2020-09-11 13:42:28 -0700

    correct artifact for rocksdb and osx

--- util/build_prep/fetch_rocksdb.sh
+++ util/build_prep/fetch_rocksdb.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 OS=`uname`
-artifact="rocksdb-Linux-6.3.6-18.tgz"
+
 
 pushd /tmp
-wget -O $artifact https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/$artifact
-tar -zxf $artifact
+if [[ "$OS" == 'Linux' ]]; then
+    wget -O rocksdb.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/rocksdb-Linux-6.3.6-18.tgz
+else
+    wget -O rocksdb.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/rocksdb-clang-latest.tgz
+fi 
+
+tar -zxf rocksdb.tgz
 mv tmp/* .
 rm -fr tmp
 popd
