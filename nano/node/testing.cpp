c906ac02f9a4de62be2ed1f61c4543ac35325405
Sergey Kroshnin <sergiysw@gmail.com>
2020-11-05 21:16:13 +0300

    Improve blocks_confirm () function

--- nano/node/testing.cpp
+++ nano/node/testing.cpp
@@ -208,9 +208,11 @@ void nano::blocks_confirm (nano::node & node_a, std::vector<std::shared_ptr<nano
 	node_a.block_processor.flush ();
 	for (auto const & block : blocks_a)
 	{
+		auto disk_block (node_a.block (block->hash ()));
 		// A sideband is required to start an election
-		debug_assert (block->has_sideband ());
-		node_a.block_confirm (block);
+		debug_assert (disk_block != nullptr);
+		debug_assert (disk_block->has_sideband ());
+		node_a.block_confirm (disk_block);
 	}
 }
 
