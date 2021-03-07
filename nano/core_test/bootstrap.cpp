af3aeb2933adecb64e7944a9384a0a45884e532d
Sergey Kroshnin <sergiysw@gmail.com>
2021-03-07 21:14:44 +0300

    Disable frontiers confirmation check as ineffective

--- nano/core_test/bootstrap.cpp
+++ nano/core_test/bootstrap.cpp
@@ -354,7 +354,7 @@ TEST (bootstrap_processor, DISABLED_pull_requeue_network_error)
 	ASSERT_EQ (0, node1->stats.count (nano::stat::type::bootstrap, nano::stat::detail::bulk_pull_failed_account, nano::stat::dir::in)); // Requeue is not increasing failed attempts
 }
 
-TEST (bootstrap_processor, frontiers_unconfirmed)
+TEST (bootstrap_processor, DISABLED_frontiers_unconfirmed)
 {
 	nano::system system;
 	nano::node_config node_config (nano::get_available_port (), system.logging);
@@ -411,7 +411,7 @@ TEST (bootstrap_processor, frontiers_unconfirmed)
 	ASSERT_FALSE (node3->network.excluded_peers.check (nano::transport::map_endpoint_to_tcp (node1->network.endpoint ()))); // Banning from bootstrap is disabled
 }
 
-TEST (bootstrap_processor, frontiers_confirmed)
+TEST (bootstrap_processor, DISABLED_frontiers_confirmed)
 {
 	nano::system system;
 	nano::node_config node_config (nano::get_available_port (), system.logging);
