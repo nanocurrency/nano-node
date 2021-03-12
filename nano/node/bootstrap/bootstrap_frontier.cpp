04f57635fa66542c178d87b323be783f11ace6c3
Sergey Kroshnin <sergiysw@gmail.com>
2021-03-12 15:13:44 +0300

    Adjust age_factor

--- nano/node/bootstrap/bootstrap_frontier.cpp
+++ nano/node/bootstrap/bootstrap_frontier.cpp
@@ -109,7 +109,7 @@ void nano::frontier_req_client::received_frontier (boost::system::error_code con
 
 		double elapsed_sec = std::max (time_span.count (), nano::bootstrap_limits::bootstrap_minimum_elapsed_seconds_blockrate);
 		double blocks_per_sec = static_cast<double> (count) / elapsed_sec;
-		double age_factor = frontiers_age == std::numeric_limits<decltype (frontiers_age)>::max () ? 1.0 : static_cast<double> (connection->node->network_params.bootstrap.default_frontiers_age_seconds) * 14.0 / static_cast<double> (frontiers_age); // Allow slower frontiers receive for requests with age
+		double age_factor = frontiers_age == std::numeric_limits<decltype (frontiers_age)>::max () ? 1.0 : static_cast<double> (connection->node->network_params.bootstrap.default_frontiers_age_seconds) * 1.5 / static_cast<double> (frontiers_age); // Allow slower frontiers receive for requests with age
 		if (elapsed_sec > nano::bootstrap_limits::bootstrap_connection_warmup_time_sec && blocks_per_sec < nano::bootstrap_limits::bootstrap_minimum_frontier_blocks_per_sec / age_factor)
 		{
 			connection->node->logger.try_log (boost::str (boost::format ("Aborting frontier req because it was too slow: %1% frontiers per second") % blocks_per_sec));
