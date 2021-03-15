4a76da4bba09cf3a66e89960eec72c12c4f512a8
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-15 13:11:36 +0100

    fix indentation

--- nano/lib/ipc.hpp
+++ nano/lib/ipc.hpp
@@ -7,10 +7,10 @@
 namespace nano::ipc
 {
 /**
-	 * The IPC framing format is simple: preamble followed by an encoding specific payload.
-	 * Preamble is uint8_t {'N', encoding_type, reserved, reserved}. Reserved bytes MUST be zero.
-	 * @note This is intentionally not an enum class as the values are only used as vector indices.
-	 */
+ * The IPC framing format is simple: preamble followed by an encoding specific payload.
+ * Preamble is uint8_t {'N', encoding_type, reserved, reserved}. Reserved bytes MUST be zero.
+ * @note This is intentionally not an enum class as the values are only used as vector indices.
+ */
 enum preamble_offset
 {
 	/** Always 'N' */
@@ -34,9 +34,9 @@ public:
 	virtual void close () = 0;
 
 	/**
-		 * Start IO timer.
-		 * @param timeout_a Seconds to wait. To wait indefinitely, use std::chrono::seconds::max ()
-		 */
+     * Start IO timer.
+     * @param timeout_a Seconds to wait. To wait indefinitely, use std::chrono::seconds::max ()
+     */
 	void timer_start (std::chrono::seconds timeout_a);
 	void timer_expired ();
 	void timer_cancel ();
@@ -47,24 +47,24 @@ private:
 };
 
 /**
-	 * Payload encodings.
-	 */
+ * Payload encodings.
+ */
 enum class payload_encoding : uint8_t
 {
 	/**
-		 * Request is preamble followed by 32-bit BE payload length and payload bytes.
-		 * Response is 32-bit BE payload length followed by payload bytes.
-		 */
+     * Request is preamble followed by 32-bit BE payload length and payload bytes.
+     * Response is 32-bit BE payload length followed by payload bytes.
+     */
 	json_v1 = 0x1,
 
 	/** Request/response is same as json_v1, but exposes unsafe RPC's */
 	json_v1_unsafe = 0x2,
 
 	/**
-		 * Request is preamble followed by 32-bit BE payload length and payload bytes.
-		 * Response is 32-bit BE payload length followed by payload bytes.
-		 * Payloads must be flatbuffer encoded.
-		 */
+     * Request is preamble followed by 32-bit BE payload length and payload bytes.
+     * Response is 32-bit BE payload length followed by payload bytes.
+     * Payloads must be flatbuffer encoded.
+     */
 	flatbuffers = 0x3,
 
 	/** JSON -> Flatbuffers -> JSON  */
