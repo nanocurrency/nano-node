264d1167c91b9af3dd8acdd6974a6e485e6f6e7e
Sergey Kroshnin <sergiysw@gmail.com>
2021-05-05 14:20:42 +0300

    Change return details for "bootstrap_lazy" RPC (#3237)
    Change return from always ` { " started": "1" }`
    to more detailed status if new election was started or/and new lazy key was added
    ``` 
     { 
        " started": "1", 
        "key_inserted": "0"
     }
     ```

--- nano/node/json_handler.cpp
+++ nano/node/json_handler.cpp
@@ -1721,9 +1721,12 @@ void nano::json_handler::bootstrap_lazy ()
 	{
 		if (!node.flags.disable_lazy_bootstrap)
 		{
+			auto existed (node.bootstrap_initiator.current_lazy_attempt () != nullptr);
 			std::string bootstrap_id (request.get<std::string> ("id", ""));
-			node.bootstrap_initiator.bootstrap_lazy (hash, force, true, bootstrap_id);
-			response_l.put ("started", "1");
+			auto key_inserted (node.bootstrap_initiator.bootstrap_lazy (hash, force, true, bootstrap_id));
+			bool started = !existed && key_inserted;
+			response_l.put ("started", started ? "1" : "0");
+			response_l.put ("key_inserted", key_inserted ? "1" : "0");
 		}
 		else
 		{
