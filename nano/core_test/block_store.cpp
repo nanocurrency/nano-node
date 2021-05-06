3ce8a547e767aa0d9b1b39f6976acc6a190b0103
Sergey Kroshnin <sergiysw@gmail.com>
2021-05-06 11:36:42 +0300

    Iterator additional test

--- nano/core_test/block_store.cpp
+++ nano/core_test/block_store.cpp
@@ -516,16 +516,61 @@ TEST (block_store, unchecked_begin_search)
 	auto store = nano::make_store (logger, nano::unique_path ());
 	ASSERT_TRUE (!store->init_error ());
 	nano::keypair key0;
-	nano::send_block block1 (0, 1, 2, key0.prv, key0.pub, 3);
+	auto block1 (std::make_shared<nano::send_block> (0, 1, 2, key0.prv, key0.pub, 3));
 	auto block2 (std::make_shared<nano::send_block> (5, 6, 7, key0.prv, key0.pub, 8));
-	auto transaction (store->tx_begin_write ());
-	store->unchecked_put (transaction, block1.hash (), block2);
-	ASSERT_NE (store->unchecked_begin (transaction), store->unchecked_end ());
-	ASSERT_NE (store->unchecked_begin (transaction, nano::unchecked_key (block1.hash (), 0)), store->unchecked_end ());
-	auto unchecked (store->unchecked_begin (transaction, nano::unchecked_key (block1.hash (), 0)));
-	ASSERT_EQ (unchecked->first.key (), block1.hash ());
-	ASSERT_EQ (unchecked->first.hash, block2->hash ());
-	ASSERT_EQ (unchecked->second.block->hash (), block2->hash ());
+	{
+		auto transaction (store->tx_begin_write ());
+		store->unchecked_put (transaction, block1->hash (), block2);
+		ASSERT_NE (store->unchecked_begin (transaction), store->unchecked_end ());
+		ASSERT_NE (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), 0)), store->unchecked_end ());
+		ASSERT_EQ (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), 0)), store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), block2->hash ())));
+		auto unchecked (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), 0)));
+		ASSERT_EQ (unchecked->first.key (), block1->hash ());
+		ASSERT_EQ (unchecked->first.hash, block2->hash ());
+		ASSERT_EQ (unchecked->second.block->hash (), block2->hash ());
+		// Put 2nd block
+		store->unchecked_put (transaction, 0, block1);
+	}
+	{
+		// New read transaction & correct full key
+		auto transaction (store->tx_begin_read ());
+		ASSERT_NE (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), block2->hash ())), store->unchecked_end ());
+		ASSERT_EQ (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), 0)), store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), block2->hash ())));
+		auto unchecked (store->unchecked_begin (transaction, nano::unchecked_key (block1->hash (), block2->hash ())));
+		ASSERT_EQ (unchecked->first.key (), block1->hash ());
+		ASSERT_EQ (unchecked->first.hash, block2->hash ());
+		ASSERT_EQ (unchecked->second.block->hash (), block2->hash ());
+		// Test other unchecked block
+		auto unchecked_iter (store->unchecked_begin (transaction, nano::unchecked_key (0, block1->hash ())));
+		ASSERT_NE (unchecked_iter, store->unchecked_end ());
+		ASSERT_EQ (unchecked_iter->first.key (), 0);
+		ASSERT_EQ (unchecked_iter->first.hash, block1->hash ());
+		ASSERT_EQ (unchecked_iter->second.block->hash (), block1->hash ());
+		// Test iterator
+		++unchecked_iter;
+		ASSERT_NE (unchecked_iter, store->unchecked_end ());
+		ASSERT_EQ (unchecked_iter, unchecked);
+		ASSERT_EQ (unchecked_iter->first.key (), block1->hash ());
+		ASSERT_EQ (unchecked_iter->first.hash, block2->hash ());
+		ASSERT_EQ (unchecked_iter->second.block->hash (), block2->hash ());
+		// Test reverse iterator
+		--unchecked_iter;
+		ASSERT_NE (unchecked_iter, store->unchecked_end ());
+		ASSERT_NE (unchecked_iter, unchecked);
+		ASSERT_EQ (unchecked_iter->first.key (), 0);
+		ASSERT_EQ (unchecked_iter->first.hash, block1->hash ());
+		ASSERT_EQ (unchecked_iter->second.block->hash (), block1->hash ());
+	}
+	{
+		// For loop check (similar to RPC "unchecked_keys")
+		auto count (0);
+		auto transaction (store->tx_begin_read ());
+		for (auto i (store->unchecked_begin (transaction, nano::unchecked_key (0, 0))), n (store->unchecked_end ()); i != n; ++i)
+		{
+			++count;
+		}
+		ASSERT_EQ (2, count);
+	}
 }
 
 TEST (block_store, frontier_retrieval)
