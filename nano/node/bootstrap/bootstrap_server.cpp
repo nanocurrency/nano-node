d043be15994635a0e9b6496ed6b0e1a8307e7e11
Matthew King <matthewking@Matthews-MacBook-Pro.local>
2021-05-25 10:25:14 +0100

    Rework socket retrying based on expected responses from accept

--- nano/node/bootstrap/bootstrap_server.cpp
+++ nano/node/bootstrap/bootstrap_server.cpp
@@ -27,9 +27,12 @@ void nano::bootstrap_listener::start ()
 	}
 	debug_assert (node.network.endpoint ().port () == listening_socket->listening_port ());
 	listening_socket->on_connection ([this] (std::shared_ptr<nano::socket> const & new_connection, boost::system::error_code const & ec_a) {
-		bool keep_accepting = true;
+		if (ec_a)
+		{
+			return false;
+		}
 		accept_action (ec_a, new_connection);
-		return keep_accepting;
+		return true;
 	});
 }
 
