5d0df7ed7ba50f271fb2884686ca72250e0cb345
Sergey Kroshnin <sergiysw@gmail.com>
2021-06-15 18:39:36 +0300

    Add "successor" field to RPC "block_info" & "blocks_info"
    To simplify ledger walk with RPC & potentially explorers

--- nano/node/json_handler.cpp
+++ nano/node/json_handler.cpp
@@ -1062,6 +1062,7 @@ void nano::json_handler::block_info ()
 			response_l.put ("balance", balance.convert_to<std::string> ());
 			response_l.put ("height", std::to_string (block->sideband ().height));
 			response_l.put ("local_timestamp", std::to_string (block->sideband ().timestamp));
+			response_l.put ("successor", block->sideband ().successor.to_string ());
 			auto confirmed (node.ledger.block_confirmed (transaction, hash));
 			response_l.put ("confirmed", confirmed);
 
@@ -1217,6 +1218,7 @@ void nano::json_handler::blocks_info ()
 					entry.put ("balance", balance.convert_to<std::string> ());
 					entry.put ("height", std::to_string (block->sideband ().height));
 					entry.put ("local_timestamp", std::to_string (block->sideband ().timestamp));
+					entry.put ("successor", block->sideband ().successor.to_string ());
 					auto confirmed (node.ledger.block_confirmed (transaction, hash));
 					entry.put ("confirmed", confirmed);
 
