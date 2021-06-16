cb34acc3aa37924bc1f5b845a86b3e2900f6ffeb
dsiganos <dimitris@siganos.org>
2021-06-16 14:50:35 +0100

    Fix code formatting (#3341)

--- nano/rpc_test/rpc.cpp
+++ nano/rpc_test/rpc.cpp
@@ -7,8 +7,8 @@
 #include <nano/node/node_rpc_config.hpp>
 #include <nano/rpc/rpc.hpp>
 #include <nano/rpc/rpc_request_processor.hpp>
-#include <nano/test_common/telemetry.hpp>
 #include <nano/test_common/system.hpp>
+#include <nano/test_common/telemetry.hpp>
 #include <nano/test_common/testutil.hpp>
 
 #include <gtest/gtest.h>
