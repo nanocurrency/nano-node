20fcdd4946c94bcba0bb819a064cba967b2b1eed
clemahieu <clemahieu@gmail.com>
2021-06-29 11:06:51 +0100

    System initializer (#3332)
    Moving nano::system tests in to their own file for better organization.
    
    Adding initializer function to nano::system::ledger_initialization_set to initialize node ledgers with an evenly-weighted set of representatives. This allows a lot of boilerplate code to be eliminated when constructing unit tests.

--- nano/test_common/system.hpp
+++ nano/test_common/system.hpp
@@ -19,6 +19,7 @@ public:
 	system ();
 	system (uint16_t, nano::transport::transport_type = nano::transport::transport_type::tcp, nano::node_flags = nano::node_flags ());
 	~system ();
+	void ledger_initialization_set (std::vector<nano::keypair> const & reps, nano::amount const & reserve = 0);
 	void generate_activity (nano::node &, std::vector<nano::account> &);
 	void generate_mass_activity (uint32_t, nano::node &);
 	void generate_usage_traffic (uint32_t, uint32_t, size_t);
@@ -53,6 +54,8 @@ public:
 	std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> deadline{ std::chrono::steady_clock::time_point::max () };
 	double deadline_scaling_factor{ 1.0 };
 	unsigned node_sequence{ 0 };
+private:
+	std::vector<std::shared_ptr<nano::block>> initialization_blocks;
 };
 std::unique_ptr<nano::state_block> upgrade_epoch (nano::work_pool &, nano::ledger &, nano::epoch);
 void blocks_confirm (nano::node &, std::vector<std::shared_ptr<nano::block>> const &, bool const = false);
