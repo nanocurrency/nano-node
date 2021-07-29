20b9b47f3c3456e90948ab616908bce3c149ed96
clemahieu <clemahieu@gmail.com>
2021-07-29 11:34:57 +0100

    Removing static mutex for the random pool and replacing the random pool with a thread_local version so it is thread safe.

--- nano/crypto_lib/random_pool.cpp
+++ nano/crypto_lib/random_pool.cpp
@@ -2,31 +2,26 @@
 
 #include <crypto/cryptopp/osrng.h>
 
-std::mutex nano::random_pool::mutex;
-
 void nano::random_pool::generate_block (unsigned char * output, size_t size)
 {
 	auto & pool = get_pool ();
-	std::lock_guard guard (mutex);
 	pool.GenerateBlock (output, size);
 }
 
 unsigned nano::random_pool::generate_word32 (unsigned min, unsigned max)
 {
 	auto & pool = get_pool ();
-	std::lock_guard guard (mutex);
 	return pool.GenerateWord32 (min, max);
 }
 
 unsigned char nano::random_pool::generate_byte ()
 {
 	auto & pool = get_pool ();
-	std::lock_guard guard (mutex);
 	return pool.GenerateByte ();
 }
 
 CryptoPP::AutoSeededRandomPool & nano::random_pool::get_pool ()
 {
-	static CryptoPP::AutoSeededRandomPool pool;
+	static thread_local CryptoPP::AutoSeededRandomPool pool;
 	return pool;
 }
