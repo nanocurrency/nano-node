cc302192427bb350eb2e90941cd6b1c3fe1288f9
clemahieu <clemahieu@gmail.com>
2021-07-29 10:20:29 +0100

    Using FQN to reference network_params.

--- nano/secure/common.cpp
+++ nano/secure/common.cpp
@@ -82,7 +82,7 @@ std::string const test_canary_public_key_data = nano::get_env_or_default ("NANO_
 
 nano::keypair nano::dev::genesis_key{ dev_private_key_data };
 nano::network_params nano::dev::network_params{ nano::networks::nano_dev_network };
-nano::ledger_constants & nano::dev::constants{ network_params.ledger };
+nano::ledger_constants & nano::dev::constants{ nano::dev::network_params.ledger };
 std::shared_ptr<nano::block> & nano::dev::genesis = nano::dev::constants.genesis;
 
 nano::network_params::network_params () :
