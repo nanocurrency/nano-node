578926f5b5d4018b9f1e61a8c308a57f160452ff
coranos <coranos@users.noreply.github.com>
2021-08-30 07:21:49 -0500

    fixing actions

--- ci/actions/linux/ghcr_push.sh
+++ ci/actions/linux/ghcr_push.sh
@@ -2,10 +2,11 @@
 set -e
 
 scripts="$PWD/ci"
+LOWER_GITHUB_REPOSITORY = echo "${GITHUB_REPOSITORY}" | tr '[:lower:]'
 if [[ "$GITHUB_WORKFLOW" = "Develop" ]]; then
-    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:base"
-    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:gcc"
-    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:clang-6"
+    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${LOWER_GITHUB_REPOSITORY}/nano-env:base"
+    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${LOWER_GITHUB_REPOSITORY}/nano-env:gcc"
+    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${LOWER_GITHUB_REPOSITORY}/nano-env:clang-6"
 else
     tags=$(docker images --format '{{.Repository}}:{{.Tag }}' | grep "ghcr.io" | grep -vE "env|none")
     for a in $tags; do
