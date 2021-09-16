75149ef6b5fbc9fa1029992a4434b3e9900e01da
theohax <81556890+theohax@users.noreply.github.com>
2021-09-16 15:19:11 +0300

    Fix CI clang-format script to tell if clang-format cannot be found (#3450)

--- ci/clang-format-all.sh
+++ ci/clang-format-all.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
-cd "${REPO_ROOT}"
-./ci/update-clang-format
-find nano -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
+CLANG_FORMAT="clang-format"
+if [ $(builtin type -p "$CLANG_FORMAT") ]; then
+	REPO_ROOT=$(git rev-parse --show-toplevel)
+	cd "$REPO_ROOT"
+	./ci/update-clang-format
+	find nano -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs "$CLANG_FORMAT" -i
+else
+	echo "'$CLANG_FORMAT' could not be detected in your PATH. Do you have it installed?"
+fi
