a6e36c90fd0a49718c5bc71a7145a3bf2704bf0e
theohax <theo@nano.org>
2021-09-16 18:21:19 +0300

    Some CI scripts refactoring + fixes for paths with spaces

--- ci/check-commit-format.sh
+++ ci/check-commit-format.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-source $(dirname $BASH_SOURCE)/detect-clang-format.sh
-source $(dirname $BASH_SOURCE)/common.sh
+source "$(dirname "$BASH_SOURCE")/detect-clang-format.sh"
+source "$(dirname "$BASH_SOURCE")/common.sh"
 
 "$REPO_ROOT/ci/update-clang-format"
 
-RESULT=$(python $REPO_ROOT/ci/git-clang-format.py --diff --extensions "hpp,cpp")
-if [ "$RESULT" != "no modified files to format" ] && [ "$RESULT" != "clang-format did not modify any files" ]; then
-    python $REPO_ROOT/ci/git-clang-format.py --diff --extensions "hpp,cpp"
+clang_format_result=$(python "$REPO_ROOT/ci/git-clang-format.py" --diff --extensions "hpp,cpp")
+if [[ $clang_format_result != "no modified files to format" ]] &&
+   [[ $clang_format_result != "clang-format did not modify any files" ]]; then
+    python "$REPO_ROOT/ci/git-clang-format.py" --diff --extensions "hpp,cpp"
     echo
     echo "Code formatting differs from expected - please run ci/clang-format-all.sh"
     exit 1
--- ci/detect-clang-format.sh
+++ ci/detect-clang-format.sh
@@ -2,17 +2,40 @@
 
 set -e
 
-CLANG_FORMAT="clang-format-12"
+is_clang_format_usable()
+{
+    if [[ $(builtin type -p $1) ]]; then
+        local output=$($1 --version)
+        if [[ $output =~ ^(.)*clang-format\ version\ $2(.)*$ ]]; then
+            echo "0"
+        else
+            echo $output
+        fi
+    else
+        echo "1"
+    fi
+}
+
+CLANG_FORMAT=""
 CLANG_FORMAT_VERSION="12"
 
-if ! [ $(builtin type -p "$CLANG_FORMAT") ]; then
-	echo "'$CLANG_FORMAT' could not be detected in your PATH. Do you have it installed?"
-	exit 1
-fi
+clang_format_attempts=("clang-format"
+                       "clang-format-$CLANG_FORMAT_VERSION")
 
-VERSION_OUTPUT=$($CLANG_FORMAT --version)
-if ! [[ $VERSION_OUTPUT =~ ^(.)*clang-format\ version\ $CLANG_FORMAT_VERSION(.)*$ ]]; then
-	echo "Your '$CLANG_FORMAT' version is not '$CLANG_FORMAT_VERSION', but '$VERSION_OUTPUT'." \
-	     "Please up/down grade it."
-	exit 1
+for itr in ${clang_format_attempts[@]}; do
+    result=$(is_clang_format_usable $itr $CLANG_FORMAT_VERSION)
+    if [[ $result == "0" ]]; then
+        CLANG_FORMAT=$itr
+        break
+    elif [[ $result == "1" ]]; then
+        continue
+    else
+        echo "Detected '$itr' with version '$result' " \
+             "(different than '$CLANG_FORMAT_VERSION'), skipping it."
+    fi
+done
+
+if [[ -z $CLANG_FORMAT ]]; then
+    echo "No 'clang-format' of version '$CLANG_FORMAT_VERSION' could be detected in your PATH."
+    exit 1
 fi
