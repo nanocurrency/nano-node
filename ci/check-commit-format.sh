bbf9bc9c6c8e3cd61e70d2715edda0f64ce1d202
theohax <theo@nano.org>
2021-09-21 18:44:13 +0300

    Improve the detection of non-clang-format-compliant commits/diffs

--- ci/check-commit-format.sh
+++ ci/check-commit-format.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-source "$(dirname "$BASH_SOURCE")/detect-clang-format.sh"
+if [[ ! -z $(git status --untracked-files=no --porcelain) ]]; then
+    echo "Unable to run script: working directory not clean (see git status)"
+    exit 1
+fi
+
 source "$(dirname "$BASH_SOURCE")/common.sh"
 
-"$REPO_ROOT/ci/update-clang-format"
+"$REPO_ROOT/ci/clang-format-all.sh"
 
-clang_format_result=$(python "$REPO_ROOT/ci/git-clang-format.py" --diff --extensions "hpp,cpp")
-if [[ $clang_format_result != "no modified files to format" ]] &&
-   [[ $clang_format_result != "clang-format did not modify any files" ]]; then
-    python "$REPO_ROOT/ci/git-clang-format.py" --diff --extensions "hpp,cpp"
-    echo
+if [[ ! -z $(git status --untracked-files=no --porcelain) ]]; then
     echo "Code formatting differs from expected - please run ci/clang-format-all.sh"
+    git reset --hard HEAD > /dev/null
     exit 1
-else
-    echo "clang-format passed"
-    exit 0
 fi
+
+echo "clang-format passed"
+exit 0
--- ci/clang-format-all.sh
+++ ci/clang-format-all.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-source $(dirname $BASH_SOURCE)/detect-clang-format.sh
-source $(dirname $BASH_SOURCE)/common.sh
+source "$(dirname "$BASH_SOURCE")/detect-clang-format.sh"
+source "$(dirname "$BASH_SOURCE")/common.sh"
 
-cd "$REPO_ROOT"
-./ci/update-clang-format
-find nano -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs "$CLANG_FORMAT" -i
+"$REPO_ROOT/ci/update-clang-format"
+
+find "$REPO_ROOT/nano" -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs "$CLANG_FORMAT" -i
