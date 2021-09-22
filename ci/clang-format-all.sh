ab8f57acd68e7ce26737b93f0b5e6b9ca0c9cd52
theohax <theo@nano.org>
2021-09-22 18:58:43 +0300

    Fix calling xargs on OSX

--- ci/clang-format-all.sh
+++ ci/clang-format-all.sh
@@ -5,4 +5,4 @@ set -e
 source "$(dirname "$BASH_SOURCE")/detect-clang-format.sh"
 source "$(dirname "$BASH_SOURCE")/common.sh"
 
-find "$REPO_ROOT/nano" -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs "$CLANG_FORMAT" -i -style=file
+find "$REPO_ROOT/nano" -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs -I source "$CLANG_FORMAT" -i -style=file "source"
