d23344fd116b19a6ab60b91d2ae5fda914a8535b
James Coxon <175479+jamescoxon@users.noreply.github.com>
2021-12-04 22:53:43 +0000

    Include election.confirmed outcome in Node log
    Noted by @RickiNano on beta-testing that not clear outcome of election (confirmed or dropped) by reviewing logs. Added true/false to log string.
    
    Co-authored-by: RickiNano

--- nano/node/active_transactions.cpp
+++ nano/node/active_transactions.cpp
@@ -382,7 +382,7 @@ void nano::active_transactions::cleanup_election (nano::unique_lock<nano::mutex>
 			node.network.publish_filter.clear (block);
 		}
 	}
-	node.logger.try_log (boost::str (boost::format ("Election erased for root %1%") % election.qualified_root.to_string ()));
+	node.logger.try_log (boost::str (boost::format ("Election erased for root %1%, confirmed: %2$b") % election.qualified_root.to_string () % election.confirmed ()));
 }
 
 std::vector<std::shared_ptr<nano::election>> nano::active_transactions::list_active (std::size_t max_a)
