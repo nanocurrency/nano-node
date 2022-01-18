b6b400eb9ea6d1f23f5336886ae6d6ab92ed9860
Thiago Silva <thiago@nano.org>
2022-01-18 15:17:58 -0300

    Removes code inspection from build-travis and refactors code-inspector so it can be reused

--- ci/build-travis.sh
+++ ci/build-travis.sh
@@ -7,24 +7,9 @@ set -o nounset
 set -o xtrace
 OS=$(uname)
 
-# This is to prevent out of scope access in async_write from asio which is not picked up by static analysers
-if [[ $(grep -rl --exclude="*asio.hpp" "asio::async_write" ./nano) ]]; then
-    echo "Using boost::asio::async_write directly is not permitted (except in nano/lib/asio.hpp). Use nano::async_write instead"
-    exit 1
-fi
+source "$(dirname "$BASH_SOURCE")/impl/code-inspector.sh"
+code_inspect "${ROOTPATH:-.}"
 
-# prevent unsolicited use of std::lock_guard, std::unique_lock, std::condition_variable & std::mutex outside of allowed areas
-if [[ $(grep -rl --exclude={"*random_pool.cpp","*random_pool.hpp","*random_pool_shuffle.hpp","*locks.hpp","*locks.cpp"} "std::unique_lock\|std::lock_guard\|std::condition_variable\|std::mutex" ./nano) ]]; then
-    echo "Using std::unique_lock, std::lock_guard, std::condition_variable or std::mutex is not permitted (except in nano/lib/locks.hpp and non-nano dependent libraries). Use the nano::* versions instead"
-    exit 1
-fi
-
-if [[ $(grep -rlP "^\s*assert \(" ./nano) ]]; then
-    echo "Using assert is not permitted. Use debug_assert instead."
-    exit 1
-fi
-
-# prevent unsolicited use of std::lock_guard & std::unique_lock outside of allowed areas
 mkdir build
 pushd build
 
--- ci/code-inspector-check.sh
+++ ci/code-inspector-check.sh
@@ -3,30 +3,15 @@
 ###################################################################################################
 
 source "$(dirname "$BASH_SOURCE")/impl/common.sh"
+source "$(dirname "$BASH_SOURCE")/impl/code-inspector.sh"
 
 ###################################################################################################
 
 set -o errexit
 set -o nounset
 
-# This is to prevent out of scope access in async_write from asio which is not picked up by static analysers
-if [[ $(grep -rl --exclude="*asio.hpp" "asio::async_write" $ROOTPATH/nano) ]]; then
-    echo "Using boost::asio::async_write directly is not permitted (except in nano/lib/asio.hpp). Use nano::async_write instead"
-    exit 1
-fi
-
-# prevent unsolicited use of std::lock_guard, std::unique_lock, std::condition_variable & std::mutex outside of allowed areas
-if [[ $(grep -rl --exclude={"*random_pool.cpp","*random_pool.hpp","*random_pool_shuffle.hpp","*locks.hpp","*locks.cpp"} "std::unique_lock\|std::lock_guard\|std::condition_variable\|std::mutex" $ROOTPATH/nano) ]]; then
-    echo "Using std::unique_lock, std::lock_guard, std::condition_variable or std::mutex is not permitted (except in nano/lib/locks.hpp and non-nano dependent libraries). Use the nano::* versions instead"
-    exit 1
-fi
-
-if [[ $(grep -rlP "^\s*assert \(" $ROOTPATH/nano) ]]; then
-    echo "Using assert is not permitted. Use debug_assert instead."
-    exit 1
-fi
+code_inspect "${ROOTPATH:-.}"
 
 echo "code-inspector check passed"
-
 
 ###################################################################################################
