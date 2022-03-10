8db9b39bfd237d910f7f1ae7a64618668a985872
Justin Randall <justin@playscale.io>
2022-03-11 02:41:56 +0800

    Fix Issue #3748 (#3749)
    * Fix Issue #3748
    Sorted command line options output
    
    * Missing link library failing tests

--- nano/lib/utility.cpp
+++ nano/lib/utility.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/dll/runtime_symbol_info.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 #include <cstddef>
 #include <iostream>
@@ -195,4 +196,24 @@ void assert_internal (char const * check_expr, char const * func, char const * f
 #endif
 
 	abort ();
+}
+
+// Issue #3748
+void nano::sort_options_description (const boost::program_options::options_description & source, boost::program_options::options_description & target)
+{
+	// Grab all of the options, get the option display name, stick it in a map using the display name as
+	// the key (the map will sort) and the value as the option itself.
+	const auto & options = source.options ();
+	std::map<std::string, boost::shared_ptr<boost::program_options::option_description>> sorted_options;
+	for (const auto & option : options)
+	{
+		auto pair = std::make_pair (option->canonical_display_name (2), option);
+		sorted_options.insert (pair);
+	}
+
+	// Rebuild for display purposes only.
+	for (const auto & option_pair : sorted_options)
+	{
+		target.add (option_pair.second);
+	}
 }
