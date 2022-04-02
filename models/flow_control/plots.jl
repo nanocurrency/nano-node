048190fb2061185f5ad8818b41dd64b2954e11dd
clemahieu <clemahieu@gmail.com>
2022-04-02 02:36:48 +0100

    More reorganizing.

--- models/flow_control/plots.jl
+++ models/flow_control/plots.jl
@@ -1,5 +1,3 @@
-
-
 function stress(node_count, bucket_count, bucket_max; type = transaction_type_default)
     series = []
     n = network(node_count = node_count, bucket_count = bucket_count, bucket_max = bucket_max, type = type)
@@ -10,24 +8,42 @@ function stress(node_count, bucket_count, bucket_max; type = transaction_type_de
     (n, series)
 end
 
-function plot_bucket_count()
+function plot_type()
+    types = [UInt8, UInt16, UInt32, UInt64]
+    ys = []
+    x = 1:10_000
+    labels = []
+    for type = types
+        n = network(type = type)
+        series = []
+        for _ in x
+            mutate(n)
+            push!(series, n.stats.deleted)
+        end
+        push!(ys, series)
+        push!(labels, string(type))
+    end
+    labels = permutedims(labels)
+    Plots.plot(x, ys, label = labels, xlabel = "Operations", ylabel = "Confirmed")
+    # Asymptote should drive a type for transaction_type_default.
+end
+
+function plot_node_count_iterations()
     y = []
-    #x = map((val) -> 2^val, 2:8)
-    x = 1:64
-    iteration_count = 5_000
-    for bucket_count = x
-        print(bucket_count, ' ')
-        n = network(bucket_count = bucket_count)
+    #x = collect(2^val for val = 2:5)
+    #x = collect(2^val for val = 2:8)
+    x = 4:64
+    for i = x
+        n = network(node_count = i)
         count = 0
-        while n.stats.deleted < iteration_count
+        print(i, ' ')
+        while n.stats.deleted == 0
             mutate(n)
             count += 1
         end
         push!(y, count)
     end
-    Plots.plot(x, y, title = "Operations per confirmations(" * string(iteration_count) * ") by bucket count", xlabel = "Bucket max", ylabel = "Operations")
-    # Asymptote should drive a value for bucket_count_default. Smaller gives better simulation throughput.
-
+    Plots.plot(x, y, title = "Operations per confirmation by node count", xlabel = "Nodes", ylabel = "Operations")
 end
 
 function plot_bucket_max()
@@ -49,42 +65,24 @@ function plot_bucket_max()
     # Asymptote should drive a value for bucket_max_default. Smaller gives better simulation throughput.
 end
 
-function plot_node_count_iterations()
+function plot_bucket_count()
     y = []
-    #x = collect(2^val for val = 2:5)
-    #x = collect(2^val for val = 2:8)
-    x = 4:64
-    for i = x
-        n = network(node_count = i)
+    #x = map((val) -> 2^val, 2:8)
+    x = 1:64
+    iteration_count = 5_000
+    for bucket_count = x
+        print(bucket_count, ' ')
+        n = network(bucket_count = bucket_count)
         count = 0
-        print(i, ' ')
-        while n.stats.deleted == 0
+        while n.stats.deleted < iteration_count
             mutate(n)
             count += 1
         end
         push!(y, count)
     end
-    Plots.plot(x, y, title = "Operations per confirmation by node count", xlabel = "Nodes", ylabel = "Operations")
-end
+    Plots.plot(x, y, title = "Operations per confirmations(" * string(iteration_count) * ") by bucket count", xlabel = "Bucket max", ylabel = "Operations")
+    # Asymptote should drive a value for bucket_count_default. Smaller gives better simulation throughput.
 
-function plot_type()
-    types = [UInt8, UInt16, UInt32, UInt64]
-    ys = []
-    x = 1:10_000
-    labels = []
-    for type = types
-        n = network(type = type)
-        series = []
-        for _ in x
-            mutate(n)
-            push!(series, n.stats.deleted)
-        end
-        push!(ys, series)
-        push!(labels, string(type))
-    end
-    labels = permutedims(labels)
-    Plots.plot(x, ys, label = labels, xlabel = "Operations", ylabel = "Confirmed")
-    # Asymptote should drive a type for transaction_type_default.
 end
 
 function plots()
