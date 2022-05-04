f1ab7681202d96d88e5d0d1cb7c73b1b8da3ad40
Dimitrios Siganos <dimitris@siganos.org>
2022-05-04 15:45:05 +0100

    Call the callback function and add some comments

--- nano/node/transport/inproc.cpp
+++ nano/node/transport/inproc.cpp
@@ -23,6 +23,10 @@ bool nano::transport::inproc::channel::operator== (nano::transport::channel cons
 	return endpoint == other_a.get_endpoint ();
 }
 
+/**
+ *  This function is called for every message received by the inproc channel.
+ *  Note that it is called from inside the context of nano::transport::inproc::channel::send_buffer
+ */
 class message_visitor_inbound : public nano::message_visitor
 {
 public:
@@ -31,8 +35,12 @@ public:
 		channel{ channel }
 	{
 	}
+
 	decltype (nano::network::inbound) & inbound;
+
+	// the channel to reply to, if a reply is generated
 	std::shared_ptr<nano::transport::inproc::channel> channel;
+
 	void keepalive (nano::keepalive const & message)
 	{
 		inbound (message, channel);
@@ -79,13 +87,31 @@ public:
 	}
 };
 
+/**
+ * Send the buffer to the peer and call the callback function when done. The call never fails.
+ * Note that the inbound message visitor will be called before the callback because it is called directly whereas the callback is spawned in the background.
+ */
 void nano::transport::inproc::channel::send_buffer (nano::shared_const_buffer const & buffer_a, std::function<void (boost::system::error_code const &, std::size_t)> const & callback_a, nano::buffer_drop_policy drop_policy_a)
 {
+	// we create a temporary channel for the reply path, in case the receiver of the message wants to reply
 	auto remote_channel = std::make_shared<nano::transport::inproc::channel> (destination, node);
+
+	// create an inbound message visitor class to handle incoming messages because that's what the message parser expects
 	message_visitor_inbound visitor{ destination.network.inbound, remote_channel };
+
 	nano::message_parser parser{ destination.network.publish_filter, destination.block_uniquer, destination.vote_uniquer, visitor, destination.work, destination.network_params.network };
+
+	// parse the message and action any work that needs to be done on that object via the visitor object
 	auto bytes = buffer_a.to_bytes ();
-	parser.deserialize_buffer (bytes.data (), bytes.size ());
+	auto size = bytes.size ();
+	parser.deserialize_buffer (bytes.data (), size);
+
+	if (callback_a)
+	{
+		node.background ([callback_a, size] () {
+			callback_a (boost::system::errc::make_error_code (boost::system::errc::success), size);
+		});
+	}
 }
 
 std::string nano::transport::inproc::channel::to_string () const
