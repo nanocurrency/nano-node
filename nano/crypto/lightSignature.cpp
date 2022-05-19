b2fa29d29a8c8508bc4a27f23eb3d9d052db81ba
NoSecretImprove <thedoom@villanclan.com>
2022-05-20 00:28:00 +0200

    Prepare for signatures

---
+++ nano/crypto/lightSignature.cpp
@@ -0,0 +1,8 @@
+#include <crypto/blake2/blake2.h>
+#include <stdlib.h>
+
+unsigned char *signMsg(unsigned char *Key, unsigned char *msg, unsigned long long msgHeight) {
+    unsigned char *Signature = (unsigned char *)malloc(32);
+
+    return NULL;
+}
