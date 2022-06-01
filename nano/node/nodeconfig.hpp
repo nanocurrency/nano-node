abc40ba82a9a03ad98d389619a7e30454399f948
fikumikudev <3044353+fikumikudev@users.noreply.github.com>
2022-06-01 19:45:22 +0200

    Reenable inactive vote cache with more aggressive threshold (big thanks to @trashman for tuning those parameters)

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -56,7 +56,7 @@ public:
 	std::chrono::milliseconds vote_generator_delay{ std::chrono::milliseconds (100) };
 	unsigned vote_generator_threshold{ 3 };
 	nano::amount online_weight_minimum{ 60000 * nano::Gxrb_ratio };
-	unsigned election_hint_weight_percent{ 10 };
+	unsigned election_hint_weight_percent{ 50 };
 	unsigned password_fanout{ 1024 };
 	unsigned io_threads{ std::max<unsigned> (4, std::thread::hardware_concurrency ()) };
 	unsigned network_threads{ std::max<unsigned> (4, std::thread::hardware_concurrency ()) };
@@ -158,7 +158,7 @@ public:
 	std::size_t block_processor_batch_size{ 0 };
 	std::size_t block_processor_full_size{ 65536 };
 	std::size_t block_processor_verification_size{ 0 };
-	std::size_t inactive_votes_cache_size{ 0 };
+	std::size_t inactive_votes_cache_size{ 1024 * 128 };
 	std::size_t vote_processor_capacity{ 144 * 1024 };
 	std::size_t bootstrap_interval{ 0 }; // For testing only
 };
