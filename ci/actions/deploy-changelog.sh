a2c1b5ea985bc9d355b3478d2803967ac2c8dc48
Thiago Silva <thiago@nano.org>
2022-06-08 21:48:09 -0300

    Allow changelog output directory in secrets

--- ci/actions/deploy-changelog.sh
+++ ci/actions/deploy-changelog.sh
@@ -11,4 +11,11 @@ if [[ -z "$TAG" ]]; then
   exit 1
 fi
 
-aws s3 cp $GITHUB_WORKSPACE/artifacts/CHANGELOG.md s3://repo.nano.org/changelogs/CHANGELOG-${TAG}.md --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+BUILD="changelogs"
+if [[ "${GITHUB_REPOSITORY:-}" == "nanocurrency/nano-node" ]]; then
+    DIRECTORY=$BUILD
+else
+    DIRECTORY="${S3_BUILD_DIRECTORY}/${BUILD}"
+fi
+
+aws s3 cp $GITHUB_WORKSPACE/artifacts/CHANGELOG.md s3://repo.nano.org/$DIRECTORY/CHANGELOG-${TAG}.md --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
