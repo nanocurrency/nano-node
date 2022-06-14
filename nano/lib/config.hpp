193bf44262ff0133136823c36c42dc994f721538
Piotr Wójcik <3044353+fikumikudev@users.noreply.github.com>
2022-06-14 19:29:37 +0200

    Add configuration environment variable for how often to check representatives in local wallet

--- nano/lib/config.hpp
+++ nano/lib/config.hpp
@@ -20,8 +20,8 @@ namespace filesystem
 #define ver_str(a) #a
 
 /**
-* Returns build version information
-*/
+ * Returns build version information
+ */
 char const * const NANO_VERSION_STRING = xstr (TAG_VERSION_STRING);
 char const * const NANO_MAJOR_VERSION_STRING = xstr (MAJOR_VERSION_STRING);
 char const * const NANO_MINOR_VERSION_STRING = xstr (MINOR_VERSION_STRING);
@@ -59,6 +59,10 @@ uint16_t test_rpc_port ();
 uint16_t test_ipc_port ();
 uint16_t test_websocket_port ();
 std::array<uint8_t, 2> test_magic_number ();
+/*
+ * How often to scan for representatives in local wallet, in milliseconds
+ */
+uint32_t test_scan_wallet_reps_delay ();
 
 /**
  * Network variants with different genesis blocks and network parameters
