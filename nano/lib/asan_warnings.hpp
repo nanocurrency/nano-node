3b4b6a4fa121040f3811f2af12f3f5b800bcd0ab
Cathal Tummon <cstummon@hotmail.com>
2022-06-21 10:11:39 +0200

    Run cmake-format, add missing #defines

--- nano/lib/asan_warnings.hpp
+++ nano/lib/asan_warnings.hpp
@@ -2,7 +2,11 @@
 #if defined(__has_feature)
 #if __has_feature(address_sanitizer)
 #define ATTRIBUTE_NO_SANITIZE_UINT_OVERFLOW __attribute__ ((no_sanitize ("unsigned-integer-overflow")))
+#else
+#define ATTRIBUTE_NO_SANITIZE_UINT_OVERFLOW
 #endif
+#else
+#define ATTRIBUTE_NO_SANITIZE_UINT_OVERFLOW
 #endif
 #else
 #define ATTRIBUTE_NO_SANITIZE_UINT_OVERFLOW
