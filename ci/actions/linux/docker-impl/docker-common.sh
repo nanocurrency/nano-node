9a5229b759774d28b71619644fc7e150c3d6c24d
Thiago Silva <thiago@nano.org>
2022-06-27 15:11:29 -0300

    Update docker-common.sh with the CI_TAG var name

--- ci/actions/linux/docker-impl/docker-common.sh
+++ ci/actions/linux/docker-impl/docker-common.sh
@@ -4,13 +4,13 @@ set -e
 set -a
 
 scripts="$PWD/ci"
-TRAVIS_BRANCH=$(git branch | cut -f2 -d' ')
+CI_BRANCH=$(git branch | cut -f2 -d' ')
 tags=()
-if [ -n "$TRAVIS_TAG" ]; then
-    tags+=("$TRAVIS_TAG")
-elif [ -n "$TRAVIS_BRANCH" ]; then
-    TRAVIS_TAG=$TRAVIS_BRANCH
-    tags+=("$TRAVIS_BRANCH")
+if [ -n "$CI_TAG" ]; then
+    tags+=("$CI_TAG")
+elif [ -n "$CI_BRANCH" ]; then
+    CI_TAG=$CI_BRANCH
+    tags+=("$CI_BRANCH")
 fi
 if [[ "$GITHUB_WORKFLOW" = "Live" ]]; then
     echo "Live"
@@ -34,7 +34,7 @@ docker_build()
 {
     if [[ "$GITHUB_WORKFLOW" != "Develop" ]]; then
         ghcr_image_name="ghcr.io/${GITHUB_REPOSITORY}/nano${network_tag_suffix}"
-        "$scripts"/build-docker-image.sh docker/node/Dockerfile "$docker_image_name" --build-arg NETWORK="$network" --build-arg CI_BUILD=true --build-arg TRAVIS_TAG="$TRAVIS_TAG"
+        "$scripts"/build-docker-image.sh docker/node/Dockerfile "$docker_image_name" --build-arg NETWORK="$network" --build-arg CI_BUILD=true --build-arg CI_TAG="$CI_TAG"
         for tag in "${tags[@]}"; do
             # Sanitize docker tag
             # https://docs.docker.com/engine/reference/commandline/tag/
