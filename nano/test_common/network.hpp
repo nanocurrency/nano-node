cb707cf19c428aa34237e7aa9ee73bd9ee956a27
Thiago Silva <thiago@nano.org>
2022-07-19 10:25:14 -0300

    Simple client/server sockets for unit tests

--- nano/test_common/network.hpp
+++ nano/test_common/network.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <nano/boost/asio/read.hpp>
+#include <nano/boost/asio/write.hpp>
 #include <nano/node/common.hpp>
+#include <nano/test_common/system.hpp>
+
+#include <boost/asio/streambuf.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <string>
 
 namespace nano
 {
@@ -14,8 +22,11 @@ namespace transport
 
 namespace test
 {
-	class system;
-	/** Waits until a TCP connection is established and returns the TCP channel on success*/
-	std::shared_ptr<nano::transport::channel_tcp> establish_tcp (nano::test::system &, nano::node &, nano::endpoint const &);
+  class system;
+  /** Waits until a TCP connection is established and returns the TCP channel on success*/
+  std::shared_ptr<nano::transport::channel_tcp> establish_tcp (nano::test::system &, nano::node &, nano::endpoint const &);
+
+  /** Adds a node to the system without establishing connections */
+  std::shared_ptr<nano::node> add_outer_node (nano::test::system & system, uint16_t port_a = nano::test::get_available_port ());
 }
 }
