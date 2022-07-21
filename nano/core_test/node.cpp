c713e991977102a663e692b6bd4a9b99f201f79c
Thiago Silva <thiago@nano.org>
2022-07-21 19:20:38 -0300

    Migrate tests from UDP to TCP (core_test/bootstrap.cpp)

--- nano/core_test/node.cpp
+++ nano/core_test/node.cpp
@@ -1,6 +1,5 @@
 #include <nano/node/election.hpp>
 #include <nano/node/transport/inproc.hpp>
-#include <nano/node/transport/udp.hpp>
 #include <nano/test_common/network.hpp>
 #include <nano/test_common/system.hpp>
 #include <nano/test_common/testutil.hpp>
