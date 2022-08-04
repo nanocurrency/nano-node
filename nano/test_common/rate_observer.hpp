73b5ebc1fa624ee91747b03c41fa7d95f5b9fa79
Piotr Wójcik <3044353+fikumikudev@users.noreply.github.com>
2022-08-05 00:05:57 +0200

    Formatting & Comments

--- nano/test_common/rate_observer.hpp
+++ nano/test_common/rate_observer.hpp
@@ -8,65 +8,71 @@
 #include <utility>
 #include <vector>
 
-namespace nano
+namespace nano::test
 {
-namespace test
+class rate_observer
 {
-	class rate_observer
+public:
+	/*
+	 * Base class used as a base to build counters
+	 */
+	class counter
 	{
 	public:
 		/*
-		 * Base class used as a base to build counters
+		 * Calculate count and time delta since last call
 		 */
-		class counter
-		{
-		public:
-			std::pair<uint64_t, std::chrono::milliseconds> observe ();
+		std::pair<uint64_t, std::chrono::milliseconds> observe ();
 
-			virtual uint64_t count () = 0;
-			virtual std::string name () = 0;
-
-		private:
-			std::chrono::system_clock::time_point last_observation{};
-		};
-
-		/*
-		 * Counter that uses node stat container to provide info about rate
-		 */
-		class stat_counter final : public counter
-		{
-		public:
-			explicit stat_counter (nano::stat & stats, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
-
-			uint64_t count () override;
-			std::string name () override;
-
-		private:
-			const nano::stat::type type;
-			const nano::stat::detail detail;
-			const nano::stat::dir dir;
-
-			uint64_t last_count{ 0 };
-
-			nano::stat & stats;
-		};
-
-	public:
-		rate_observer () = default;
-		~rate_observer ();
-
-		void background_print (std::chrono::seconds interval);
-
-		void observe (nano::node &, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
+		virtual uint64_t count () = 0;
+		virtual std::string name () = 0;
 
 	private:
-		void background_print_impl (std::chrono::seconds interval);
-		void print_once ();
-
-		std::vector<std::shared_ptr<counter>> counters;
-
-		std::atomic<bool> stopped{ false };
-		std::thread thread;
+		std::chrono::system_clock::time_point last_observation{};
 	};
+
+	/*
+	 * Counter that uses node stat container to provide info about rate
+	 */
+	class stat_counter final : public counter
+	{
+	public:
+		explicit stat_counter (nano::stat & stats, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
+
+		uint64_t count () override;
+		std::string name () override;
+
+	private:
+		const nano::stat::type type;
+		const nano::stat::detail detail;
+		const nano::stat::dir dir;
+
+		uint64_t last_count{ 0 };
+
+		nano::stat & stats;
+	};
+
+public:
+	rate_observer () = default;
+	~rate_observer ();
+
+	/*
+	 * Periodically prints all observed rates onto the standard output
+	 */
+	void background_print (std::chrono::seconds interval);
+
+	/*
+	 * Starts observing a particular node stat from stat container
+	 */
+	void observe (nano::node &, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
+
+private:
+	void background_print_impl (std::chrono::seconds interval);
+	void print_once ();
+
+	std::vector<std::shared_ptr<counter>> counters;
+
+	std::atomic<bool> stopped{ false };
+	std::thread thread;
+};
 }
-} // nano
