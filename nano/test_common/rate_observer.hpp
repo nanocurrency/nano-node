97901bed2edefa03fc66077a2d3ab7397c121147
Piotr Wójcik <3044353+fikumikudev@users.noreply.github.com>
2022-08-04 17:24:21 +0200

    Add `rate_observer` test utility

---
+++ nano/test_common/rate_observer.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <nano/lib/stats.hpp>
+
+#include <chrono>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+namespace nano
+{
+namespace test
+{
+	class rate_observer
+	{
+	public:
+		/*
+		 * Base class used as a base to build counters
+		 */
+		class counter
+		{
+		public:
+			std::pair<uint64_t, std::chrono::milliseconds> observe ();
+
+			virtual uint64_t count () = 0;
+			virtual std::string name () = 0;
+
+		private:
+			std::chrono::system_clock::time_point last_observation{};
+		};
+
+		/*
+		 * Counter that uses node stat container to provide info about rate
+		 */
+		class stat_counter final : public counter
+		{
+		public:
+			explicit stat_counter (nano::stat & stats, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
+
+			uint64_t count () override;
+			std::string name () override;
+
+		private:
+			const nano::stat::type type;
+			const nano::stat::detail detail;
+			const nano::stat::dir dir;
+
+			uint64_t last_count{ 0 };
+
+			nano::stat & stats;
+		};
+
+	public:
+		rate_observer () = default;
+		~rate_observer ();
+
+		void background_print (std::chrono::seconds interval);
+
+		void observe (nano::node &, nano::stat::type type, nano::stat::detail detail, nano::stat::dir dir);
+
+	private:
+		void background_print_impl (std::chrono::seconds interval);
+		void print_once ();
+
+		std::vector<std::shared_ptr<counter>> counters;
+
+		std::atomic<bool> stopped{ false };
+		std::thread thread;
+	};
+}
+} // nano
