701665028db4a290356b49dc8d64c4257d0c4a2f
Thiago Silva <thiago@nano.org>
2022-08-25 11:54:46 -0300

    Helper function to create a node out of the network

--- nano/test_common/network.hpp
+++ nano/test_common/network.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <nano/node/common.hpp>
+#include <nano/test_common/system.hpp>
 
 namespace nano
 {
@@ -17,5 +18,7 @@ namespace test
 	class system;
 	/** Waits until a TCP connection is established and returns the TCP channel on success*/
 	std::shared_ptr<nano::transport::channel_tcp> establish_tcp (nano::test::system &, nano::node &, nano::endpoint const &);
+	/** Adds a node to the system without establishing connections */
+	std::shared_ptr<nano::node> add_outer_node (nano::test::system & system, uint16_t port_a = nano::test::get_available_port ());
 }
 }
