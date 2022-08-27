05e4e2a702e5e099eb8518e9d2507e2422d7d6da
Dimitrios Siganos <dimitris@siganos.org>
2022-08-27 01:55:20 +0100

    Move observer_set class to its own file

--- nano/lib/utility.hpp
+++ nano/lib/utility.hpp
@@ -147,50 +147,6 @@ std::string generate_stacktrace ();
 std::size_t get_file_descriptor_limit ();
 void set_file_descriptor_limit (std::size_t limit);
 
-template <typename... T>
-class observer_set final
-{
-public:
-	void add (std::function<void (T...)> const & observer_a)
-	{
-		nano::lock_guard<nano::mutex> lock (mutex);
-		observers.push_back (observer_a);
-	}
-
-	void notify (T... args) const
-	{
-		nano::unique_lock<nano::mutex> lock (mutex);
-		auto observers_copy = observers;
-		lock.unlock ();
-
-		for (auto & i : observers_copy)
-		{
-			i (args...);
-		}
-	}
-
-	bool empty () const
-	{
-		nano::lock_guard<nano::mutex> lock (mutex);
-		return observers.empty();
-	}
-
-	std::unique_ptr<container_info_component> collect_container_info (std::string const & name) const
-	{
-		nano::unique_lock<nano::mutex> lock (mutex);
-		auto count = observers.size ();
-		lock.unlock();
-		auto sizeof_element = sizeof (typename decltype (observers)::value_type);
-		auto composite = std::make_unique<container_info_composite> (name);
-		composite->add_component (std::make_unique<container_info_leaf> (container_info{ "observers", count, sizeof_element }));
-		return composite;
-	}
-
-private:
-	mutable nano::mutex mutex{ mutex_identifier (mutexes::observer_set) };
-	std::vector<std::function<void (T...)>> observers;
-};
-
 void remove_all_files_in_dir (boost::filesystem::path const & dir);
 void move_all_files_to_dir (boost::filesystem::path const & from, boost::filesystem::path const & to);
 
