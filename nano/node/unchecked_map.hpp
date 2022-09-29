bd274da26106e2d17a734b63f8b8e2d3c821641b
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2022-09-29 14:48:43 +0200

    Remove insert queue from unchecked_map
    There is no need to buffer inserts anymore

--- nano/node/unchecked_map.hpp
+++ nano/node/unchecked_map.hpp
@@ -47,33 +47,25 @@ public: // Trigger requested dependencies
 	std::function<void (nano::unchecked_info const &)> satisfied{ [] (nano::unchecked_info const &) {} };
 
 private:
-	using insert = std::pair<nano::hash_or_account, nano::unchecked_info>;
-	using query = nano::hash_or_account;
-	class item_visitor : boost::static_visitor<>
-	{
-	public:
-		item_visitor (unchecked_map & unchecked);
-		void operator() (insert const & item);
-		void operator() (query const & item);
-		unchecked_map & unchecked;
-	};
 	void run ();
-	void insert_impl (nano::hash_or_account const & dependency, nano::unchecked_info const & info);
 	void query_impl (nano::block_hash const & hash);
+
+private: // Dependencies
 	nano::store & store;
+
+private:
 	bool const & disable_delete;
-	std::deque<boost::variant<insert, query>> buffer;
-	std::deque<boost::variant<insert, query>> back_buffer;
+	std::deque<nano::hash_or_account> buffer;
+	std::deque<nano::hash_or_account> back_buffer;
 	bool writing_back_buffer{ false };
 	bool stopped{ false };
 	nano::condition_variable condition;
 	nano::mutex mutex;
 	std::thread thread;
-	void write_buffer (decltype (buffer) const & back_buffer);
+
+	void process_queries (decltype (buffer) const & back_buffer);
 
 	static size_t constexpr mem_block_count_max = 1024 * 1024;
-
-	friend class item_visitor;
 
 private: // In memory store
 	class entry
@@ -89,7 +81,7 @@ private: // In memory store
 
 	using ordered_unchecked = boost::multi_index_container<entry,
 		mi::indexed_by<
-			mi::random_access<mi::tag<tag_sequenced>>,
+			mi::sequenced<mi::tag<tag_sequenced>>,
 			mi::ordered_unique<mi::tag<tag_root>,
 				mi::member<entry, nano::unchecked_key, &entry::key>>>>;
 	// clang-format on
