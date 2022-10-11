24fc69bfb53614f2dbdc4e03b5f6f1e42d7e4d85
clemahieu <clemahieu@gmail.com>
2022-10-11 14:23:12 +0100

    Removing check to ensure no elections are active as this is unrelated to the test.

--- nano/core_test/bootstrap.cpp
+++ nano/core_test/bootstrap.cpp
@@ -383,13 +383,13 @@ TEST (bootstrap_processor, process_state)
 	ASSERT_EQ (nano::process_result::progress, node0->process (*block1).code);
 	ASSERT_EQ (nano::process_result::progress, node0->process (*block2).code);
 
+	config.peering_port = nano::test::get_available_port ();
 	auto node1 (std::make_shared<nano::node> (system.io_ctx, nano::test::get_available_port (), nano::unique_path (), system.logging, system.work, node_flags));
 	ASSERT_EQ (node0->latest (nano::dev::genesis_key.pub), block2->hash ());
 	ASSERT_NE (node1->latest (nano::dev::genesis_key.pub), block2->hash ());
 	node1->bootstrap_initiator.bootstrap (node0->network.endpoint (), false);
 	ASSERT_NE (node1->latest (nano::dev::genesis_key.pub), node0->latest (nano::dev::genesis_key.pub));
 	ASSERT_TIMELY (10s, node1->latest (nano::dev::genesis_key.pub) == node0->latest (nano::dev::genesis_key.pub));
-	ASSERT_TIMELY (10s, node1->active.empty ());
 	node1->stop ();
 }
 
