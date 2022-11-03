4bfcc235392c306ee54f83b414858074ead73dfa
Dimitrios Siganos <dimitris@siganos.org>
2022-11-03 16:40:27 +0000

    Also publish docker nanocurrency-env:centos

--- ci/actions/linux/ghcr_push.sh
+++ ci/actions/linux/ghcr_push.sh
@@ -6,6 +6,7 @@ if [[ "$GITHUB_WORKFLOW" = "Develop" ]]; then
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:base"
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:gcc"
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:clang"
+    "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/${GITHUB_REPOSITORY}/nano-env:centos"
 else
     tags=$(docker images --format '{{.Repository}}:{{.Tag }}' | grep "ghcr.io" | grep -vE "env|none")
     for a in $tags; do
