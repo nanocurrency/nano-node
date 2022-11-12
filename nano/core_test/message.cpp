3833e5f5d791659f4f6bd7695f0e6962394d372b
Ian <ianpatrickcunningham@gmail.com>
2022-11-12 16:01:07 -0600

    removing unnecessary network constants initialization and using this instead

--- nano/core_test/message.cpp
+++ nano/core_test/message.cpp
@@ -261,9 +261,7 @@ TEST (message, message_header_to_string)
 
 TEST (message, telemetry_req_to_string)
 {
-	nano::work_thresholds work_threshold = nano::work_thresholds (0, 0, 0);
-	nano::network_constants network_constants = nano::network_constants (work_threshold, nano::networks::nano_dev_network);
-	nano::telemetry_req telemetry_req = nano::telemetry_req (network_constants);
+	nano::telemetry_req telemetry_req = nano::telemetry_req (nano::dev::network_params.network);
 
 	ASSERT_EQ (telemetry_req.to_string (), "NetID: 5241(dev), VerMaxUsingMin: 19/19/18, MsgType: 12(telemetry_req), Extensions: 0000");
 }
