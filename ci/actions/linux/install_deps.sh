aa6bd58cfd967fc2eb5fe896d40f89d06c796290
Thiago Silva <82097354+thsfs@users.noreply.github.com>
2022-11-26 00:35:54 -0300

    Fix compiler flag on docker build (#4005)

--- ci/actions/linux/install_deps.sh
+++ ci/actions/linux/install_deps.sh
@@ -9,7 +9,7 @@ sudo mkdir -p /etc/docker && echo '{"ipv6":true,"fixed-cidr-v6":"2001:db8:1::/64
 
 ci/build-docker-image.sh docker/ci/Dockerfile-base nanocurrency/nano-env:base
 if [[ "${COMPILER:-}" != "" ]]; then
-    ci/build-docker-image.sh docker/ci/Dockerfile-gcc nanocurrency/nano-env:${COMPILER}
+    ci/build-docker-image.sh docker/ci/Dockerfile-${COMPILER} nanocurrency/nano-env:${COMPILER}
 else
     ci/build-docker-image.sh docker/ci/Dockerfile-gcc nanocurrency/nano-env:gcc
     ci/build-docker-image.sh docker/ci/Dockerfile-clang nanocurrency/nano-env:clang
