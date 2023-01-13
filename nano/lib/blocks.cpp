d42be6475ff30d8186f02206f39ca6b71bb5f851
Thiago Silva <thiago@nano.org>
2023-01-13 13:33:15 -0300

    Apply uniform initialization to the mutexes (blocks.cpp)

--- nano/lib/blocks.cpp
+++ nano/lib/blocks.cpp
@@ -1877,7 +1877,7 @@ std::shared_ptr<nano::block> nano::block_uniquer::unique (std::shared_ptr<nano::
 	if (result != nullptr)
 	{
 		nano::uint256_union key (block_a->full_hash ());
-		nano::lock_guard<nano::mutex> lock (mutex);
+		nano::lock_guard<nano::mutex> lock{ mutex };
 		auto & existing (blocks[key]);
 		if (auto block_l = existing.lock ())
 		{
@@ -1914,7 +1914,7 @@ std::shared_ptr<nano::block> nano::block_uniquer::unique (std::shared_ptr<nano::
 
 size_t nano::block_uniquer::size ()
 {
-	nano::lock_guard<nano::mutex> lock (mutex);
+	nano::lock_guard<nano::mutex> lock{ mutex };
 	return blocks.size ();
 }
 
