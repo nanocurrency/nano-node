3d6cec0aba6ee7527dcd9a49e9e3ce5461742f00
clemahieu <clemahieu@gmail.com>
2023-01-26 14:05:47 +0000

    Removing nano::process_return::previous_balance which is assigned but unused.

--- nano/secure/ledger.cpp
+++ nano/secure/ledger.cpp
@@ -276,7 +276,6 @@ void ledger_processor::state_block_impl (nano::state_block & block_a)
 				{
 					// Account already exists
 					epoch = info.epoch ();
-					result.previous_balance = info.balance;
 					result.code = block_a.hashables.previous.is_zero () ? nano::process_result::fork : nano::process_result::progress; // Has this account already been opened? (Ambigious)
 					if (result.code == nano::process_result::progress)
 					{
@@ -293,7 +292,6 @@ void ledger_processor::state_block_impl (nano::state_block & block_a)
 				else
 				{
 					// Account does not yet exists
-					result.previous_balance = 0;
 					result.code = block_a.previous ().is_zero () ? nano::process_result::progress : nano::process_result::gap_previous; // Does the first block in an account yield 0 for previous() ? (Unambigious)
 					if (result.code == nano::process_result::progress)
 					{
@@ -392,7 +390,6 @@ void ledger_processor::epoch_block_impl (nano::state_block & block_a)
 				if (!account_error)
 				{
 					// Account already exists
-					result.previous_balance = info.balance;
 					result.code = block_a.hashables.previous.is_zero () ? nano::process_result::fork : nano::process_result::progress; // Has this account already been opened? (Ambigious)
 					if (result.code == nano::process_result::progress)
 					{
@@ -405,7 +402,6 @@ void ledger_processor::epoch_block_impl (nano::state_block & block_a)
 				}
 				else
 				{
-					result.previous_balance = 0;
 					result.code = block_a.hashables.representative.is_zero () ? nano::process_result::progress : nano::process_result::representative_mismatch;
 					// Non-exisitng account should have pending entries
 					if (result.code == nano::process_result::progress)
@@ -486,7 +482,6 @@ void ledger_processor::change_block (nano::change_block & block_a)
 							ledger.update_account (transaction, account, info, new_info);
 							ledger.store.frontier.del (transaction, block_a.hashables.previous);
 							ledger.store.frontier.put (transaction, hash, account);
-							result.previous_balance = info.balance;
 							ledger.stats.inc (nano::stat::type::ledger, nano::stat::detail::change);
 						}
 					}
@@ -539,7 +534,6 @@ void ledger_processor::send_block (nano::send_block & block_a)
 								ledger.store.pending.put (transaction, nano::pending_key (block_a.hashables.destination, hash), { account, amount, nano::epoch::epoch_0 });
 								ledger.store.frontier.del (transaction, block_a.hashables.previous);
 								ledger.store.frontier.put (transaction, hash, account);
-								result.previous_balance = info.balance;
 								ledger.stats.inc (nano::stat::type::ledger, nano::stat::detail::send);
 							}
 						}
@@ -609,7 +603,6 @@ void ledger_processor::receive_block (nano::receive_block & block_a)
 											ledger.cache.rep_weights.representation_add (info.representative, pending.amount.number ());
 											ledger.store.frontier.del (transaction, block_a.hashables.previous);
 											ledger.store.frontier.put (transaction, hash, account);
-											result.previous_balance = info.balance;
 											ledger.stats.inc (nano::stat::type::ledger, nano::stat::detail::receive);
 										}
 									}
@@ -675,7 +668,6 @@ void ledger_processor::open_block (nano::open_block & block_a)
 									ledger.update_account (transaction, block_a.hashables.account, info, new_info);
 									ledger.cache.rep_weights.representation_add (block_a.representative (), pending.amount.number ());
 									ledger.store.frontier.put (transaction, hash, block_a.hashables.account);
-									result.previous_balance = 0;
 									ledger.stats.inc (nano::stat::type::ledger, nano::stat::detail::open);
 								}
 							}
