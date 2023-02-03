f558254714e516d5ec47ca6d7a7653ebf7a63f34
Thiago Silva <thiago@nano.org>
2023-02-03 16:25:45 -0300

    Removing the udp_channels from the node's network (part 3)

--- nano/node/network.cpp
+++ nano/node/network.cpp
@@ -655,17 +655,7 @@ void nano::network::fill_keepalive_self (std::array<nano::endpoint, 8> & target_
 
 nano::tcp_endpoint nano::network::bootstrap_peer ()
 {
-	nano::tcp_endpoint result (boost::asio::ip::address_v6::any (), 0);
-	bool use_udp_peer (nano::random_pool::generate_word32 (0, 1));
-	if (use_udp_peer || tcp_channels.size () == 0)
-	{
-		result = udp_channels.bootstrap_peer (node.network_params.network.protocol_version_min);
-	}
-	if (result == nano::tcp_endpoint (boost::asio::ip::address_v6::any (), 0))
-	{
-		result = tcp_channels.bootstrap_peer (node.network_params.network.protocol_version_min);
-	}
-	return result;
+	return tcp_channels.bootstrap_peer (node.network_params.network.protocol_version_min);
 }
 
 std::shared_ptr<nano::transport::channel> nano::network::find_channel (nano::endpoint const & endpoint_a)
