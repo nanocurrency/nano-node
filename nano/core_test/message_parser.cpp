c17c4ddd90693c680a68075ad9991c86a88b237f
Thiago Silva <thiago@nano.org>
2023-03-01 22:02:39 -0300

    Replace message_parser test for confirm_ack to test message_deserializer
    The old test checked the bytes generated the same confirm_ack object by
    ensuring its visitor was processed only once. Also verified the
    parse_status was success. In the new implementation the visitor isn't
    part of the deserializer. The outcome object is compared to the
    original, and the parse_status should be success. This is the sanity
    test, where the deserialization is expected to happen successfully.
    
    Also checks if the message pointer can be downcast to the proper type,
    that is the same of verifying its type is supported.

--- nano/core_test/message_parser.cpp
+++ nano/core_test/message_parser.cpp
@@ -1,7 +1,11 @@
+#include <nano/node/transport/message_deserializer.hpp>
 #include <nano/test_common/system.hpp>
 #include <nano/test_common/testutil.hpp>
 
 #include <gtest/gtest.h>
+
+#include <memory>
+#include <vector>
 
 namespace
 {
@@ -60,14 +64,51 @@ public:
 };
 }
 
-TEST (message_parser, exact_confirm_ack_size)
+template <class message_type>
+auto message_deserializer_success_checker (message_type & message_original) -> void
 {
-	nano::test::system system (1);
-	dev_visitor visitor;
+	// Dependencies for the message deserializer.
 	nano::network_filter filter (1);
 	nano::block_uniquer block_uniquer;
 	nano::vote_uniquer vote_uniquer (block_uniquer);
-	nano::message_parser parser (filter, block_uniquer, vote_uniquer, visitor, system.work, nano::dev::network_params.network);
+
+	// Data used to simulate the incoming buffer to be deserialized, the offset tracks how much has been read from the input_source
+	// as the read function is called first to read the header, then called again to read the payload.
+	std::vector<uint8_t> input_source;
+	auto offset = 0u;
+
+	// Message Deserializer with the query function tweaked to read from the `input_source`.
+	auto const message_deserializer = std::make_shared<nano::transport::message_deserializer> (nano::dev::network_params.network, filter, block_uniquer, vote_uniquer,
+	[&input_source, &offset] (std::shared_ptr<std::vector<uint8_t>> const & data_a, size_t size_a, std::function<void (boost::system::error_code const &, std::size_t)> callback_a) {
+		debug_assert (input_source.size () >= size_a);
+		data_a->resize (size_a);
+		auto const copy_start = input_source.begin () + offset;
+		std::copy (copy_start, copy_start + size_a, data_a->data ());
+		offset += size_a;
+		callback_a (boost::system::errc::make_error_code (boost::system::errc::success), size_a);
+	});
+
+	// Generating the values for the `input_source`.
+	{
+		nano::vectorstream stream (input_source);
+		message_original.serialize (stream);
+	}
+
+	// Deserializing and testing the success path.
+	message_deserializer->read (
+	[&message_original] (boost::system::error_code ec_a, std::unique_ptr<nano::message> message_a) {
+		auto deserialized_message = dynamic_cast<message_type *> (message_a.get ());
+		ASSERT_NE (deserialized_message, nullptr);
+		auto deserialized_bytes = deserialized_message->to_bytes ();
+		auto original_bytes = message_original.to_bytes ();
+		ASSERT_EQ (*deserialized_bytes, *original_bytes);
+	});
+	ASSERT_EQ (message_deserializer->status, nano::transport::message_deserializer::parse_status::success);
+}
+
+TEST (message_deserializer, exact_confirm_ack)
+{
+	nano::test::system system{ 1 };
 	nano::block_builder builder;
 	auto block = builder
 				 .send ()
@@ -79,27 +120,8 @@ TEST (message_parser, exact_confirm_ack_size)
 				 .build_shared ();
 	auto vote (std::make_shared<nano::vote> (0, nano::keypair ().prv, 0, 0, std::vector<nano::block_hash>{ block->hash () }));
 	nano::confirm_ack message{ nano::dev::network_params.network, vote };
-	std::vector<uint8_t> bytes;
-	{
-		nano::vectorstream stream (bytes);
-		message.serialize (stream);
-	}
-	ASSERT_EQ (0, visitor.confirm_ack_count);
-	ASSERT_EQ (parser.status, nano::message_parser::parse_status::success);
-	auto error (false);
-	nano::bufferstream stream1 (bytes.data (), bytes.size ());
-	nano::message_header header1 (error, stream1);
-	ASSERT_FALSE (error);
-	parser.deserialize_confirm_ack (stream1, header1);
-	ASSERT_EQ (1, visitor.confirm_ack_count);
-	ASSERT_EQ (parser.status, nano::message_parser::parse_status::success);
-	bytes.push_back (0);
-	nano::bufferstream stream2 (bytes.data (), bytes.size ());
-	nano::message_header header2 (error, stream2);
-	ASSERT_FALSE (error);
-	parser.deserialize_confirm_ack (stream2, header2);
-	ASSERT_EQ (1, visitor.confirm_ack_count);
-	ASSERT_NE (parser.status, nano::message_parser::parse_status::success);
+
+	message_deserializer_success_checker<decltype (message)> (message);
 }
 
 TEST (message_parser, exact_confirm_req_size)
