7f9a518b025710425b25edb6e014d4d0c5736b24
Thiago Silva <thiago@nano.org>
2023-03-14 21:51:37 -0300

    Add ascending bootstrap account_sets_config class

--- nano/node/bootstrap/bootstrap_config.hpp
+++ nano/node/bootstrap/bootstrap_config.hpp
@@ -8,6 +8,18 @@ namespace nano
 {
 class tomlconfig;
 
+class account_sets_config final
+{
+public:
+	nano::error deserialize (nano::tomlconfig & toml);
+	nano::error serialize (nano::tomlconfig & toml) const;
+
+	std::size_t consideration_count{ 4 };
+	std::size_t priorities_max{ 256 * 1024 };
+	std::size_t blocking_max{ 256 * 1024 };
+	nano::millis_t cooldown{ 1000 * 3 };
+};
+
 class bootstrap_ascending_config final
 {
 public:
@@ -18,5 +30,7 @@ public:
 	std::size_t database_requests_limit{ 1024 };
 	std::size_t pull_count{ nano::bootstrap_server::max_blocks };
 	nano::millis_t timeout{ 1000 * 3 };
+
+	nano::account_sets_config account_sets;
 };
 }
