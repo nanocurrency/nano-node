52335d44e216a3068350fe9937166a404294367e
Thiago Silva <thiago@nano.org>
2023-05-17 17:01:02 -0300

    Remove unused build_prep scripts

--- util/build_prep/macosx/build_qt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd /tmp
-wget -O qtbase-clang-latest.tgz https://s3.us-east-2.amazonaws.com/repo.nano.org/artifacts/qtbase-clang-latest.tgz
-tar -zxf qtbase-clang-latest.tgz
-mv tmp/* .
-rm -fr tmp
-popd
