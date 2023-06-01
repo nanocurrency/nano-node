a6da6b8b0936e8f84361413bd685468a13558f33
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-06-01 22:38:10 +0200

    Simpler approach

--- ci/actions/deploy.sh
+++ ci/actions/deploy.sh
@@ -26,44 +26,39 @@ if [[ "$OS" == 'Linux' && "$IS_RPM_DEPLOY" -eq "1" ]]; then
 
     for rpm in $RPMS; do
         SHA=$(sha256sum ${rpm})
-        echo "${rpm}: $SHA"
-        echo $SHA > "${rpm}.sha256"
+        echo $SHA
+        echo $SHA > ${GITHUB_WORKSPACE}/$(basename "${rpm}.sha256")
 
-        aws s3 cp "${rpm}" s3://repo.nano.org/$DIRECTORY/binaries/$(basename "${rpm}") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-        aws s3 cp "${rpm}.sha256" s3://repo.nano.org/$DIRECTORY/binaries/$(basename "${rpm}.sha256") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+        aws s3 cp ${rpm} s3://repo.nano.org/$DIRECTORY/binaries/$(basename "${rpm}") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+        aws s3 cp ${GITHUB_WORKSPACE}/$(basename "${rpm}.sha256") s3://repo.nano.org/$DIRECTORY/binaries/$(basename "${rpm}.sha256") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
     done
-
+    
     for srpm in $SRPMS; do
         SHA=$(sha256sum ${srpm})
-        echo "${srpm}: $SHA"
-        echo $SHA > "${srpm}.sha256"
+        echo $SHA
+        echo $SHA > ${GITHUB_WORKSPACE}/$(basename "${srpm}).sha256")
 
-        aws s3 cp "${srpm}" s3://repo.nano.org/$DIRECTORY/source/$(basename "${srpm}") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-        aws s3 cp "${srpm}.sha256" s3://repo.nano.org/$DIRECTORY/source/$(basename "${srpm}.sha256") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+        aws s3 cp ${srpm} s3://repo.nano.org/$DIRECTORY/source/$(basename "${srpm}") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+        aws s3 cp ${GITHUB_WORKSPACE}/$(basename "${srpm}).sha256") s3://repo.nano.org/$DIRECTORY/source/$(basename "${srpm}.sha256") --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
     done
 elif [[ "$OS" == 'Linux' ]]; then
-    TAR_PATH=$GITHUB_WORKSPACE/build/nano-node-*-Linux.tar.bz2
-    DEB_PATH=$GITHUB_WORKSPACE/build/nano-node-*-Linux.deb
+    SHA=$(sha256sum $GITHUB_WORKSPACE/build/nano-node-*-Linux.tar.bz2)
+    echo $SHA
+    echo $SHA >$GITHUB_WORKSPACE/nano-node-$TAG-Linux.tar.bz2.sha256
 
-    SHA=$(sha256sum $TAR_PATH)
-    echo "${TAR_PATH}: $SHA"
-    echo $SHA > "${TAR_PATH}.sha256"
+    SHA=$(sha256sum $GITHUB_WORKSPACE/build/nano-node-*-Linux.deb)
+    echo $SHA
+    echo $SHA >$GITHUB_WORKSPACE/nano-node-$TAG-Linux.deb.sha256
 
-    SHA=$(sha256sum $DEB_PATH)
-    echo "${DEB_PATH}: $SHA"
-    echo $SHA > "${DEB_PATH}.sha256"
-
-    aws s3 cp "${TAR_PATH}" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.tar.bz2 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-    aws s3 cp "${TAR_PATH}.sha256" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.tar.bz2.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-    aws s3 cp "${DEB_PATH}" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.deb --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-    aws s3 cp "${DEB_PATH}.sha256" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.deb.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/build/nano-node-*-Linux.tar.bz2 s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.tar.bz2 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/nano-node-$TAG-Linux.tar.bz2.sha256 s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.tar.bz2.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/build/nano-node-*-Linux.deb s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.deb --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/nano-node-$TAG-Linux.deb.sha256 s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Linux.deb.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 else
-    DMG_PATH=$GITHUB_WORKSPACE/build/nano-node-*-Darwin.dmg
+    SHA=$(sha256sum $GITHUB_WORKSPACE/build/nano-node-*-Darwin.dmg)
+    echo $SHA
+    echo $SHA >$GITHUB_WORKSPACE/build/nano-node-$TAG-Darwin.dmg.sha256
 
-    SHA=$(sha256sum $DMG_PATH)
-    echo "${DMG_PATH}: $SHA"
-    echo $SHA > "${DMG_PATH}.sha256"
-    
-    aws s3 cp "${DMG_PATH}" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Darwin.dmg --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-    aws s3 cp "${DMG_PATH}.sha256" s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Darwin.dmg.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/build/nano-node-*-Darwin.dmg s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Darwin.dmg --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp $GITHUB_WORKSPACE/build/nano-node-$TAG-Darwin.dmg.sha256 s3://repo.nano.org/$DIRECTORY/binaries/nano-node-$TAG-Darwin.dmg.sha256 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 fi
