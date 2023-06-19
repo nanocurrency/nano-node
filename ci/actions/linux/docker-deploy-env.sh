b86b65bceae6f80d3d9ae50caf8a792e966a93f9
gr0vity-dev <iq.cc@pm.me>
2023-06-19 16:55:56 +0200

    Make docker builds and deploys more modular
    - Remove dependency on the workflow name
    - COnvert $GITHUB_WORKFLOW to $NETWORK
    - Create smaller functions with limited scope
    - Create similar deploy functions for docker and github container registries (hub.docker and ghcr)

---
+++ ci/actions/linux/docker-deploy-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -x
+
+source "$(dirname "$BASH_SOURCE")/docker-impl/docker-common.sh"
+
+docker_deploy_env
--- ci/actions/linux/docker-impl/docker-common.sh
+++ ci/actions/linux/docker-impl/docker-common.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-
 set -e
 set -a
 
 scripts="$PWD/ci"
-CI_BRANCH=$(git branch | cut -f2 -d' ')
+CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 tags=()
+
 if [ -n "$CI_TAG" ]; then
     tags+=("$CI_TAG")
 elif [ -n "$CI_BRANCH" ]; then
     CI_TAG=$CI_BRANCH
     tags+=("$CI_BRANCH")
 fi
-if [[ "$GITHUB_WORKFLOW" = "Live" ]]; then
+
+
+if [[ "$NETWORK" = "LIVE" ]]; then
     echo "Live"
     network_tag_suffix=''
     network="live"
-elif [[ "$GITHUB_WORKFLOW" = "Beta" ]]; then
+elif [[ "$NETWORK" = "BETA" ]]; then
     echo "Beta"
     network_tag_suffix="-beta"
     network="beta"
-elif [[ "$GITHUB_WORKFLOW" = "Test" ]]; then
+elif [[ "$NETWORK" = "TEST" ]]; then
     echo "Test"
     network_tag_suffix="-test"
     network="test"
 fi
 
-if [[ "$GITHUB_WORKFLOW" != "Develop Branch Dockers Deploy" ]]; then
-    docker_image_name="nanocurrency/nano${network_tag_suffix}"
-fi
+docker_image_name="nanocurrency/nano${network_tag_suffix}"
+ghcr_image_name="ghcr.io/${GITHUB_REPOSITORY}/nano${network_tag_suffix}"
 
 docker_build()
 {
@@ -37,44 +38,116 @@ docker_build()
         ci_version_pre_release="$CI_VERSION_PRE_RELEASE"
     fi
 
-    if [[ "$GITHUB_WORKFLOW" != "Develop Branch Dockers Deploy" ]]; then
-        ghcr_image_name="ghcr.io/${GITHUB_REPOSITORY}/nano${network_tag_suffix}"
-        "$scripts"/build-docker-image.sh docker/node/Dockerfile "$docker_image_name" --build-arg NETWORK="$network" --build-arg CI_BUILD=true --build-arg CI_VERSION_PRE_RELEASE="$ci_version_pre_release" --build-arg CI_TAG="$CI_TAG"
-        for tag in "${tags[@]}"; do
-            # Sanitize docker tag
-            # https://docs.docker.com/engine/reference/commandline/tag/
-            tag="$(printf '%s' "$tag" | tr -c '[a-z][A-Z][0-9]_.-' -)"
-            if [ "$tag" != "latest" ]; then
-                docker tag "$docker_image_name" "${docker_image_name}:$tag"
-                docker tag "$ghcr_image_name" "${ghcr_image_name}:$tag"
-            fi
-        done
+    build_docker_image "$ci_version_pre_release"
+    
+    for tag in "${tags[@]}"; do
+        local sanitized_tag=$(sanitize_tag "$tag")
+        tag_docker_image "$sanitized_tag"
+    done
+}
+
+build_docker_image() {
+    local ci_version_pre_release="$1"
+    "$scripts"/build-docker-image.sh docker/node/Dockerfile "$docker_image_name" \
+        --build-arg NETWORK="$network" \
+        --build-arg CI_VERSION_PRE_RELEASE="$ci_version_pre_release" \
+        --build-arg CI_TAG="$CI_TAG"
+}
+
+sanitize_tag() {
+    local tag="$1"
+    # Sanitize docker tag
+    # https://docs.docker.com/engine/reference/commandline/tag/
+    tag="$(printf '%s' "$tag" | tr -c '[a-z][A-Z][0-9]_.-' -)"
+    echo "$tag"
+}
+
+tag_docker_image() {
+    local tag="$1"
+    if [ "$tag" != "latest" ]; then
+        docker tag "$docker_image_name" "${docker_image_name}:$tag"
+        docker tag "$ghcr_image_name" "${ghcr_image_name}:$tag"
     fi
 }
 
+
+
 docker_deploy()
 {
-    if [ -n "$DOCKER_PASSWORD" ]; then
-        echo "$DOCKER_PASSWORD" | docker login -u nanoreleaseteam --password-stdin
-        if [[ "$GITHUB_WORKFLOW" = "Develop Branch Dockers Deploy" ]]; then
-            "$scripts"/custom-timeout.sh 30 docker push "nanocurrency/nano-env:base"
-            "$scripts"/custom-timeout.sh 30 docker push "nanocurrency/nano-env:gcc"
-            "$scripts"/custom-timeout.sh 30 docker push "nanocurrency/nano-env:clang"
-            echo "Deployed nano-env"
-            exit 0
-        else
-            if [[ "$GITHUB_WORKFLOW" = "Live" ]]; then
-                tags=$(docker images --format '{{.Repository}}:{{.Tag }}' | grep nanocurrency | grep -vE "env|ghcr.io|none|latest")
-            else
-                tags=$(docker images --format '{{.Repository}}:{{.Tag }}' | grep nanocurrency | grep -vE "env|ghcr.io|none")
-            fi
-            for a in $tags; do
-                "$scripts"/custom-timeout.sh 30 docker push "$a"
-            done
-            echo "$docker_image_name with tags ${tags//$'\n'/' '} deployed"
-        fi
+    docker_login "nanoreleaseteam" "$DOCKER_PASSWORD"
+    if [[ "$NETWORK" = "LIVE" ]]; then
+        deploy_tags "nanocurrency" "env|ghcr.io|none|latest"
     else
-        echo "\$DOCKER_PASSWORD environment variable required"
-        exit 0
+        deploy_tags "nanocurrency" "env|ghcr.io|none"
     fi
+}
+
+ghcr_deploy()
+{    
+    deploy_tags "ghcr.io/${GITHUB_REPOSITORY}" "env|none"
+}
+
+ghcr_deploy_env()
+{        
+    local images=(
+        "ghcr.io/${GITHUB_REPOSITORY}/nano-env:base"
+        "ghcr.io/${GITHUB_REPOSITORY}/nano-env:gcc"
+        "ghcr.io/${GITHUB_REPOSITORY}/nano-env:clang"
+        "ghcr.io/${GITHUB_REPOSITORY}/nano-env:rhel"
+    )
+    deploy_env_images "${images[@]}"   
+}
+
+docker_deploy_env()
+{
+    docker_login "nanoreleaseteam" "$DOCKER_PASSWORD" 
+    local images=(
+        "nanocurrency/nano-env:base"
+        "nanocurrency/nano-env:gcc"
+        "nanocurrency/nano-env:clang"
+    )
+    deploy_env_images "${images[@]}"
+}
+
+docker_login()
+{
+    local username=$1
+    local password=$2
+
+    if [ -z "$password" ]; then
+        echo "\$DOCKER_PASSWORD or \$GHCR_TOKEN environment variable required"
+        exit 1
+    fi
+
+    echo "$password" | docker login -u "$username" --password-stdin
+}
+
+push_docker_image()
+{
+    local image_name=$1
+    "$scripts"/custom-timeout.sh 30 docker push "$image_name"
+}
+
+deploy_env_images()
+{
+    local images=("$@")
+
+    for image in "${images[@]}"; do
+        push_docker_image "$image"
+    done
+
+    echo "Deployed nano-env"
+}
+
+deploy_tags()
+{
+    local repo=$1
+    local exclude_pattern=$2
+    local tags=$(docker images --format '{{.Repository}}:{{.Tag }}' | grep "$repo" | grep -vE "$exclude_pattern")
+
+    for tag in $tags; do
+        push_docker_image "$tag"
+    done
+
+    echo "$repo with tags ${tags//$'\n'/' '} deployed"
 }
