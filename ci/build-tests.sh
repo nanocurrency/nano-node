54593ee2a74a0a99839896574b43173c8eba64da
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-06-22 16:14:02 +0200

    Fix build type in Docker (#4249)

--- ci/build-tests.sh
+++ ci/build-tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euox pipefail
 
-BUILD_TYPE="Debug"
-if [[ "${RELEASE:-false}" == "true" ]]; then
-    BUILD_TYPE="RelWithDebInfo"
-fi
-
-BUILD_TYPE=$BUILD_TYPE \
 NANO_TEST=ON \
 NANO_NETWORK=dev \
 NANO_GUI=ON \
