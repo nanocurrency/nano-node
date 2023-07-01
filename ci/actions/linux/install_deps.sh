64b697f33eec750da4850de6dc0ec90cebad4ffe
gr0vity-dev <iq.cc@pm.me>
2023-07-01 10:47:13 +0200

    Make usage of nano-env:rhel explicit
    - Build the nano-env docker image in the current workflow
    - Use the locally built image.

--- ci/actions/linux/install_deps.sh
+++ ci/actions/linux/install_deps.sh
@@ -2,18 +2,20 @@
 
 set -x
 
+DOCKER_REGISTRY="${DOCKER_REGISTRY:-nanocurrency}"
+
 echo "Script ci/actions/linux/install_deps.sh starting COMPILER=\"$COMPILER\""
 
 # This enables IPv6 support in docker, needed to run node tests inside docker container
 sudo mkdir -p /etc/docker && echo '{"ipv6":true,"fixed-cidr-v6":"2001:db8:1::/64"}' | sudo tee /etc/docker/daemon.json && sudo service docker restart
 
-ci/build-docker-image.sh docker/ci/Dockerfile-base nanocurrency/nano-env:base
+ci/build-docker-image.sh docker/ci/Dockerfile-base ${DOCKER_REGISTRY}/nano-env:base
 if [[ "${COMPILER:-}" != "" ]]; then
-    ci/build-docker-image.sh docker/ci/Dockerfile-${COMPILER} nanocurrency/nano-env:${COMPILER}
+    ci/build-docker-image.sh docker/ci/Dockerfile-${COMPILER} ${DOCKER_REGISTRY}/nano-env:${COMPILER}
 else
-    ci/build-docker-image.sh docker/ci/Dockerfile-gcc nanocurrency/nano-env:gcc
-    ci/build-docker-image.sh docker/ci/Dockerfile-clang nanocurrency/nano-env:clang
-    ci/build-docker-image.sh docker/ci/Dockerfile-rhel nanocurrency/nano-env:rhel
+    ci/build-docker-image.sh docker/ci/Dockerfile-gcc ${DOCKER_REGISTRY}/nano-env:gcc
+    ci/build-docker-image.sh docker/ci/Dockerfile-clang ${DOCKER_REGISTRY}/nano-env:clang
+    ci/build-docker-image.sh docker/ci/Dockerfile-rhel ${DOCKER_REGISTRY}/nano-env:rhel
 fi
 
 echo "Script ci/actions/linux/install_deps.sh finished"
