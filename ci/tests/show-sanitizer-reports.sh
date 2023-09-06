6f916f09d88b5b2aa5024b705148c9373a58a44d
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-09-06 11:47:46 +0200

    Enhance code sanitizers workflow (fixed & resubmitted) (#4278)

---
+++ ci/tests/show-sanitizer-reports.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -uo pipefail
+
+issue_reported=false
+
+# Check for sanitizer reports using glob
+shopt -s nullglob
+reports=(./sanitizer_report*)
+
+if [[ ${#reports[@]} -gt 0 ]]; then
+    for report in "${reports[@]}"; do
+        report_name=$(basename "${report}")
+        echo "::group::Report: $report_name"
+        
+        cat "${report}"
+        
+        echo "::endgroup::"
+        
+        issue_reported=true
+    done
+else
+    echo "::notice::No report has been generated."
+fi
+
+echo "issue_reported=${issue_reported}" >> $GITHUB_OUTPUT
+
+if $issue_reported; then
+    echo "::error::Issues were reported in the sanitizer report."
+    exit 1
+else
+    echo "::notice::No issues found in the sanitizer reports."
+    exit 0
+fi
