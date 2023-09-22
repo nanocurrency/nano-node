eb458593fe2c55aca0ed650a8af8628852d3261a
Colin LeMahieu <clemahieu@gmail.com>
2023-09-22 11:48:48 +0100

    Moving meta_handle to nano::store::lmdb::version

--- nano/store/lmdb/version.hpp
+++ nano/store/lmdb/version.hpp
@@ -2,6 +2,8 @@
 
 #include <nano/store/version.hpp>
 
+#include <lmdb/libraries/liblmdb/lmdb.h>
+
 namespace nano::store::lmdb
 {
 class version : public nano::store::version
@@ -13,5 +15,11 @@ public:
 	explicit version (nano::store::lmdb::component & store_a);
 	void put (store::write_transaction const & transaction_a, int version_a) override;
 	int get (store::transaction const & transaction_a) const override;
+
+	/**
+		 * Meta information about block store, such as versions.
+		 * nano::uint256_union (arbitrary key) -> blob
+		 */
+	MDB_dbi meta_handle{ 0 };
 };
 } // namespace nano::store::lmdb
