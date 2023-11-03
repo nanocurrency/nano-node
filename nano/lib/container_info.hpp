17dcdcc8a37720131204bdf214577ba87cc94a5a
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-11-03 23:37:33 +0100

    Introduce better `container_info` class

--- nano/lib/container_info.hpp
+++ nano/lib/container_info.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <list>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -46,5 +48,125 @@ public:
 
 private:
 	container_info info;
+};
+}
+
+/*
+ * New version
+ */
+namespace nano
+{
+class container_info;
+
+template <typename T>
+concept sized_container = requires (T a) {
+	typename T::value_type;
+	{
+		a.size ()
+	} -> std::convertible_to<std::size_t>;
+};
+
+template <typename T>
+concept container_info_collectable = requires (T a) {
+	{
+		a.collect_info ()
+	} -> std::convertible_to<container_info>;
+};
+
+class container_info
+{
+public:
+	// Child represented as <name, container_info>
+	// Using pair to avoid problems with incomplete types
+	using child = std::pair<std::string, container_info>;
+
+	struct entry
+	{
+		std::string name;
+		std::size_t size;
+		std::size_t sizeof_element;
+	};
+
+public:
+	/**
+	 * Adds a subcontainer
+	 */
+	void add (std::string const & name, container_info const & info)
+	{
+		children_m.emplace_back (name, info);
+	}
+
+	template <container_info_collectable T>
+	void add (std::string const & name, T const & container)
+	{
+		add (name, container.collect_info ());
+	}
+
+	/**
+	 * Adds an entry to this container
+	 */
+	void put (std::string const & name, std::size_t size, std::size_t sizeof_element = 0)
+	{
+		entries_m.emplace_back (entry{ name, size, sizeof_element });
+	}
+
+	template <class T>
+	void put (std::string const & name, std::size_t size)
+	{
+		put (name, size, sizeof (T));
+	}
+
+	template <sized_container T>
+	void put (std::string const & name, T const & container)
+	{
+		put (name, container.size (), sizeof (typename T::value_type));
+	}
+
+public:
+	bool children_empty () const
+	{
+		return children_m.empty ();
+	}
+
+	auto const & children () const
+	{
+		return children_m;
+	}
+
+	bool entries_empty () const
+	{
+		return entries_m.empty ();
+	}
+
+	auto const & entries () const
+	{
+		return entries_m;
+	}
+
+public:
+	// Needed to convert to legacy container_info_component during transition period
+	std::unique_ptr<nano::container_info_component> to_legacy (std::string const & name) const
+	{
+		auto composite = std::make_unique<nano::container_info_composite> (name);
+
+		// Add entries as leaf components
+		for (const auto & entry : entries_m)
+		{
+			nano::container_info info{ entry.name, entry.size, entry.sizeof_element };
+			composite->add_component (std::make_unique<nano::container_info_leaf> (info));
+		}
+
+		// Recursively convert children to composites and add them
+		for (const auto & [child_name, child] : children_m)
+		{
+			composite->add_component (child.to_legacy (child_name));
+		}
+
+		return composite;
+	}
+
+private:
+	std::list<child> children_m;
+	std::list<entry> entries_m;
 };
 }
