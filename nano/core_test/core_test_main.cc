652c0f02d97539b313706786868cf9359fc84aa0
Dimitrios Siganos <dimitris@siganos.org>
2024-02-10 10:37:35 +0000

    Print stacktrace in unit tests when receiving signal SIGSEGV

--- nano/core_test/core_test_main.cc
+++ nano/core_test/core_test_main.cc
@@ -1,10 +1,14 @@
 #include "gtest/gtest.h"
 
 #include <nano/lib/logging.hpp>
+#include <nano/lib/stacktrace.hpp>
 #include <nano/node/common.hpp>
 #include <nano/secure/utility.hpp>
 
 #include <boost/filesystem/path.hpp>
+
+#include <stdlib.h>
+#include <signal.h>
 
 constexpr std::size_t OPEN_FILE_DESCRIPTORS_LIMIT = 16384;
 
@@ -17,8 +21,16 @@ namespace test
 void force_nano_dev_network ();
 }
 
+void signalHandler(int signum)
+{
+	std::cerr << "SIGSEGV signal handler\n";
+	std::cerr << nano::generate_stacktrace () << std::endl;
+	exit(signum);
+}
+
 GTEST_API_ int main (int argc, char ** argv)
 {
+	signal(SIGSEGV, signalHandler);
 	nano::logger::initialize_for_tests (nano::log_config::tests_default ());
 	nano::set_file_descriptor_limit (OPEN_FILE_DESCRIPTORS_LIMIT);
 	nano::force_nano_dev_network ();
