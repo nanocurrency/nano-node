817f346eb3d0dc09c7d050581935cdf1f4d07238
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-02-13 14:37:43 +0100

    Analyze core dumps on test failure (#4430)

--- ci/tests/run-core-tests.sh
+++ ci/tests/run-core-tests.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-source "$(dirname "$BASH_SOURCE")/common.sh"
-
-BUILD_DIR=${1-${PWD}}
-
-${BUILD_DIR}/core_test$(get_exec_extension)
+$(dirname "$BASH_SOURCE")/run-tests.sh core_test
