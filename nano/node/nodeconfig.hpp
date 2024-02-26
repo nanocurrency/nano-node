72d6ca50db8c341d5ac42023cc7236f85df0a4f2
Colin LeMahieu <clemahieu@gmail.com>
2024-02-26 10:52:19 +0000

    Increasing block processing timeout default from 15 to 300 seconds.

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -88,7 +88,7 @@ public:
 	uint16_t external_port{ 0 };
 	std::chrono::milliseconds block_processor_batch_max_time{ std::chrono::milliseconds (500) };
 	/** Time to wait for block processing result */
-	std::chrono::seconds block_process_timeout{ 15 };
+	std::chrono::seconds block_process_timeout{ 300 };
 	std::chrono::seconds unchecked_cutoff_time{ std::chrono::seconds (4 * 60 * 60) }; // 4 hours
 	/** Timeout for initiated async operations */
 	std::chrono::seconds tcp_io_timeout{ (network_params.network.is_dev_network () && !is_sanitizer_build ()) ? std::chrono::seconds (5) : std::chrono::seconds (15) };
