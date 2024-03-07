254aa88dc14bdccfe303f9d819d609ab844e26ec
cuinix <65650185+cuinix@users.noreply.github.com>
2024-03-07 23:38:50 +0800

    fix some typos in docs (#4467)
    Signed-off-by: cuinix <915115094@qq.com>

--- nano/core_test/confirmation_height.cpp
+++ nano/core_test/confirmation_height.cpp
@@ -1279,7 +1279,7 @@ TEST (confirmation_heightDeathTest, modified_chain)
 		ledger, write_database_queue, 10ms, logger, stopped, batch_write_size, [] (auto const &) {}, [] (auto const &) {}, [] () { return 0; });
 
 		{
-			// This reads the blocks in the account, but prevents any writes from occuring yet
+			// This reads the blocks in the account, but prevents any writes from occurring yet
 			auto scoped_write_guard = write_database_queue.wait (nano::writer::testing);
 			bounded_processor.process (send);
 		}
@@ -1298,7 +1298,7 @@ TEST (confirmation_heightDeathTest, modified_chain)
 		ledger, write_database_queue, 10ms, logger, stopped, batch_write_size, [] (auto const &) {}, [] (auto const &) {}, [] () { return 0; });
 
 		{
-			// This reads the blocks in the account, but prevents any writes from occuring yet
+			// This reads the blocks in the account, but prevents any writes from occurring yet
 			auto scoped_write_guard = write_database_queue.wait (nano::writer::testing);
 			unbounded_processor.process (send);
 		}
@@ -1367,7 +1367,7 @@ TEST (confirmation_heightDeathTest, modified_chain_account_removed)
 		ledger, write_database_queue, 10ms, logger, stopped, batch_write_size, [] (auto const &) {}, [] (auto const &) {}, [] () { return 0; });
 
 		{
-			// This reads the blocks in the account, but prevents any writes from occuring yet
+			// This reads the blocks in the account, but prevents any writes from occurring yet
 			auto scoped_write_guard = write_database_queue.wait (nano::writer::testing);
 			unbounded_processor.process (open);
 		}
@@ -1387,7 +1387,7 @@ TEST (confirmation_heightDeathTest, modified_chain_account_removed)
 		ledger, write_database_queue, 10ms, logger, stopped, batch_write_size, [] (auto const &) {}, [] (auto const &) {}, [] () { return 0; });
 
 		{
-			// This reads the blocks in the account, but prevents any writes from occuring yet
+			// This reads the blocks in the account, but prevents any writes from occurring yet
 			auto scoped_write_guard = write_database_queue.wait (nano::writer::testing);
 			bounded_processor.process (open);
 		}
