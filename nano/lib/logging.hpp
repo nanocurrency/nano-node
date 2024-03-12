4552ad0b3c6c3ef81a3255ca472dceec595bb996
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-03-12 19:29:52 +0100

    Logging time helpers

--- nano/lib/logging.hpp
+++ nano/lib/logging.hpp
@@ -32,11 +32,57 @@ using logger_id = std::pair<nano::log::type, nano::log::detail>;
 
 std::string to_string (logger_id);
 logger_id parse_logger_id (std::string const &);
+}
 
+// Time helpers
+namespace nano::log
+{
 template <class Clock>
 auto microseconds (std::chrono::time_point<Clock> time)
 {
 	return std::chrono::duration_cast<std::chrono::microseconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto microseconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::microseconds> (duration).count ();
+}
+
+template <class Clock>
+auto milliseconds (std::chrono::time_point<Clock> time)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto milliseconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (duration).count ();
+}
+
+template <class Clock>
+auto seconds (std::chrono::time_point<Clock> time)
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto seconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (duration).count ();
+}
+
+template <class Clock>
+auto milliseconds_delta (std::chrono::time_point<Clock> time, std::chrono::time_point<Clock> now = Clock::now ())
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (now - time).count ();
+}
+
+template <class Clock>
+auto seconds_delta (std::chrono::time_point<Clock> time, std::chrono::time_point<Clock> now = Clock::now ())
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (now - time).count ();
 }
 }
 
