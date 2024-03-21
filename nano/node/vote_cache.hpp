8041e77f7cef6c26ad57d6b41bdfc8e908a3e1fe
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-03-21 16:58:35 +0100

    Adjust default max vote cache size

--- nano/node/vote_cache.hpp
+++ nano/node/vote_cache.hpp
@@ -38,8 +38,8 @@ public:
 	nano::error serialize (nano::tomlconfig & toml) const;
 
 public:
-	std::size_t max_size{ 1024 * 128 };
-	std::size_t max_voters{ 128 };
+	std::size_t max_size{ 1024 * 64 };
+	std::size_t max_voters{ 64 };
 	std::chrono::seconds age_cutoff{ 15 * 60 };
 };
 
