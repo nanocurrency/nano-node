6f8e1d3be2ea051222480c6fadbd21865e934b93
clemahieu <clemahieu@gmail.com>
2024-04-14 11:35:38 +0100

    Move node::receive_confirmed to wallets::receive_confirmed (#4557)
    This is inherently a wallet operation so it's more appropriate to be on the wallets class.
    This also breaks a direct dependency/cycle from block processing to the wallet operations.
    This also breaks a lock order inversion where a ledger transaction mutex is held while acquiring the wallet mutexes while most operations do it in the reverse order.

--- nano/node/node.hpp
+++ nano/node/node.hpp
@@ -87,7 +87,6 @@ public:
 	void stop ();
 	std::shared_ptr<nano::node> shared ();
 	int store_version ();
-	void receive_confirmed (store::transaction const & block_transaction_a, nano::block_hash const & hash_a, nano::account const & destination_a);
 	void process_confirmed (nano::election_status const &, uint64_t = 0);
 	void process_active (std::shared_ptr<nano::block> const &);
 	std::optional<nano::block_status> process_local (std::shared_ptr<nano::block> const &);
