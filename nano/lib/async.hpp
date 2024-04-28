3f61cd94a63a2d93d5a99ee7ae0286b63058440c
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-04-28 14:37:05 +0200

    Async task helper (#4590)
    * Introduce async task class
    
    * Use in `tcp_listener`
    
    * Comments
    
    * Tests
    
    * Use constructor for spawning

--- nano/lib/async.hpp
+++ nano/lib/async.hpp
@@ -4,6 +4,8 @@
 
 #include <boost/asio.hpp>
 
+#include <future>
+
 namespace asio = boost::asio;
 
 namespace nano::async
@@ -27,14 +29,30 @@ class cancellation
 {
 public:
 	explicit cancellation (nano::async::strand & strand) :
-		strand{ strand }
+		strand{ strand },
+		signal{ std::make_unique<asio::cancellation_signal> () }
 	{
 	}
 
+	cancellation (cancellation && other) = default;
+
+	cancellation & operator= (cancellation && other)
+	{
+		// Can only move if the strands are the same
+		debug_assert (strand == other.strand);
+
+		if (this != &other)
+		{
+			signal = std::move (other.signal);
+		}
+		return *this;
+	};
+
+public:
 	void emit (asio::cancellation_type type = asio::cancellation_type::all)
 	{
 		asio::dispatch (strand, asio::use_future ([this, type] () {
-			signal.emit (type);
+			signal->emit (type);
 		}))
 		.wait ();
 	}
@@ -43,13 +61,92 @@ public:
 	{
 		// Ensure that the slot is only connected once
 		debug_assert (std::exchange (slotted, true) == false);
-		return signal.slot ();
+		return signal->slot ();
 	}
 
-private:
 	nano::async::strand & strand;
-	asio::cancellation_signal signal;
+
+private:
+	std::unique_ptr<asio::cancellation_signal> signal; // Wrap the signal in a unique_ptr to enable moving
 
 	bool slotted{ false };
+};
+
+/**
+ * Wrapper with convenience functions and safety checks for asynchronous tasks.
+ * Aims to provide interface similar to std::thread.
+ */
+class task
+{
+public:
+	// Only thread-like void tasks are supported for now
+	using value_type = void;
+
+	task (nano::async::strand & strand) :
+		strand{ strand },
+		cancellation{ strand }
+	{
+	}
+
+	task (nano::async::strand & strand, auto && func) :
+		strand{ strand },
+		cancellation{ strand }
+	{
+		future = asio::co_spawn (
+		strand,
+		std::forward<decltype (func)> (func),
+		asio::bind_cancellation_slot (cancellation.slot (), asio::use_future));
+	}
+
+	~task ()
+	{
+		release_assert (!joinable () || ready (), "async task not joined before destruction");
+	}
+
+	task (task && other) = default;
+
+	task & operator= (task && other)
+	{
+		// Can only move if the strands are the same
+		debug_assert (strand == other.strand);
+
+		if (this != &other)
+		{
+			future = std::move (other.future);
+			cancellation = std::move (other.cancellation);
+		}
+		return *this;
+	}
+
+public:
+	bool joinable () const
+	{
+		return future.valid ();
+	}
+
+	bool ready () const
+	{
+		release_assert (future.valid ());
+		return future.wait_for (std::chrono::seconds{ 0 }) == std::future_status::ready;
+	}
+
+	void join ()
+	{
+		release_assert (future.valid ());
+		future.wait ();
+		future = {};
+	}
+
+	void cancel ()
+	{
+		debug_assert (joinable ());
+		cancellation.emit ();
+	}
+
+	nano::async::strand & strand;
+
+private:
+	std::future<value_type> future;
+	nano::async::cancellation cancellation;
 };
 }
