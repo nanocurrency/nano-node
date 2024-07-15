8c1493240e667b329e0a9238e5aeaeb7f61df693
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-07-15 20:30:58 +0200

    Avoid out of date code

--- nano/rpc_test/rpc.cpp
+++ nano/rpc_test/rpc.cpp
@@ -6775,7 +6775,12 @@ TEST (rpc, telemetry_all)
 		nano::telemetry_data telemetry_data;
 		auto const should_ignore_identification_metrics = true;
 		ASSERT_FALSE (telemetry_data.deserialize_json (config, should_ignore_identification_metrics));
-		ASSERT_TRUE (nano::test::compare_telemetry_data (telemetry_data, node->local_telemetry ()));
+		auto local_telemetry = node->local_telemetry ();
+		// Ignore the identification metrics
+		local_telemetry.signature.clear ();
+		local_telemetry.node_id.clear ();
+		local_telemetry.timestamp = telemetry_data.timestamp = {};
+		ASSERT_EQ (telemetry_data, local_telemetry);
 		ASSERT_FALSE (response.get_optional<std::string> ("node_id").is_initialized ());
 		ASSERT_FALSE (response.get_optional<std::string> ("signature").is_initialized ());
 	}
