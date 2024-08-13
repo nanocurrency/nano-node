05e42378ed2fc9a80be2ef34f9fe8e7f800d9002
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-08-13 18:07:03 +0200

    Move rep weights data to text files

---
+++ nano/node/bootstrap_weights_beta.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace nano
+{
+// TODO: Insert beta weights
+std::vector<std::pair<std::string, std::string>> preconfigured_weights_beta = {
+
+};
+uint64_t max_blocks_beta = 1;
+}
