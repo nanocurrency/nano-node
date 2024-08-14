9fcee4ebc3abccd682e3d2d44a00c577a6a32a1e
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-08-14 17:08:40 +0200

    Update reps weights with current values

--- nano/node/bootstrap_weights_beta.hpp
+++ nano/node/bootstrap_weights_beta.hpp
@@ -7,7 +7,25 @@ namespace nano
 {
 // TODO: Insert beta weights
 std::vector<std::pair<std::string, std::string>> preconfigured_weights_beta = {
-
+	{ "nano_3faucet4t1nnru6yra9iioia76jddur6zqg6d3fp7h1soyyd8qhgx6tizrsy", "37999200000000000000000000000000000000" },
+	{ "nano_1betazh7m3c9gwcsy7w3rzynbqr9gomjwn3cp59xqky48we46eaqptbdskh4", "36981063781291209870813128842298384384" },
+	{ "nano_3immionim1ypak7xbxe53ozdgk8sarjsu1ae7xbrnc8z9ntb8upnq47eugkx", "30000000000000000000000000000000000000" },
+	{ "nano_3kedrin3axwpe6jcx5fi8bx6sgjcre7bj4su5gpmfyd4gaijn8ndcyzgxche", "24000100000000000000000000000000000000" },
+	{ "nano_1robotghjtaub18dmo1ihkzg9jjs53ukthxrpt5x7eie3pg7k4ahb5i1uw64", "23200000000000000000000000000000000000" },
+	{ "nano_1bnano1dnhc356frb1owg4mhi4r47j1i15yq8nuyyso8fg64ux9kdxzmae5g", "16000000000000000000000000000000000000" },
+	{ "nano_1kitteh45srbwthaxq11tj54awh1trwuyt6o56ya4ghqinqo3a3jisbjg4dd", "12800000000000000000000000000000000000" },
+	{ "nano_18cgy87ikc4ruyh5aqwqe6dybe9os1ip3681y9wukypz5j7kgh35uxftss1x", "12000000000000000000000000000000000000" },
+	{ "nano_1betag7az9wk6rbis38s1d35hdsycz1bi95xg4g4j148p6afjk7embcurda4", "5800000000000000000000000000000000001" },
+	{ "nano_1defau1t9off1ine9rep99999999999999999999999999999999wgmuzxxy", "1009299201843717416503167458269866900" },
+	{ "nano_133ip6zipo9f5i949whwtf3gghh5ogai33tjo8biwozm4nkabi44ra3pyput", "200000000000000000000000000000000000" },
+	{ "nano_3powerbe4o7b9dzzixyfcraomnbogj9w4jtiip5nqzoxmneecrqffatcwq6z", "202443274731931539132406669770560" },
+	{ "nano_3power3gwb43rs7u9ky3rsjp6fojftejceexfkf845sfczyue4q3r1hfpr3o", "59304729762716301842242946990080" },
+	{ "nano_31kouqzaqbq4xryoge99n7jheaigrzom9d6yh13j37fmqnmac318ohwu6ine", "58500000000000000000000000000000" },
+	{ "nano_1iuz18n4g4wfp9gf7p1s8qkygxw7wx9qfjq6a9aq68uyrdnningdcjontgar", "52999999999999999999999998689227" },
+	{ "nano_33tmu196ih9q8bgz5mpyk8xaohydk4bahi96dgyx1b7fmzh3oopny9pr1uwm", "50000000000000000000000000000000" },
+	{ "nano_1j3ha7fczabhfenhcdg4zsbpkws7fwqpzzard8bdyme1uy3uoaojrds3dbxf", "40694338782254234122550878666752" },
+	{ "nano_3a91unimnh1mm74txmgwfmfo3q781iiqo844hu4ji85y6nxrou1nmrzbfj6y", "16500000000000000000000001310703" },
+	{ "nano_39cfzwuamk4ca4d9kawsyogc7pricoj361je4hxthm8xyu76r8bizhag4c3q", "10000000000000000000000000000000" },
 };
-uint64_t max_blocks_beta = 1;
+uint64_t max_blocks_beta = 38976923;
 }
