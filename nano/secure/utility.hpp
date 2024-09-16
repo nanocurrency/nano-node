d831937f12fd59f98cdab2401ec15237b19d6a92
Colin LeMahieu <clemahieu@gmail.com>
2024-09-16 13:27:52 +0100

    Extract function to create a random filename.

--- nano/secure/utility.hpp
+++ nano/secure/utility.hpp
@@ -9,6 +9,8 @@ namespace nano
 std::filesystem::path app_path ();
 // OS-specific way of finding a path to a home directory.
 std::filesystem::path working_path (nano::networks network = nano::network_constants::active_network);
+// Construct a random filename
+std::filesystem::path random_filename ();
 // Get a unique path within the home directory, used for testing.
 // Any directories created at this location will be removed when a test finishes.
 std::filesystem::path unique_path (nano::networks network = nano::network_constants::active_network);
