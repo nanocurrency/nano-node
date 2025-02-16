eedb696b923842f07d58038b0975810f1d2e3a14
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-02-16 14:47:00 +0100

    Cleanup

--- nano/node/vote_spacing.hpp
+++ nano/node/vote_spacing.hpp
@@ -16,31 +16,42 @@ namespace nano
 {
 class vote_spacing final
 {
-	class entry
+public:
+	explicit vote_spacing (std::chrono::milliseconds const & delay) :
+		delay{ delay }
 	{
-	public:
+	}
+
+	bool votable (nano::root const & root, nano::block_hash const & hash) const;
+	void flag (nano::root const & root, nano::block_hash const & hash);
+	std::size_t size () const;
+
+private:
+	void trim ();
+
+private:
+	std::chrono::milliseconds const delay;
+
+	struct entry
+	{
 		nano::root root;
 		std::chrono::steady_clock::time_point time;
 		nano::block_hash hash;
 	};
 
-	boost::multi_index_container<entry,
-	mi::indexed_by<
-	mi::hashed_non_unique<mi::tag<class tag_root>,
-	mi::member<entry, nano::root, &entry::root>>,
-	mi::ordered_non_unique<mi::tag<class tag_time>,
-	mi::member<entry, std::chrono::steady_clock::time_point, &entry::time>>>>
-	recent;
-	std::chrono::milliseconds const delay;
-	void trim ();
+	// clang-format off
+	class tag_root {};
+	class tag_time {};
 
-public:
-	vote_spacing (std::chrono::milliseconds const & delay) :
-		delay{ delay }
-	{
-	}
-	bool votable (nano::root const & root_a, nano::block_hash const & hash_a) const;
-	void flag (nano::root const & root_a, nano::block_hash const & hash_a);
-	std::size_t size () const;
+	using ordered_votes = boost::multi_index_container<entry,
+	mi::indexed_by<
+	    mi::hashed_non_unique<mi::tag<tag_root>,
+			mi::member<entry, nano::root, &entry::root>>,
+	    mi::ordered_non_unique<mi::tag<tag_time>,
+			mi::member<entry, std::chrono::steady_clock::time_point, &entry::time>>
+	>>;
+	// clang-format on
+
+	ordered_votes recent;
 };
 }
