dc1dbad292136f80fa6433b276411476f561cb4f
RickiNano <81099017+RickiNano@users.noreply.github.com>
2026-01-29 07:07:42 +0100

    Use C++ 23 for Docker

--- ci/prepare/linux/prepare-gcc.sh
+++ ci/prepare/linux/prepare-gcc.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 set -euox pipefail
+
+# Install GCC 13 for C++23 support (std::unreachable, etc.)
+apt-get install -yqq software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update -qq
+apt-get install -yqq gcc-13 g++-13
+
+# Set GCC 13 as default
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
